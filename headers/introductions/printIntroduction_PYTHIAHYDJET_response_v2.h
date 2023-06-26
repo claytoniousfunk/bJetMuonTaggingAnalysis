c7bec39980d7a22812e2d96086a94993d949c7e2
claytoniousfunk <cbennett0715@gmail.com>
2023-06-25 19:17:04 -0500

    updated stylized introduction

--- headers/introductions/printIntroduction_PYTHIAHYDJET_response_v2.h
+++ headers/introductions/printIntroduction_PYTHIAHYDJET_response_v2.h
@@ -1,5 +1,25 @@
 void printIntroduction(){
 
+
+cout << "          _,met$$$$$gg.                              " << endl;
+cout << "       ,g$$$$$$$$$$$$$$$P.                           " << endl;
+cout << "     ,g$$Ph     hhhY$$.h.h                           " << endl;
+cout << "    ,$$P'              `$$$.                         " << endl;
+cout << "   ',$$P       ,ggs.     `$$b:                       " << endl;
+cout << "   `d$$'     ,$Ph'   .    $$$                        " << endl;
+cout << "    $$P      d$'     ,    $$P                        " << endl;
+cout << "    $$:      $$.   -    ,d$$'                        " << endl;
+cout << "    $$\;      Y$b._   _,d$P'                         " << endl;
+cout << "    Y$$.    `.`hY$$$$Ph'                             " << endl;
+cout << "    `$$b      h-.__                                  " << endl;
+cout << "     `Y$$                                            " << endl;
+cout << "      `Y$$.               Clayton Bennett            " << endl;
+cout << "        `$$b.             UIC                        " << endl;
+cout << "          `Y$$b.          clayton.bennett@cern.ch    " << endl;
+cout << "             `hY$b._      github.com/claytoniousfunk " << endl;
+cout << "                 `}]]=u                              " << endl;
+
+
   cout << " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ " << endl;
   cout << " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ " << endl;
   cout << " ~~~~ PYTHIA+HYDJET response code ~~~~ " << endl;
