172db84bb56abb0c3122d09ad2a3ebde67cf7340
claytoniousfunk <cbennett0715@gmail.com>
2023-06-25 23:16:33 -0500

    moving condor to same folder as scan code

---
+++ src/scanning/pp/condor_pp_scan.py
@@ -0,0 +1,24 @@
+from myProcesses.condor.condor_utility import jobManager
+
+jobname = 'pp_SingleMuon_scan'
+
+#put the file list here
+dblist = '../../../fileNames/fileNames_pp_SingleMuon.txt'
+#dblist = '../../../fileNames/fileNames_pp_MinBias.txt'
+
+exe = 'pp_scan.C'
+
+jm = jobManager(jobSite ='cern', 
+	jobname = jobname, 	
+	method = 'root',
+	executable = exe, 
+	runlist = dblist,
+	time = '8h',
+	output_dir = ''
+)
+
+#how many files for each jobs
+jm.nsplit = 5 
+jm.make_tarball = False
+jm.generate_cfg()
+jm.submit()
