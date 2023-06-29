daf27104ea137cba8d84571764b2796acef7b1ac
claytoniousfunk <cbennett0715@gmail.com>
2023-06-29 10:32:18 -0500

    added central-only setup file, goldenFileNames

---
+++ headers/goldenFileNames.h
@@ -0,0 +1,10 @@
+// MC
+TString goldenFile_PYTHIA = "../rootFiles/PYTHIA/PYTHIA_scan.root";
+TString goldenFile_PYTHIAHYDJET_DiJet = "../rootFiles/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_scan.root";
+TString goldenFile_PYTHIAHYDJET_BJet = "";
+TString goldenFile_PYTHIAHYDJET_MuJet = "";
+// DATA
+TString goldenFile_pp_SingleMuon = "../rootFiles/pp/pp_SingleMuon_scan.root";
+TString goldenFile_pp_MinBias = "";
+TString goldenFile_PbPb_SingleMuon = "../rootFiles/PbPb/PbPb_SingleMuon_scan.root";
+TString goldenFile_PbPb_MinBias = "";
