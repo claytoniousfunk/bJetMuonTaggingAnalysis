c6b1ab68dddf84585f3a0b43c1548861f742915b
claytoniousfunk <cbennett0715@gmail.com>
2023-07-03 14:21:26 -0500

    added cout in hiBin fit function call

--- headers/fitFunctions/fitFxn_hiBin.h
+++ headers/fitFunctions/fitFxn_hiBin.h
@@ -1,12 +1,18 @@
 void loadFitFxn_hiBin(){
-fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]+[1]*x+[2]*x*x+[3]*x*x*x+[4]*x*x*x*x+[5]*x*x*x*x*x+[6]*x*x*x*x*x*x+[7]*x*x*x*x*x*x*x+[8]*x*x*x*x*x*x*x*x",0,190);
-fitFxn_hiBin->SetParameter(0,hiBinFitParam_0);
-fitFxn_hiBin->SetParameter(1,hiBinFitParam_1);
-fitFxn_hiBin->SetParameter(2,hiBinFitParam_2);
-fitFxn_hiBin->SetParameter(3,hiBinFitParam_3);
-fitFxn_hiBin->SetParameter(4,hiBinFitParam_4);
-fitFxn_hiBin->SetParameter(5,hiBinFitParam_5);
-fitFxn_hiBin->SetParameter(6,hiBinFitParam_6);
-fitFxn_hiBin->SetParameter(7,hiBinFitParam_7);
-fitFxn_hiBin->SetParameter(8,hiBinFitParam_8);
+
+  cout << "~~~~ loading hiBin fit function..." << endl
+  
+  fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]+[1]*x+[2]*x*x+[3]*x*x*x+[4]*x*x*x*x+[5]*x*x*x*x*x+[6]*x*x*x*x*x*x+[7]*x*x*x*x*x*x*x+[8]*x*x*x*x*x*x*x*x",0,190);
+  fitFxn_hiBin->SetParameter(0,hiBinFitParam_0);
+  fitFxn_hiBin->SetParameter(1,hiBinFitParam_1);
+  fitFxn_hiBin->SetParameter(2,hiBinFitParam_2);
+  fitFxn_hiBin->SetParameter(3,hiBinFitParam_3);
+  fitFxn_hiBin->SetParameter(4,hiBinFitParam_4);
+  fitFxn_hiBin->SetParameter(5,hiBinFitParam_5);
+  fitFxn_hiBin->SetParameter(6,hiBinFitParam_6);
+  fitFxn_hiBin->SetParameter(7,hiBinFitParam_7);
+  fitFxn_hiBin->SetParameter(8,hiBinFitParam_8);
+
+  cout << "~~~~ ... done!" << endl;
+  
 }
