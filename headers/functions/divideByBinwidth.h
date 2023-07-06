d5ef331f8795ab312c8c71367d9635daa84f522d
claytoniousfunk <cbennett0715@gmail.com>
2023-07-05 23:32:06 -0500

    changed variable name

--- headers/functions/divideByBinwidth.h
+++ headers/functions/divideByBinwidth.h
@@ -2,13 +2,13 @@ TH1D* divideByBinwidth(TH1D* h_in){
 
   for(int k = 0; k < h_in->GetSize(); k++){
 
-    double center = h_in->GetBinContent(k);
+    double value = h_in->GetBinContent(k);
     double error = h_in->GetBinError(k);
     double width = h_in->GetBinWidth(k);
 
     if(width != 0){
 
-      h_in->SetBinContent(k,center/width);
+      h_in->SetBinContent(k,value/width);
       h_in->SetBinError(k,error/width);
 
     }
