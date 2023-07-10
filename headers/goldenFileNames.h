9a049cd58b535a519578aa41ef793ca187c8407e
claytoniousfunk <cbennett0715@gmail.com>
2023-07-10 09:50:35 -0500

    added golden fileNames

--- headers/goldenFileNames.h
+++ headers/goldenFileNames.h
@@ -1,8 +1,16 @@
 // MC
 TString goldenFile_PYTHIA = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_scan.root";
+TString goldenFile_PYTHIA_response = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_scan_response.root";
+TString goldenFile_PYTHIA_muonReco = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_scan_muonReco.root";
+
 TString goldenFile_PYTHIAHYDJET_DiJet = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_scan.root";
+TString goldenFile_PYTHIAHYDJET_DiJet_response = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_response.root";
+TString goldenFile_PYTHIAHYDJET_DiJet_muonReco = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_muonReco.root";
+
 TString goldenFile_PYTHIAHYDJET_BJet = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_BJet_scan.root";
 TString goldenFile_PYTHIAHYDJET_MuJet = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_MuJet_scan.root";
+
+
 // DATA
 TString goldenFile_pp_SingleMuon = "../../rootFiles/scanningOutput/pp/pp_SingleMuon_scan.root";
 TString goldenFile_pp_MinBias = "../../rootFiles/scanningOutput/pp/pp_MinBias_scan.root";
