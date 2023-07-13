b28b6113cee74916a407f98f4d4692d3390b9633
claytoniousfunk <cbennett0715@gmail.com>
2023-07-13 09:53:34 -0500

    deleting unused configs

--- headers/config/config_PYTHIAHYDJET_matt.h
+++
@@ -1,6 +0,0 @@
-int isMC_status = 1;
-int AASetup_status = 0;
-TString jetTreeString = "akFlowPuCs4PFJetAnalyzer";
-TString hltString = "hltanalysis";
-const int NeventFilters = 4;
-std::string eventFilters[NeventFilters] = {"pprimaryVertexFilter", "collisionEventSelectionAODv2", "pclusterCompatibilityFilter", "pBeamScrapingFilter"};
