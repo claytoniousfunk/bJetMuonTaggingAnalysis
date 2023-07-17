b00315b61156073efea2be2e75883f0c340e9501
claytoniousfunk <cbennett0715@gmail.com>
2023-07-17 15:48:47 -0500

    more files

--- headers/goldenFileNames.h
+++ headers/goldenFileNames.h
@@ -1,10 +1,12 @@
 // MC
 TString goldenFile_PYTHIA = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_scan.root";
+TString goldenFile_PYTHIA_mu7 = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_mu7_scan.root";
 TString goldenFile_PYTHIA_response = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_response.root";
 TString goldenFile_PYTHIA_muonReco = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_muonReco.root";
 TString goldenFile_PYTHIA_muonReco_bJets = "../../rootFiles/scanningOutput/PYTHIA/PYTHIA_muonReco_bJets.root";
 
 TString goldenFile_PYTHIAHYDJET_DiJet = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_scan.root";
+TString goldenFile_PYTHIAHYDJET_DiJet_mu7 = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_mu7_scan.root";
 TString goldenFile_PYTHIAHYDJET_DiJet_response = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_response.root";
 TString goldenFile_PYTHIAHYDJET_DiJet_muonReco = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_muonReco.root";
 TString goldenFile_PYTHIAHYDJET_DiJet_muonReco_bJets = "../../rootFiles/scanningOutput/PYTHIAHYDJET/PYTHIAHYDJET_DiJet_muonReco_bJets.root";
@@ -15,6 +17,8 @@ TString goldenFile_PYTHIAHYDJET_MuJet = "../../rootFiles/scanningOutput/PYTHIAHY
 
 // DATA
 TString goldenFile_pp_SingleMuon = "../../rootFiles/scanningOutput/pp/pp_SingleMuon_scan.root";
+TString goldenFile_pp_SingleMuon_mu7 = "../../rootFiles/scanningOutput/pp/pp_SingleMuon_mu7_scan.root";
 TString goldenFile_pp_MinBias = "../../rootFiles/scanningOutput/pp/pp_MinBias_scan.root";
 TString goldenFile_PbPb_SingleMuon = "../../rootFiles/scanningOutput/PbPb/PbPb_SingleMuon_scan.root";
+TString goldenFile_PbPb_SingleMuon_mu7 = "../../rootFiles/scanningOutput/PbPb/PbPb_SingleMuon_mu7_scan.root";
 TString goldenFile_PbPb_MinBias = "../../rootFiles/scanningOutput/PbPb/PbPb_MinBias_scan.root";
