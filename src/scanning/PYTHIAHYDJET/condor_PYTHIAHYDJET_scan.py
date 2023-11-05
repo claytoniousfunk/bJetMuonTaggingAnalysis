c6c0fb59a3107773490a030fa5d0521b8a747789
claytoniousfunk <cbennett0715@gmail.com>
2023-11-05 13:22:17 -0600

    changed default fileNames and job title

--- src/scanning/PYTHIAHYDJET/condor_PYTHIAHYDJET_scan.py
+++ src/scanning/PYTHIAHYDJET/condor_PYTHIAHYDJET_scan.py
@@ -1,9 +1,12 @@
 from myProcesses.condor.condor_utility import jobManager
 
-jobname = 'PYTHIAHYDJET_DiJet_scan'
+jobname = 'PYTHIAHYDJET_DiJet_scan_Group1'
 
 #put the file list here
-dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_DiJet.txt'
+#dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_DiJet.txt'
+dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_DiJet_19Sep23_Group1.txt'
+#dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_DiJet_19Sep23_Group2.txt'
+#dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_DiJet_19Sep23_Group3.txt'
 #dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_BJet.txt'
 #dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_BJet_onlyMuonTaggedJets.txt'
 #dblist = '../../../fileNames/fileNames_PYTHIAHYDJET_MuJet.txt'
