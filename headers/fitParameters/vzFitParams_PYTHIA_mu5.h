612f0502c19febdff2ab6bb96d4957db2d27825f
claytoniousfunk <cbennett0715@gmail.com>
2023-11-05 16:13:39 -0600

    dedicated mu5 fit params file

---
+++ headers/fitParameters/vzFitParams_PYTHIA_mu5.h
@@ -0,0 +1,19 @@
+// PYTHIA
+// ---------- vz fit parameters
+// ---------- 3rd order polynomial
+
+cout << "loading fit params ... " << endl;
+
+int N_vzFitParam = 4;
+
+double vzFitParam_0 = 0.951479;
+double vzFitParam_1 = -0.00134172;
+double vzFitParam_2 = 0.0020366;
+double vzFitParam_3 = 1.89959e-05;
+
+double e_vzFitParam_0 = 0.0115145;
+double e_vzFitParam_1 = 0.00332478;
+double e_vzFitParam_2 = 0.00021252;
+double e_vzFitParam_3 = 2.97868e-05;
+
+cout << " ... done! " << endl;
