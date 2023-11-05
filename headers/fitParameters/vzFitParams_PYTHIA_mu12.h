4230080eb0d5df1ea5cbe8f43c559c4cdaafe569
claytoniousfunk <cbennett0715@gmail.com>
2023-11-05 17:15:02 -0600

    mu12 vzFitParams

---
+++ headers/fitParameters/vzFitParams_PYTHIA_mu12.h
@@ -0,0 +1,16 @@
+// PYTHIA
+// ---------- vz fit parameters
+// ---------- 3rd order polynomial
+
+int N_vzFitParam = 4;
+
+double vzFitParam_0 = 0.98111;
+double vzFitParam_1 = 0.00243898;
+double vzFitParam_2 = 0.0008241;
+double vzFitParam_3 = 2.59339e-06;
+
+double e_vzFitParam_0 = 0.0138631;
+double e_vzFitParam_1 = 0.00368576;
+double e_vzFitParam_2 = 0.000254239;
+double e_vzFitParam_3 = 3.48711e-05;
+
