8084d88b15845856c00fcc951994f838f6dc90cd
claytoniousfunk <cbennett0715@gmail.com>
2023-11-05 17:32:53 -0600

    mu7 fitParams

---
+++ headers/fitParameters/vzFitParams_PYTHIA_mu7.h
@@ -0,0 +1,16 @@
+// PYTHIA
+// ---------- vz fit parameters
+// ---------- 3rd order polynomial
+
+int N_vzFitParam = 4;
+
+double vzFitParam_0 = 1.00047;
+double vzFitParam_1 = 0.00139683;
+double vzFitParam_2 = 0.000651075;
+double vzFitParam_3 = 6.39534e-07;
+
+double e_vzFitParam_0 = 0.0116984;
+double e_vzFitParam_1 = 0.00347688;
+double e_vzFitParam_2 = 0.000206488;
+double e_vzFitParam_3 = 2.9965e-05;
+
