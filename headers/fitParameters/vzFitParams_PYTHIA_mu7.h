c2aa4c1742d1bded63ffdcab0697d1d16fa3e82d
claytoniousfunk <cbennett0715@gmail.com>
2023-11-05 22:05:03 -0600

    updated mu7 params

--- headers/fitParameters/vzFitParams_PYTHIA_mu7.h
+++ headers/fitParameters/vzFitParams_PYTHIA_mu7.h
@@ -4,13 +4,13 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 1.00047;
-double vzFitParam_1 = 0.00139683;
-double vzFitParam_2 = 0.000651075;
-double vzFitParam_3 = 6.39534e-07;
+double vzFitParam_0 = 0.990223;
+double vzFitParam_1 = 0.00116281;
+double vzFitParam_2 = 0.00081387;
+double vzFitParam_3 = -8.51494e-06;
 
-double e_vzFitParam_0 = 0.0116984;
-double e_vzFitParam_1 = 0.00347688;
-double e_vzFitParam_2 = 0.000206488;
-double e_vzFitParam_3 = 2.9965e-05;
+double e_vzFitParam_0 = 0.0093992;
+double e_vzFitParam_1 = 0.00269993;
+double e_vzFitParam_2 = 0.000184949;
+double e_vzFitParam_3 = 2.45627e-05;
 
