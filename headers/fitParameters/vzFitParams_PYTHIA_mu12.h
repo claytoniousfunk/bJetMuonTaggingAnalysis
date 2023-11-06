d8b839ad56c93e138fdffbeb1c2aa9679afad7fe
claytoniousfunk <cbennett0715@gmail.com>
2023-11-05 22:09:54 -0600

    updated mu12 vzFitParams

--- headers/fitParameters/vzFitParams_PYTHIA_mu12.h
+++ headers/fitParameters/vzFitParams_PYTHIA_mu12.h
@@ -4,13 +4,13 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 0.98111;
-double vzFitParam_1 = 0.00243898;
-double vzFitParam_2 = 0.0008241;
-double vzFitParam_3 = 2.59339e-06;
+double vzFitParam_0 = 0.978233;
+double vzFitParam_1 = -0.000585145;
+double vzFitParam_2 = 0.000852487;
+double vzFitParam_3 = 1.79466e-05;
 
-double e_vzFitParam_0 = 0.0138631;
-double e_vzFitParam_1 = 0.00368576;
-double e_vzFitParam_2 = 0.000254239;
-double e_vzFitParam_3 = 3.48711e-05;
+double e_vzFitParam_0 = 0.0108672;
+double e_vzFitParam_1 = 0.00289068;
+double e_vzFitParam_2 = 0.000193562;
+double e_vzFitParam_3 = 2.69439e-05;
 
