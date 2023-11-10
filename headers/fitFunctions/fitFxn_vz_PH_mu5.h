0778458d9e1a6ad8b5349afe5ff8f98a7dbe02cb
claytoniousfunk <cbennett0715@gmail.com>
2023-11-10 09:14:55 -0600

    rename fitFxn to specify mu5

---
+++ headers/fitFunctions/fitFxn_vz_PH_mu5.h
@@ -0,0 +1,19 @@
+void loadFitFxn_vz(){
+
+  cout << "~~ loading fit function ..." << endl;
+  
+  fitFxn_vz = new TF1("fitFxn_vz","[0] + [1]*x+ [2]*x*x + [3]*x*x*x",-15,15);
+
+  fitFxn_vz->SetParameter(0,vzFitParam_0);
+  fitFxn_vz->SetParameter(1,vzFitParam_1);
+  fitFxn_vz->SetParameter(2,vzFitParam_2);
+  fitFxn_vz->SetParameter(3,vzFitParam_3);
+
+  fitFxn_vz->SetParError(0,e_vzFitParam_0);
+  fitFxn_vz->SetParError(1,e_vzFitParam_1);
+  fitFxn_vz->SetParError(2,e_vzFitParam_2);
+  fitFxn_vz->SetParError(2,e_vzFitParam_3);
+
+  cout << "~~ ... finished!" << endl;
+ 
+}
