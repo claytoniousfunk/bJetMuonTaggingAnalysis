9f27f25e1b374f7549f374c425eafdce1961e32c
claytoniousfunk <cbennett0715@gmail.com>
2023-11-10 09:10:39 -0600

    new fit params

---
+++ headers/fitParameters/vzFitParams_PH_mu5.h
@@ -0,0 +1,15 @@
+// PYTHIA + HYDJET 0-90%
+// ------- vz fit parameters
+// ---------- 1st order polynomial
+
+int N_vzFitParam = 4;
+
+double vzFitParam_0 = 0.921724;
+double vzFitParam_1 = -0.0259746;
+double vzFitParam_2 = 0.00524493;
+double vzFitParam_3 = -0.000339564;
+
+double e_vzFitParam_0 = 0.0335123;
+double e_vzFitParam_1 = 0.00907256;
+double e_vzFitParam_2 = 0.00066744;
+double e_vzFitParam_3 = 8.50283e-05;
