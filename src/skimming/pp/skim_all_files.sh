e329d250586943431e58d4a24a2b93f705cac374
claytoniousfunk <cbennett0715@gmail.com>
2023-12-07 14:35:42 -0600

    adding skim_all_files.sh

---
+++ src/skimming/pp/skim_all_files.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+STARTGROUP=0
+ENDGROUP=0
+j=0
+
+for i in {208..302}
+
+do
+    
+    let j=$i-1
+
+    if [ $i -eq 1 ] ; then
+	let STARTGROUP=0
+	let ENDGROUP=20
+
+    elif [ $i -ne 302 ] ; then
+	let STARTGROUP=20*$j+1
+	let ENDGROUP=20*i
+    
+    elif [ $i -eq 302 ] ; then
+	let STARTGROUP=20*$j+1
+	let ENDGROUP=6033
+    fi
+
+    root -l -q "pp_skim.C($i,$STARTGROUP,$ENDGROUP)"
+
+done
+
