33e792a37126b7caadab6604d191be6cfca7c904
claytoniousfunk <cbennett0715@gmail.com>
2023-12-12 15:45:22 -0600

    batch skimming code

--- src/skimming/PbPb/skim_all_files.sh
+++ src/skimming/PbPb/skim_all_files.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-for i in {0..71}
+STARTGROUP=0
+ENDGROUP=0
+j=0
+
+for i in {1..359}
 
 do
     
-    root -l -q "PbPb_skim.C($i)"
+    let j=$i-1
+
+    if [ $i -eq 1 ] ; then
+	let STARTGROUP=0
+	let ENDGROUP=20
+
+    elif [ $i -ne 359 ] ; then
+	let STARTGROUP=20*$j+1
+	let ENDGROUP=20*i
+    
+    elif [ $i -eq 359 ] ; then
+	let STARTGROUP=20*$j+1
+	let ENDGROUP=7176
+    fi
+
+    root -l -q "PbPb_skim.C($i,$STARTGROUP,$ENDGROUP)"
 
 done
-
