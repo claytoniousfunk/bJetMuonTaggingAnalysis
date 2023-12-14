355ea72d3f6a92116d7eff82e3d53b9cb6216bdf
claytoniousfunk <cbennett0715@gmail.com>
2023-12-14 17:01:03 -0600

    add simple skim

---
+++ src/skimming/pp/pp_skim_simple.C
@@ -0,0 +1,53 @@
+void pp_skim_simple(int group = 1){
+
+  string in_file_name;
+  string output_file_base = "./";
+  in_file_name = "fileNames/fileNames_pp_SingleMuon_24Oct23.txt";
+  output_file_base += Form("output/pp_SingleMuon_skim_output_%i",group);
+  string output_file_extension = "";
+  output_file_extension += ".root";
+  TFile *output_file = new TFile((TString) (output_file_base+output_file_extension), "RECREATE");
+
+
+
+  std::ifstream instr(in_file_name.c_str(), std::ifstream::in);
+  if(!instr.is_open()) cout << "filelist not found!! Exiting..." << endl;
+  std::string filename;
+  Int_t ifile = 0;
+
+  int endfile = 6033;
+  while(instr>>filename && ifile<endfile){
+
+    if(ifile != group) continue;
+  
+    cout << (100.)*(1.0*ifile / (1.0*endfile)) << " %" << endl;
+    
+    TFile *old_file = TFile::Open(filename.c_str());
+    
+    TTree *old_jet_tree;
+    old_file->GetObject("ak4PFJetAnalyzer/t",old_jet_tree);
+    // deactivate all branches                                                                                                                                             
+    old_jet_tree->SetBranchStatus("*",0);
+    // activate only our variables of interest                                                                                                                             
+    old_jet_tree->SetBranchStatus("jtpt",1);
+    old_jet_tree->SetBranchStatus("jteta",1);
+    old_jet_tree->SetBranchStatus("jtphi",1);
+    old_jet_tree->SetBranchStatus("nref",1);
+
+    auto new_jet_tree = old_jet_tree->CloneTree(0);
+
+    new_jet_tree->CopyEntries(old_jet_tree);
+
+    output_file->Write(); 
+  
+
+
+  }
+
+
+
+
+
+
+
+}
