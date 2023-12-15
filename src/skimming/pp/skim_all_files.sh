004d2b95ae5efad5006a00d7af18a897a866c0b4
claytoniousfunk <cbennett0715@gmail.com>
2023-12-14 22:06:37 -0600

    alter

--- src/skimming/pp/skim_all_files.sh
+++ src/skimming/pp/skim_all_files.sh
@@ -4,26 +4,11 @@ STARTGROUP=0
 ENDGROUP=0
 j=0
 
-for i in {1..302}
+for i in {1..6033}
 
 do
     
-    let j=$i-1
-
-    if [ $i -eq 1 ] ; then
-	let STARTGROUP=0
-	let ENDGROUP=20
-
-    elif [ $i -ne 302 ] ; then
-	let STARTGROUP=20*$j+1
-	let ENDGROUP=20*i
-    
-    elif [ $i -eq 302 ] ; then
-	let STARTGROUP=20*$j+1
-	let ENDGROUP=6033
-    fi
-
-    root -l -q "pp_skim.C($i,$STARTGROUP,$ENDGROUP)"
+    root -l -q "pp_skim.C($i)"
 
 done
 
