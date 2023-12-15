275bc2998dbac6942fa11f8e586f4a2414990398
claytoniousfunk <cbennett0715@gmail.com>
2023-12-14 21:10:06 -0600

    rearrange output file declaration location

--- src/skimming/pp/pp_skim_simple.C
+++ src/skimming/pp/pp_skim_simple.C
@@ -6,7 +6,7 @@ void pp_skim_simple(int group = 1){
   output_file_base += Form("output/pp_SingleMuon_skim_output_%i",group);
   string output_file_extension = "";
   output_file_extension += ".root";
-  TFile *output_file = new TFile((TString) (output_file_base+output_file_extension), "RECREATE");
+  
 
 
 
@@ -18,7 +18,7 @@ void pp_skim_simple(int group = 1){
   int endfile = 6033;
 
   TFile *old_file;
-  while(instr>>filename && ifile<endfile){
+  while(instr>>filename && ifile!=){
 
     ifile++;
 
@@ -43,6 +43,8 @@ void pp_skim_simple(int group = 1){
 
     new_jet_tree->CopyEntries(old_jet_tree);
 
+
+    TFile *output_file = new TFile::Open((TString) (output_file_base+output_file_extension), "RECREATE");
     output_file->Write(); 
   
 
