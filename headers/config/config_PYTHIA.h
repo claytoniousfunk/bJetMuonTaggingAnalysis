50522b840af34891786f18ba29499899f0bbfd59
claytoniousfunk <cbennett0715@gmail.com>
2024-03-22 11:28:16 -0500

    adding removeHYDJETjet boolean

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -7,17 +7,18 @@ TString hltString = "hltTree";
 const int NeventFilters = 2;
 std::string eventFilters[NeventFilters] = {"pPAprimaryVertexFilter", "HBHENoiseFilterResultRun2Loose"};
 // options for systematic studies
-bool apply_JER_smear = true;
+bool apply_JER_smear = false;
 bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
 bool skipGenParticles = false;
-bool doVzReweight = true;
-bool doGenJetPthatFilter = true;
-bool doLeadingXjetDumpFilter = true;
+bool doVzReweight = false;
+bool doGenJetPthatFilter = false;
+bool doLeadingXjetDumpFilter = false;
 bool doXdumpReweight = false;
 bool doJetTrkMaxFilter = true;
-bool doEtaPhiMask = true;
+bool doRemoveHYDJETjet = true;
+bool doEtaPhiMask = false;
 
 
 
