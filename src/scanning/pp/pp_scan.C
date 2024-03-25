d64533307e39fcfeaf79669c9ef956f173a12905
claytoniousfunk <cbennett0715@gmail.com>
2024-03-25 12:03:52 -0500

    fix header path

--- src/scanning/pp/pp_scan.C
+++ src/scanning/pp/pp_scan.C
@@ -58,7 +58,7 @@
 // pthat filter function
 #include "../../../headers/functions/passesLeadingGenJetPthatFilter.h"
 // JetTrkMax filter function
-#include "../../../headers/functions/passesJetTrkMaxFilter.h"
+#include "../../../headers/functions/jet_filter/passesJetTrkMaxFilter.h"
 // print introduction
 #include "../../../headers/introductions/printIntroduction_pp_scan_V3p7.h"
 // analysis config
