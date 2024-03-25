b7c536f1af76a877ff93f349728c4f771abe3df8
claytoniousfunk <cbennett0715@gmail.com>
2024-03-25 12:46:09 -0500

    update to simple skim loop

--- src/skimming/PYTHIAHYDJET/skim_all_files.sh
+++ src/skimming/PYTHIAHYDJET/skim_all_files.sh
@@ -1,29 +1,9 @@
 #!/bin/bash
 
-STARTGROUP=0
-ENDGROUP=0
-j=0
-
-for i in {1..361}
+for i in {1..6863}
 
 do
     
-    let j=$i-1
-
-    if [ $i -eq 1 ] ; then
-	let STARTGROUP=0
-	let ENDGROUP=20
-
-    elif [ $i -ne 361 ] ; then
-	let STARTGROUP=20*$j+1
-	let ENDGROUP=20*i
-	
-    elif [ $i -eq 361 ] ; then
-	let STARTGROUP=20*$j+1
-	let ENDGROUP=7202
-    fi
-
-    root -l -q "PYTHIAHYDJET_skim.C($i,$STARTGROUP,$ENDGROUP)"
+    root -l -q "PYTHIAHYDJET_skim_simple.C($i)"
 
 done
-
