f6f841c8333850ef8a308202f62ef853f9f3829f
claytoniousfunk <cbennett0715@gmail.com>
2024-04-01 15:02:22 -0500

    configure for mu12

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan.C
@@ -40,12 +40,12 @@
 #include "../../../headers/AnalysisSetupV2p1.h"
 // vz-fit parameters
 //#include "../../../headers/fitParameters/vzFitParams_PH_mu5.h"
-#include "../../../headers/fitParameters/vzFitParams_PH_mu7.h"
-//#include "../../../headers/fitParameters/vzFitParams_PH_mu12.h"
+//#include "../../../headers/fitParameters/vzFitParams_PH_mu7.h"
+#include "../../../headers/fitParameters/vzFitParams_PH_mu12.h"
 // hiBin-fit parameters
 //#include "../../../headers/fitParameters/hiBinFitParams_mu5.h"
-#include "../../../headers/fitParameters/hiBinFitParams_mu7.h"
-//#include "../../../headers/fitParameters/hiBinFitParams_mu12.h"
+//#include "../../../headers/fitParameters/hiBinFitParams_mu7.h"
+#include "../../../headers/fitParameters/hiBinFitParams_mu12.h"
 
 
 TF1 *fitFxn_hiBin, *fitFxn_vz;
@@ -198,7 +198,7 @@ void PYTHIAHYDJET_scan(int group = 1){
   // TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_DiJet_withGS_scan_mu12_pThat15_removeHYDJETjets_vzAndHiBinReweight_JERsmear/PYTHIAHYDJET_scan_output_%i.root",group);
 
   TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_MuJet_withGS_withWTA/PYTHIAHYDJET_MuJet_skim_output_%i.root",group);
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_MuJet_withGS_scan_mu7_pThat15_removeHYDJETjets_vzAndHiBinReweight/PYTHIAHYDJET_scan_output_%i.root",group);
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_MuJet_withGS_scan_mu12_pThat15_removeHYDJETjets_vzAndHiBinReweight/PYTHIAHYDJET_scan_output_%i.root",group);
 
   // TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_BJet_withGS_withWTA/PYTHIAHYDJET_BJet_skim_output_%i.root",group);
   // TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_BJet_withGS_scan_mu5_pThat15_removeHYDJETjets_vzAndHiBinReweight/PYTHIAHYDJET_scan_output_%i.root",group);
@@ -636,11 +636,11 @@ void PYTHIAHYDJET_scan(int group = 1){
     // int triggerDecision = em->HLT_HIL3Mu5_NHitQ10_v1;
     // int triggerDecision_Prescl = em->HLT_HIL3Mu5_NHitQ10_v1_Prescl;
 
-    int triggerDecision = em->HLT_HIL3Mu7_NHitQ10_v1;
-    int triggerDecision_Prescl = em->HLT_HIL3Mu7_NHitQ10_v1_Prescl;
+    // int triggerDecision = em->HLT_HIL3Mu7_NHitQ10_v1;
+    // int triggerDecision_Prescl = em->HLT_HIL3Mu7_NHitQ10_v1_Prescl;
 
-    // int triggerDecision = em->HLT_HIL3Mu12_v1;
-    // int triggerDecision_Prescl = em->HLT_HIL3Mu12_v1_Prescl;
+    int triggerDecision = em->HLT_HIL3Mu12_v1;
+    int triggerDecision_Prescl = em->HLT_HIL3Mu12_v1_Prescl;
 
     if(triggerIsOn(triggerDecision,triggerDecision_Prescl)) {
       evtTriggerDecision = true;
