171f119b61f98ccad164f7463d1fd6ceafc4532f
claytoniousfunk <cbennett0715@gmail.com>
2024-04-01 17:56:12 -0500

    configure for BJet scan

--- src/scanning/PYTHIAHYDJET/scan_all_files_18.sh
+++ src/scanning/PYTHIAHYDJET/scan_all_files_18.sh
@@ -5,7 +5,7 @@
 # N = 4257 for PH BJet
 # N = 2923 for PH MuJet
 
-for i in {2801..3000}
+for i in {3401..3600}
 
 do
 
--- src/scanning/PYTHIAHYDJET/scan_all_files_21.sh
+++ src/scanning/PYTHIAHYDJET/scan_all_files_21.sh
@@ -5,7 +5,7 @@
 # N = 4257 for PH BJet
 # N = 2923 for PH MuJet
 
-for i in {2801..3000}
+for i in {4001..4200}
 
 do
 
