33356a0ecc381d3e5ba91be3f0d58133d8bd2e39
claytoniousfunk <cbennett0715@gmail.com>
2024-04-04 11:14:43 -0500

    updated scan-calling macro

---
+++ src/scanning/pp/scan_select_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Prompt user for directory path
+echo "Enter starting file number:"
+read FILE_NUMBER_START
+echo "Enter ending file number:"
+read FILE_NUMBER_END
+
+
+for i in {$FILE_NUMBER_START..$FILE_NUMBER_END}
+
+do
+
+    root -l -q "pp_scan.C($i)"
+
+done
+
+      
+
+
