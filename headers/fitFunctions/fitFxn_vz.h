05a03df4b022e6772fb4658789310f4a1efb5e5f
claytoniousfunk <cbennett0715@gmail.com>
2024-04-08 16:05:08 -0500

    update fitFxn to pol3

--- headers/fitFunctions/fitFxn_vz.h
+++ headers/fitFunctions/fitFxn_vz.h
@@ -1,5 +1,7 @@
 void loadFitFxn_vz(){
-fitFxn_vz = new TF1("fitFxn_vz","[0]+[1]*x",-15,15);
-fitFxn_vz->SetParameter(0,vzFitParam_0);
-fitFxn_vz->SetParameter(1,vzFitParam_1);
+  fitFxn_vz = new TF1("fitFxn_vz","[0]+[1]*x+[2]*x*x+[3]*x*x*x",-15,15);
+  fitFxn_vz->SetParameter(0,vzFitParam_0);
+  fitFxn_vz->SetParameter(1,vzFitParam_1);
+  fitFxn_vz->SetParameter(2,vzFitParam_2);
+  fitFxn_vz->SetParameter(3,vzFitParam_3);
 }
