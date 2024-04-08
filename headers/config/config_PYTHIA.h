737871f7a04594cf01e672b0f130546e97ecaf12
claytoniousfunk <cbennett0715@gmail.com>
2024-04-08 17:33:08 -0500

    turn on JERsmear

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -7,19 +7,22 @@ TString hltString = "hltTree";
 const int NeventFilters = 2;
 std::string eventFilters[NeventFilters] = {"pPAprimaryVertexFilter", "HBHENoiseFilterResultRun2Loose"};
 // options for systematic studies
-bool apply_JER_smear = false;
+bool apply_JER_smear = true;
 bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
 bool skipGenParticles = false;
+// reweighting functions
 bool doVzReweight = true;
+bool doJetPtReweight = true;
+// jet-based filers
 bool doGenJetPthatFilter = false;
 bool doLeadingXjetDumpFilter = false;
 bool doXdumpReweight = false;
 bool doJetTrkMaxFilter = true;
 bool doRemoveHYDJETjet = true;
 bool doEtaPhiMask = false;
-bool doJetPtReweight = true;
+
 
 
 
