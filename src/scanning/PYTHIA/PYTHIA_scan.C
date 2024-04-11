3c9731657fc163ff663a72d167d04a8f0b105a46
claytoniousfunk <cbennett0715@gmail.com>
2024-04-11 10:33:29 -0500

    config for JERsmear

--- src/scanning/PYTHIA/PYTHIA_scan.C
+++ src/scanning/PYTHIA/PYTHIA_scan.C
@@ -166,7 +166,7 @@ TH2D *h_recoGenDpt_flavor[NJetPtIndices];
 void PYTHIA_scan(int group = 1){
 
   TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIA_DiJet_withGS/PYTHIA_DiJet_skim_output_%i.root",group);
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_scan_mu12_tight_pThat15_removeHYDJETjets_updatedTriggerLogic_jetPtReweight_vzReweight/PYTHIA_DiJet_scan_output_%i.root",group);
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_scan_mu12_tight_pThat15_removeHYDJETjets_updatedTriggerLogic_jetPtReweight_vzReweight_JERsmear/PYTHIA_DiJet_scan_output_%i.root",group);
 
 
   printIntroduction_PYTHIA_scan_V3p7();
