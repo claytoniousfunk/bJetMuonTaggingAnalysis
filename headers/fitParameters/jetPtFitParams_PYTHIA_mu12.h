1256da29de15ed2b1f4f7582d764cdc5215397b5
claytoniousfunk <cbennett0715@gmail.com>
2024-04-11 09:08:56 -0500

    update jetPt reweight parameters

--- headers/fitParameters/jetPtFitParams_PYTHIA_mu12.h
+++ headers/fitParameters/jetPtFitParams_PYTHIA_mu12.h
@@ -4,13 +4,13 @@
 
 int N_jetPtFitParam = 4;
 
-double jetPtFitParam_0 = 0.864986;
-double jetPtFitParam_1 = 0.00242109;
-double jetPtFitParam_2 = -5.20034e-06;
-double jetPtFitParam_3 = -8.94811e-10;
+double jetPtFitParam_0 = 0.810428;
+double jetPtFitParam_1 = 0.00366141;
+double jetPtFitParam_2 = -1.32835e-05;
+double jetPtFitParam_3 = 1.44467e-08;
 
-double e_jetPtFitParam_0 = 0.0468411;
-double e_jetPtFitParam_1 = 0.000930405;
-double e_jetPtFitParam_2 = 5.44367e-06;
-double e_jetPtFitParam_3 = 9.48967e-09;
+double e_jetPtFitParam_0 = 0.0387162;
+double e_jetPtFitParam_1 = 0.000711458;
+double e_jetPtFitParam_2 = 3.79039e-06;
+double e_jetPtFitParam_3 = 5.92132e-09;
 
