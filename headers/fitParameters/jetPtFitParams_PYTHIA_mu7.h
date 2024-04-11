36b319897f9e0912456105f92dffafd6922a29c0
claytoniousfunk <cbennett0715@gmail.com>
2024-04-11 11:38:28 -0500

    updated jetPt fit params

--- headers/fitParameters/jetPtFitParams_PYTHIA_mu7.h
+++ headers/fitParameters/jetPtFitParams_PYTHIA_mu7.h
@@ -4,13 +4,13 @@
 
 int N_jetPtFitParam = 4;
 
-double jetPtFitParam_0 = 0.92962;
-double jetPtFitParam_1 = 0.00079916;
-double jetPtFitParam_2 = 2.16074e-06;
-double jetPtFitParam_3 = -7.23028e-09;
+double jetPtFitParam_0 = 0.942281;
+double jetPtFitParam_1 = 0.000665849;
+double jetPtFitParam_2 = 2.10975e-06;
+double jetPtFitParam_3 = -7.06832e-09;
 
-double e_jetPtFitParam_0 = 0.0620178;
-double e_jetPtFitParam_1 = 0.00118314;
-double e_jetPtFitParam_2 = 6.48465e-06;
-double e_jetPtFitParam_3 = 1.03604e-08;
+double e_jetPtFitParam_0 = 0.0616573;
+double e_jetPtFitParam_1 = 0.00117203;
+double e_jetPtFitParam_2 = 6.398e-06;
+double e_jetPtFitParam_3 = 1.01774e-08;
 
