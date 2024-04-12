f6a0f9b3740a1bef08d95c54f19d305bef8d00fe
claytoniousfunk <cbennett0715@gmail.com>
2024-04-11 21:53:47 -0500

    new hiBin fit params (pThat 15, mu7)

---
+++ headers/fitParameters/hiBinFitParams_pThat15_mu7.h
@@ -0,0 +1,22 @@
+
+double hiBinFitParam_0 = 1.1292;
+double hiBinFitParam_1 = 0.0554725;
+double hiBinFitParam_2 = -0.00144271;
+double hiBinFitParam_3 = 1.06159e-05;
+double hiBinFitParam_4 = -2.51826e-08;
+double hiBinFitParam_5 = 0.0;
+double hiBinFitParam_6 = 0.0;
+double hiBinFitParam_7 = 0.0;
+double hiBinFitParam_8 = 0.0;
+
+
+double e_hiBinFitParam_0 = 0.153492;
+double e_hiBinFitParam_1 = 0.009744;
+double e_hiBinFitParam_2 = 0.000190517;
+double e_hiBinFitParam_3 = 1.4052e-06;
+double e_hiBinFitParam_4 = 3.50137e-09;
+double e_hiBinFitParam_5 = 0.0;
+double e_hiBinFitParam_6 = 0.0;
+double e_hiBinFitParam_7 = 0.0;
+double e_hiBinFitParam_8 = 0.0;
+
