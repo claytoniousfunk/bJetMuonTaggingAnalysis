19756c5c3f267c8e7ed76883ff5e0ac556c0c6ab
claytoniousfunk <cbennett0715@gmail.com>
2024-04-12 10:02:05 -0500

    newly derived mu12 vz and hiBin fitParams

--- headers/fitParameters/hiBinFitParams_mu12.h
+++ headers/fitParameters/hiBinFitParams_mu12.h
@@ -1,20 +1,20 @@
 
-double hiBinFitParam_0 = 1.73086;
-double hiBinFitParam_1 = -0.000628177;
-double hiBinFitParam_2 = -0.000273071;
-double hiBinFitParam_3 = 1.81191e-06;
-double hiBinFitParam_4 = -3.10518e-09;
+double hiBinFitParam_0 = 2.77572;
+double hiBinFitParam_1 = -0.0306793;
+double hiBinFitParam_2 = -1.61439e-05;
+double hiBinFitParam_3 = 1.12534e-06;
+double hiBinFitParam_4 = -3.11143e-09;
 double hiBinFitParam_5 = 0.0;
 double hiBinFitParam_6 = 0.0;
 double hiBinFitParam_7 = 0.0;
 double hiBinFitParam_8 = 0.0;
 
 
-double e_hiBinFitParam_0 = 0.112152;
-double e_hiBinFitParam_1 = 0.00663453;
-double e_hiBinFitParam_2 = 0.000123072;
-double e_hiBinFitParam_3 = 8.85069e-07;
-double e_hiBinFitParam_4 = 2.17634e-09;
+double e_hiBinFitParam_0 = 0.188906;
+double e_hiBinFitParam_1 = 0.00892099;
+double e_hiBinFitParam_2 = 0.000145581;
+double e_hiBinFitParam_3 = 9.70868e-07;
+double e_hiBinFitParam_4 = 2.27297e-09;
 double e_hiBinFitParam_5 = 0.0;
 double e_hiBinFitParam_6 = 0.0;
 double e_hiBinFitParam_7 = 0.0;
--- headers/fitParameters/vzFitParams_PH_mu12.h
+++ headers/fitParameters/vzFitParams_PH_mu12.h
@@ -4,12 +4,12 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 0.958218;
-double vzFitParam_1 = -0.00288168;
-double vzFitParam_2 = 0.00220743;
-double vzFitParam_3 = -0.000253701;
+double vzFitParam_0 = 1.04067;
+double vzFitParam_1 = -0.0141435;
+double vzFitParam_2 = 0.000990322;
+double vzFitParam_3 = -8.1175e-05;
 
-double e_vzFitParam_0 = 0.0181642;
-double e_vzFitParam_1 = 0.00491345;
-double e_vzFitParam_2 = 0.000424041;
-double e_vzFitParam_3 = 5.01748e-05;
+double e_vzFitParam_0 = 0.017221;
+double e_vzFitParam_1 = 0.00483552;
+double e_vzFitParam_2 = 0.00039276;
+double e_vzFitParam_3 = 4.84311e-05;
