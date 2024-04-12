d5d7add34cf6a2374144c0c32f3220133d1b7aa8
claytoniousfunk <cbennett0715@gmail.com>
2024-04-11 21:58:21 -0500

    updated params for pThat15

--- headers/fitParameters/hiBinFitParams_mu7.h
+++ headers/fitParameters/hiBinFitParams_mu7.h
@@ -1,20 +1,20 @@
 
-double hiBinFitParam_0 = 1.84514;
-double hiBinFitParam_1 = -0.00501789;
-double hiBinFitParam_2 = -0.000253121;
-double hiBinFitParam_3 = 1.97719e-06;
-double hiBinFitParam_4 = -4.01901e-09;
+double hiBinFitParam_0 = 1.1292;
+double hiBinFitParam_1 = 0.0554725;
+double hiBinFitParam_2 = -0.00144271;
+double hiBinFitParam_3 = 1.06159e-05;
+double hiBinFitParam_4 = -2.51826e-08;
 double hiBinFitParam_5 = 0.0;
 double hiBinFitParam_6 = 0.0;
 double hiBinFitParam_7 = 0.0;
 double hiBinFitParam_8 = 0.0;
 
 
-double e_hiBinFitParam_0 = 0.114801;
-double e_hiBinFitParam_1 = 0.00655088;
-double e_hiBinFitParam_2 = 0.000119548;
-double e_hiBinFitParam_3 = 8.51985e-07;
-double e_hiBinFitParam_4 = 2.08276e-09;
+double e_hiBinFitParam_0 = 0.153492;
+double e_hiBinFitParam_1 = 0.009744;
+double e_hiBinFitParam_2 = 0.000190517;
+double e_hiBinFitParam_3 = 1.4052e-06;
+double e_hiBinFitParam_4 = 3.50137e-09;
 double e_hiBinFitParam_5 = 0.0;
 double e_hiBinFitParam_6 = 0.0;
 double e_hiBinFitParam_7 = 0.0;
