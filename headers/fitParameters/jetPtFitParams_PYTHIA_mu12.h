27af43f696fe5036bd338d00815f939938e60519
claytoniousfunk <cbennett0715@gmail.com>
2024-04-22 16:09:45 -0500

    updated jetPt fit parameters

--- headers/fitParameters/jetPtFitParams_PYTHIA_mu12.h
+++ headers/fitParameters/jetPtFitParams_PYTHIA_mu12.h
@@ -2,15 +2,14 @@
 // ---------- jetPt fit parameters
 // ---------- 3rd order polynomial
 
-int N_jetPtFitParam = 4;
+int N_jetPtFitParam = 3;
 
-double jetPtFitParam_0 = 0.810428;
-double jetPtFitParam_1 = 0.00366141;
-double jetPtFitParam_2 = -1.32835e-05;
-double jetPtFitParam_3 = 1.44467e-08;
+double jetPtFitParam_0 = 1.12916;
+double jetPtFitParam_1 = -0.454931;
+double jetPtFitParam_2 = -0.0190468;
 
-double e_jetPtFitParam_0 = 0.0387162;
-double e_jetPtFitParam_1 = 0.000711458;
-double e_jetPtFitParam_2 = 3.79039e-06;
-double e_jetPtFitParam_3 = 5.92132e-09;
+double e_jetPtFitParam_0 = 0.0122295;
+double e_jetPtFitParam_1 = 0.114483;
+double e_jetPtFitParam_2 = 0.00437666;
+
 
