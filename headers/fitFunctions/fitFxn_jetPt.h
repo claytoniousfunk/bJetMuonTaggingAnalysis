20ff5243062570a75e668b06db861e37ef3d324d
claytoniousfunk <cbennett0715@gmail.com>
2024-04-22 13:45:38 -0500

    change functional to f(x) = a + b*exp(c*x)

--- headers/fitFunctions/fitFxn_jetPt.h
+++ headers/fitFunctions/fitFxn_jetPt.h
@@ -1,7 +1,6 @@
 void loadFitFxn_jetPt(){
-  fitFxn_jetPt = new TF1("fitFxn_jetPt","[0]+[1]*x+[2]*x*x+[3]*x*x*x",0,1000);
+  fitFxn_jetPt = new TF1("fitFxn_jetPt","[0]+[1]*TMath::Exp([2]*x)",0,500);
   fitFxn_jetPt->SetParameter(0,jetPtFitParam_0);
   fitFxn_jetPt->SetParameter(1,jetPtFitParam_1);
   fitFxn_jetPt->SetParameter(2,jetPtFitParam_2);
-  fitFxn_jetPt->SetParameter(3,jetPtFitParam_3);
 }
