bfbffb414767ef2d99af0d36d1c37cd091f1aaab
claytoniousfunk <cbennett0715@gmail.com>
2024-04-22 13:46:01 -0500

    derived mu7 PYTHIA jetPt fit params

--- headers/fitParameters/jetPtFitParams_PYTHIA_mu7.h
+++ headers/fitParameters/jetPtFitParams_PYTHIA_mu7.h
@@ -2,15 +2,14 @@
 // ---------- jetPt fit parameters
 // ---------- 3rd order polynomial
 
-int N_jetPtFitParam = 4;
+int N_jetPtFitParam = 3;
 
-double jetPtFitParam_0 = 0.942281;
-double jetPtFitParam_1 = 0.000665849;
-double jetPtFitParam_2 = 2.10975e-06;
-double jetPtFitParam_3 = -7.06832e-09;
+double jetPtFitParam_0 = 1.18793;
+double jetPtFitParam_1 = -0.310975;
+double jetPtFitParam_2 = -0.0076222;
 
-double e_jetPtFitParam_0 = 0.0616573;
-double e_jetPtFitParam_1 = 0.00117203;
-double e_jetPtFitParam_2 = 6.398e-06;
-double e_jetPtFitParam_3 = 1.01774e-08;
+double e_jetPtFitParam_0 = 0.051405;
+double e_jetPtFitParam_1 = 0.0400062;
+double e_jetPtFitParam_2 = 0.00351582;
+
 
