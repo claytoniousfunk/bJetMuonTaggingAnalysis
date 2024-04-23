5954e2d2fbc1bb4f14dcf070e76e5e9de3078210
claytoniousfunk <cbennett0715@gmail.com>
2024-04-22 22:44:41 -0500

    updated hiBin fit params for mu7 PH

--- headers/fitParameters/hiBinFitParams_mu7.h
+++ headers/fitParameters/hiBinFitParams_mu7.h
@@ -1,20 +1,20 @@
 
-double hiBinFitParam_0 = 1.1292;
-double hiBinFitParam_1 = 0.0554725;
-double hiBinFitParam_2 = -0.00144271;
-double hiBinFitParam_3 = 1.06159e-05;
-double hiBinFitParam_4 = -2.51826e-08;
+double hiBinFitParam_0 = 3.2556;
+double hiBinFitParam_1 = -0.0554602;
+double hiBinFitParam_2 = 0.000315899;
+double hiBinFitParam_3 = -5.97993e-07;
+double hiBinFitParam_4 = 0.0;
 double hiBinFitParam_5 = 0.0;
 double hiBinFitParam_6 = 0.0;
 double hiBinFitParam_7 = 0.0;
 double hiBinFitParam_8 = 0.0;
 
 
-double e_hiBinFitParam_0 = 0.153492;
-double e_hiBinFitParam_1 = 0.009744;
-double e_hiBinFitParam_2 = 0.000190517;
-double e_hiBinFitParam_3 = 1.4052e-06;
-double e_hiBinFitParam_4 = 3.50137e-09;
+double e_hiBinFitParam_0 = 0.107888;
+double e_hiBinFitParam_1 = 0.00286065;
+double e_hiBinFitParam_2 = 2.45638e-05;
+double e_hiBinFitParam_3 = 6.76112e-08;
+double e_hiBinFitParam_4 = 0.0;
 double e_hiBinFitParam_5 = 0.0;
 double e_hiBinFitParam_6 = 0.0;
 double e_hiBinFitParam_7 = 0.0;
