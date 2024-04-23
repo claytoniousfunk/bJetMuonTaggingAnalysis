2848380a5ee3f133c613134e5b67d0c58455fd64
claytoniousfunk <cbennett0715@gmail.com>
2024-04-23 01:05:30 -0500

    update vz and jetPt fit params (for pThat > 30 instead of 15)

--- headers/fitParameters/jetPtFitParams_PYTHIA_mu12.h
+++ headers/fitParameters/jetPtFitParams_PYTHIA_mu12.h
@@ -4,12 +4,12 @@
 
 int N_jetPtFitParam = 3;
 
-double jetPtFitParam_0 = 1.12916;
-double jetPtFitParam_1 = -0.454931;
-double jetPtFitParam_2 = -0.0190468;
+double jetPtFitParam_0 = 1.09397;
+double jetPtFitParam_1 = -0.355879;
+double jetPtFitParam_2 = -0.0165736;
 
-double e_jetPtFitParam_0 = 0.0122295;
-double e_jetPtFitParam_1 = 0.114483;
-double e_jetPtFitParam_2 = 0.00437666;
+double e_jetPtFitParam_0 = 0.0131985;
+double e_jetPtFitParam_1 = 0.101813;
+double e_jetPtFitParam_2 = 0.00457404;
 
 
--- headers/fitParameters/vzFitParams_PYTHIA_mu12.h
+++ headers/fitParameters/vzFitParams_PYTHIA_mu12.h
@@ -4,13 +4,13 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 0.972492;
-double vzFitParam_1 = 0.00306736;
-double vzFitParam_2 = 0.000910234;
-double vzFitParam_3 = -1.06023e-05;
+double vzFitParam_0 = 0.971963;
+double vzFitParam_1 = 0.00307992;
+double vzFitParam_2 = 0.000898936;
+double vzFitParam_3 = -1.06707e-05;
 
-double e_vzFitParam_0 = 0.00885079;
-double e_vzFitParam_1 = 0.00231223;
-double e_vzFitParam_2 = 0.000172376;
-double e_vzFitParam_3 = 2.27954e-05;
+double e_vzFitParam_0 = 0.00868678;
+double e_vzFitParam_1 = 0.00230871;
+double e_vzFitParam_2 = 0.000171075;
+double e_vzFitParam_3 = 2.27624e-05;
 
