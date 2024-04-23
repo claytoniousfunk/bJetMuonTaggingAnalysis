d20f7e0657ef838917c9d1659f4b9e4b6a44bdb2
claytoniousfunk <cbennett0715@gmail.com>
2024-04-22 22:41:31 -0500

    updated vz fit params for mu7 PH

--- headers/fitParameters/vzFitParams_PH_mu7.h
+++ headers/fitParameters/vzFitParams_PH_mu7.h
@@ -4,12 +4,12 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 1.09367;
-double vzFitParam_1 = -0.00438119;
-double vzFitParam_2 = 0.000700935;
-double vzFitParam_3 = -0.000295203;
+double vzFitParam_0 = 0.981882;
+double vzFitParam_1 = -0.00839655;
+double vzFitParam_2 = 0.00153693;
+double vzFitParam_3 = -0.000100758;
 
-double e_vzFitParam_0 = 0.0281295;
-double e_vzFitParam_1 = 0.00769854;
-double e_vzFitParam_2 = 0.000728245;
-double e_vzFitParam_3 = 8.96752e-05;
+double e_vzFitParam_0 = 0.0172593;
+double e_vzFitParam_1 = 0.00468942;
+double e_vzFitParam_2 = 0.000464629;
+double e_vzFitParam_3 = 5.61348e-05;
