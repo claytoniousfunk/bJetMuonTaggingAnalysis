1d473a24f0be31e73cf9a251a1d6cb9c73576e0d
claytoniousfunk <cbennett0715@gmail.com>
2024-04-25 17:10:17 -0500

    updated measurements

--- headers/systematicsResults.h
+++ headers/systematicsResults.h
@@ -4,90 +4,136 @@
 
 // pp
 // -- mu5 
-double sys_lowerBound_pp_mu5_J2 = 0.0165;
-double sys_lowerBound_pp_mu5_J3 = 0.0049;
-double sys_lowerBound_pp_mu5_J4 = 0.0355;
+double sys_lowerBound_pp_mu5_J2 = 0.0249;
+double sys_lowerBound_pp_mu5_J3 = 0.0270;
+double sys_lowerBound_pp_mu5_J4 = 0.0213;
 // -- mu7
-double sys_lowerBound_pp_mu7_J2 = 0.0162;
-double sys_lowerBound_pp_mu7_J3 = 0.0139;
-double sys_lowerBound_pp_mu7_J4 = 0.0362;
+double sys_lowerBound_pp_mu7_J2 = 0.0201;
+double sys_lowerBound_pp_mu7_J3 = 0.0600;
+double sys_lowerBound_pp_mu7_J4 = 0.0569;
 // -- mu12
-double sys_lowerBound_pp_mu12_J2 = 0.0293;
-double sys_lowerBound_pp_mu12_J3 = 0.0279;
-double sys_lowerBound_pp_mu12_J4 = 0.0306;
+double sys_lowerBound_pp_mu12_J2 = 0.0318;
+double sys_lowerBound_pp_mu12_J3 = 0.0431;
+double sys_lowerBound_pp_mu12_J4 = 0.0328;
 
 // PbPb 30-90%
 // -- mu5 
-double sys_lowerBound_PbPb_mu5_C2_J2 = 0.0383;
-double sys_lowerBound_PbPb_mu5_C2_J3 = 0.0586;
-double sys_lowerBound_PbPb_mu5_C2_J4 = 0.0919;
+double sys_lowerBound_PbPb_mu5_C2_J2 = 0.0195;
+double sys_lowerBound_PbPb_mu5_C2_J3 = 0.0735;
+double sys_lowerBound_PbPb_mu5_C2_J4 = 0.1296;
 // -- mu7 
-double sys_lowerBound_PbPb_mu7_C2_J2 = 0.0205;
-double sys_lowerBound_PbPb_mu7_C2_J3 = 0.0440;
-double sys_lowerBound_PbPb_mu7_C2_J4 = 0.0457;
+double sys_lowerBound_PbPb_mu7_C2_J2 = 0.0384;
+double sys_lowerBound_PbPb_mu7_C2_J3 = 0.0390;
+double sys_lowerBound_PbPb_mu7_C2_J4 = 0.1794;
 // -- mu12
-double sys_lowerBound_PbPb_mu12_C2_J2 = 0.0286;
-double sys_lowerBound_PbPb_mu12_C2_J3 = 0.0268;
-double sys_lowerBound_PbPb_mu12_C2_J4 = 0.0602;
+double sys_lowerBound_PbPb_mu12_C2_J2 = 0.0512;
+double sys_lowerBound_PbPb_mu12_C2_J3 = 0.0462;
+double sys_lowerBound_PbPb_mu12_C2_J4 = 0.1030;
 
 // PbPb 0-30%
 // -- mu5 
-double sys_lowerBound_PbPb_mu5_C1_J2 = 0.0395;
-double sys_lowerBound_PbPb_mu5_C1_J3 = 0.0865;
-double sys_lowerBound_PbPb_mu5_C1_J4 = 0.0522;
+double sys_lowerBound_PbPb_mu5_C1_J2 = 0.0423;
+double sys_lowerBound_PbPb_mu5_C1_J3 = 0.0965;
+double sys_lowerBound_PbPb_mu5_C1_J4 = 0.0670;
 // -- mu7 
-double sys_lowerBound_PbPb_mu7_C1_J2 = 0.0691;
-double sys_lowerBound_PbPb_mu7_C1_J3 = 0.0560;
-double sys_lowerBound_PbPb_mu7_C1_J4 = 0.0326;
+double sys_lowerBound_PbPb_mu7_C1_J2 = 0.0389;
+double sys_lowerBound_PbPb_mu7_C1_J3 = 0.1282;
+double sys_lowerBound_PbPb_mu7_C1_J4 = 0.0729;
 // -- mu12 
-double sys_lowerBound_PbPb_mu12_C1_J2 = 0.2499;
-double sys_lowerBound_PbPb_mu12_C1_J3 = 0.0752;
-double sys_lowerBound_PbPb_mu12_C1_J4 = 0.0314;
+double sys_lowerBound_PbPb_mu12_C1_J2 = 0.0481;
+double sys_lowerBound_PbPb_mu12_C1_J3 = 0.0612;
+double sys_lowerBound_PbPb_mu12_C1_J4 = 0.0682;
 
 
 // ------------- c-fraction-variation systematics ------------------
 
 // PYTHIA
 // -- mu5
-double sys_cFraction_pp_mu5_J2 = 0.0032;
-double sys_cFraction_pp_mu5_J3 = 0.0094;
-double sys_cFraction_pp_mu5_J4 = 0.0200;
+double sys_cFraction_pp_mu5_J2 = 0.0040;
+double sys_cFraction_pp_mu5_J3 = 0.0118;
+double sys_cFraction_pp_mu5_J4 = 0.0208;
 // -- mu7
-double sys_cFraction_pp_mu7_J2 = 0.0017;
-double sys_cFraction_pp_mu7_J3 = 0.0056;
-double sys_cFraction_pp_mu7_J4 = 0.0145;
+double sys_cFraction_pp_mu7_J2 = 0.0055;
+double sys_cFraction_pp_mu7_J3 = 0.0137;
+double sys_cFraction_pp_mu7_J4 = 0.0313;
 // -- mu12
-double sys_cFraction_pp_mu12_J2 = 0.0001;
-double sys_cFraction_pp_mu12_J3 = 0.0005;
-double sys_cFraction_pp_mu12_J4 = 0.0063;
+double sys_cFraction_pp_mu12_J2 = 0.0039;
+double sys_cFraction_pp_mu12_J3 = 0.0052;
+double sys_cFraction_pp_mu12_J4 = 0.0119;
 
 // PYTHIA+HYDJET 30-90%
 // -- mu5
-double sys_cFraction_PbPb_mu5_C2_J2 = 0.0002;
-double sys_cFraction_PbPb_mu5_C2_J3 = 0.0112;
-double sys_cFraction_PbPb_mu5_C2_J4 = 0.0396;
+double sys_cFraction_PbPb_mu5_C2_J2 = 0.0028;
+double sys_cFraction_PbPb_mu5_C2_J3 = 0.0215;
+double sys_cFraction_PbPb_mu5_C2_J4 = 0.0519;
 // -- mu7
-double sys_cFraction_PbPb_mu7_C2_J2 = 0.0017;
-double sys_cFraction_PbPb_mu7_C2_J3 = 0.0063;
-double sys_cFraction_PbPb_mu7_C2_J4 = 0.0196;
+double sys_cFraction_PbPb_mu7_C2_J2 = 0.0015;
+double sys_cFraction_PbPb_mu7_C2_J3 = 0.0163;
+double sys_cFraction_PbPb_mu7_C2_J4 = 0.0254;
 // -- mu12
-double sys_cFraction_PbPb_mu12_C2_J2 = 0.0021;
-double sys_cFraction_PbPb_mu12_C2_J3 = 0.0009;
-double sys_cFraction_PbPb_mu12_C2_J4 = 0.0116;
+double sys_cFraction_PbPb_mu12_C2_J2 = 0.0037;
+double sys_cFraction_PbPb_mu12_C2_J3 = 0.0001;
+double sys_cFraction_PbPb_mu12_C2_J4 = 0.0135;
 
 // PYTHIA+HYDJET 0-30%
 // -- mu5
-double sys_cFraction_PbPb_mu5_C1_J2 = 0.0272;
-double sys_cFraction_PbPb_mu5_C1_J3 = 0.0082;
-double sys_cFraction_PbPb_mu5_C1_J4 = 0.0263;
+double sys_cFraction_PbPb_mu5_C1_J2 = 0.0148;
+double sys_cFraction_PbPb_mu5_C1_J3 = 0.0068;
+double sys_cFraction_PbPb_mu5_C1_J4 = 0.0314;
 // -- mu7
-double sys_cFraction_PbPb_mu7_C1_J2 = 0.0347;
-double sys_cFraction_PbPb_mu7_C1_J3 = 0.0104;
-double sys_cFraction_PbPb_mu7_C1_J4 = 0.0156;
+double sys_cFraction_PbPb_mu7_C1_J2 = 0.0088;
+double sys_cFraction_PbPb_mu7_C1_J3 = 0.0171;
+double sys_cFraction_PbPb_mu7_C1_J4 = 0.0258;
 // -- mu12
-double sys_cFraction_PbPb_mu12_C1_J2 = 0.0746;
-double sys_cFraction_PbPb_mu12_C1_J3 = 0.0039;
-double sys_cFraction_PbPb_mu12_C1_J4 = 0.0177;
+double sys_cFraction_PbPb_mu12_C1_J2 = 0.0079;
+double sys_cFraction_PbPb_mu12_C1_J3 = 0.0060;
+double sys_cFraction_PbPb_mu12_C1_J4 = 0.0174;
+
+
+
+// ------------- bGS-multiplier systematics ------------------
+
+// PYTHIA
+// -- mu5
+double sys_bGS_pp_mu5_J2 = 0.0128;
+double sys_bGS_pp_mu5_J3 = 0.0256;
+double sys_bGS_pp_mu5_J4 = 0.0368;
+// -- mu7
+double sys_bGS_pp_mu7_J2 = 0.0231;
+double sys_bGS_pp_mu7_J3 = 0.0455;
+double sys_bGS_pp_mu7_J4 = 0.0704;
+// -- mu12
+double sys_bGS_pp_mu12_J2 = 0.0074;
+double sys_bGS_pp_mu12_J3 = 0.0120;
+double sys_bGS_pp_mu12_J4 = 0.0198;
+
+// PYTHIA+HYDJET 30-90%
+// -- mu5
+double sys_bGS_PbPb_mu5_C2_J2 = 0.0110;
+double sys_bGS_PbPb_mu5_C2_J3 = 0.0230;
+double sys_bGS_PbPb_mu5_C2_J4 = 0.0451;
+// -- mu7
+double sys_bGS_PbPb_mu7_C2_J2 = 0.0117;
+double sys_bGS_PbPb_mu7_C2_J3 = 0.0324;
+double sys_bGS_PbPb_mu7_C2_J4 = 0.0876;
+// -- mu12
+double sys_bGS_PbPb_mu12_C2_J2 = 0.0014;
+double sys_bGS_PbPb_mu12_C2_J3 = 0.0084;
+double sys_bGS_PbPb_mu12_C2_J4 = 0.0225;
+
+// PYTHIA+HYDJET 0-30%
+// -- mu5
+double sys_bGS_PbPb_mu5_C1_J2 = 0.0090;
+double sys_bGS_PbPb_mu5_C1_J3 = 0.0170;
+double sys_bGS_PbPb_mu5_C1_J4 = 0.0392;
+// -- mu7
+double sys_bGS_PbPb_mu7_C1_J2 = 0.0108;
+double sys_bGS_PbPb_mu7_C1_J3 = 0.0263;
+double sys_bGS_PbPb_mu7_C1_J4 = 0.0634;
+// -- mu12
+double sys_bGS_PbPb_mu12_C1_J2 = 0.0013;
+double sys_bGS_PbPb_mu12_C1_J3 = 0.0053;
+double sys_bGS_PbPb_mu12_C1_J4 = 0.0250;
 
 
 
@@ -110,31 +156,31 @@ double sys_bEnhance_pp_mu12_J4 = 0.0;
 
 // PbPb 30-90%
 // -- mu5
-double sys_bEnhance_PbPb_mu5_C2_J2 = 0.0289;
-double sys_bEnhance_PbPb_mu5_C2_J3 = 0.0284;
-double sys_bEnhance_PbPb_mu5_C2_J4 = 0.0383;
+double sys_bEnhance_PbPb_mu5_C2_J2 = 0.0436;
+double sys_bEnhance_PbPb_mu5_C2_J3 = 0.0130;
+double sys_bEnhance_PbPb_mu5_C2_J4 = 0.0160;
 // -- mu7
-double sys_bEnhance_PbPb_mu7_C2_J2 = 0.0095;
-double sys_bEnhance_PbPb_mu7_C2_J3 = 0.0306;
-double sys_bEnhance_PbPb_mu7_C2_J4 = 0.0329;
+double sys_bEnhance_PbPb_mu7_C2_J2 = 0.0501;
+double sys_bEnhance_PbPb_mu7_C2_J3 = 0.0308;
+double sys_bEnhance_PbPb_mu7_C2_J4 = 0.0817;
 // -- mu12
-double sys_bEnhance_PbPb_mu12_C2_J2 = 0.0118;
-double sys_bEnhance_PbPb_mu12_C2_J3 = 0.0524;
-double sys_bEnhance_PbPb_mu12_C2_J4 = 0.0429;
+double sys_bEnhance_PbPb_mu12_C2_J2 = 0.0551;
+double sys_bEnhance_PbPb_mu12_C2_J3 = 0.0135;
+double sys_bEnhance_PbPb_mu12_C2_J4 = 0.0054;
 
 // PbPb 0-30%
 // -- mu5
-double sys_bEnhance_PbPb_mu5_C1_J2 = 0.2281;
-double sys_bEnhance_PbPb_mu5_C1_J3 = 0.1688;
-double sys_bEnhance_PbPb_mu5_C1_J4 = 0.1059;
+double sys_bEnhance_PbPb_mu5_C1_J2 = 0.0870;
+double sys_bEnhance_PbPb_mu5_C1_J3 = 0.0477;
+double sys_bEnhance_PbPb_mu5_C1_J4 = 0.0291;
 // -- mu7
-double sys_bEnhance_PbPb_mu7_C1_J2 = 0.2301;
-double sys_bEnhance_PbPb_mu7_C1_J3 = 0.1387;
-double sys_bEnhance_PbPb_mu7_C1_J4 = 0.0739;
+double sys_bEnhance_PbPb_mu7_C1_J2 = 0.3645;
+double sys_bEnhance_PbPb_mu7_C1_J3 = 0.2529;
+double sys_bEnhance_PbPb_mu7_C1_J4 = 0.0956;
 // -- mu12
-double sys_bEnhance_PbPb_mu12_C1_J2 = 0.2135;
-double sys_bEnhance_PbPb_mu12_C1_J3 = 0.1614;
-double sys_bEnhance_PbPb_mu12_C1_J4 = 0.0666;
+double sys_bEnhance_PbPb_mu12_C1_J2 = 0.0966;
+double sys_bEnhance_PbPb_mu12_C1_J3 = 0.0828;
+double sys_bEnhance_PbPb_mu12_C1_J4 = 0.0423;
 
 
 
@@ -158,76 +204,76 @@ double sys_cEnhance_pp_mu12_J4 = 0.0;
 
 // PbPb 30-90%
 // -- mu5
-double sys_cEnhance_PbPb_mu5_C2_J2 = 0.0008;
-double sys_cEnhance_PbPb_mu5_C2_J3 = 0.0026;
-double sys_cEnhance_PbPb_mu5_C2_J4 = 0.0115;
+double sys_cEnhance_PbPb_mu5_C2_J2 = 0.0226;
+double sys_cEnhance_PbPb_mu5_C2_J3 = 0.0113;
+double sys_cEnhance_PbPb_mu5_C2_J4 = 0.0642;
 // -- mu7
-double sys_cEnhance_PbPb_mu7_C2_J2 = 0.0004;
-double sys_cEnhance_PbPb_mu7_C2_J3 = 0.0026;
-double sys_cEnhance_PbPb_mu7_C2_J4 = 0.0071;
+double sys_cEnhance_PbPb_mu7_C2_J2 = 0.0155;
+double sys_cEnhance_PbPb_mu7_C2_J3 = 0.0098;
+double sys_cEnhance_PbPb_mu7_C2_J4 = 0.0192;
 // -- mu12
-double sys_cEnhance_PbPb_mu12_C2_J2 = 0.0013;
-double sys_cEnhance_PbPb_mu12_C2_J3 = 0.0040;
-double sys_cEnhance_PbPb_mu12_C2_J4 = 0.0054;
+double sys_cEnhance_PbPb_mu12_C2_J2 = 0.0088;
+double sys_cEnhance_PbPb_mu12_C2_J3 = 0.0136;
+double sys_cEnhance_PbPb_mu12_C2_J4 = 0.0087;
 
 // PbPb 0-30%
 // -- mu5
-double sys_cEnhance_PbPb_mu5_C1_J2 = 0.0004;
-double sys_cEnhance_PbPb_mu5_C1_J3 = 0.0003;
-double sys_cEnhance_PbPb_mu5_C1_J4 = 0.0036;
+double sys_cEnhance_PbPb_mu5_C1_J2 = 0.0130;
+double sys_cEnhance_PbPb_mu5_C1_J3 = 0.0171;
+double sys_cEnhance_PbPb_mu5_C1_J4 = 0.0033;
 // -- mu7
-double sys_cEnhance_PbPb_mu7_C1_J2 = 0.0000;
-double sys_cEnhance_PbPb_mu7_C1_J3 = 0.0004;
-double sys_cEnhance_PbPb_mu7_C1_J4 = 0.0021;
+double sys_cEnhance_PbPb_mu7_C1_J2 = 0.0082;
+double sys_cEnhance_PbPb_mu7_C1_J3 = 0.0377;
+double sys_cEnhance_PbPb_mu7_C1_J4 = 0.0304;
 // -- mu12
-double sys_cEnhance_PbPb_mu12_C1_J2 = 0.0033;
-double sys_cEnhance_PbPb_mu12_C1_J3 = 0.0003;
-double sys_cEnhance_PbPb_mu12_C1_J4 = 0.0005;
+double sys_cEnhance_PbPb_mu12_C1_J2 = 0.0267;
+double sys_cEnhance_PbPb_mu12_C1_J3 = 0.0045;
+double sys_cEnhance_PbPb_mu12_C1_J4 = 0.0198;
 
 
 // ------------- JER-smear systematics ------------------
 
 // PYTHIA
 // -- mu5
-double sys_JERsmear_pp_mu5_J2 = 0.0162;
-double sys_JERsmear_pp_mu5_J3 = 0.0098;
-double sys_JERsmear_pp_mu5_J4 = 0.0231;
+double sys_JERsmear_pp_mu5_J2 = 0.0002;
+double sys_JERsmear_pp_mu5_J3 = 0.0043;
+double sys_JERsmear_pp_mu5_J4 = 0.0130;
 // -- mu7
-double sys_JERsmear_pp_mu7_J2 = 0.0068;
-double sys_JERsmear_pp_mu7_J3 = 0.0117;
-double sys_JERsmear_pp_mu7_J4 = 0.0084;
+double sys_JERsmear_pp_mu7_J2 = 0.0478;
+double sys_JERsmear_pp_mu7_J3 = 0.0041;
+double sys_JERsmear_pp_mu7_J4 = 0.0263;
 // -- mu12
-double sys_JERsmear_pp_mu12_J2 = 0.0110;
-double sys_JERsmear_pp_mu12_J3 = 0.0057;
-double sys_JERsmear_pp_mu12_J4 = 0.0112;
+double sys_JERsmear_pp_mu12_J2 = 0.0187;
+double sys_JERsmear_pp_mu12_J3 = 0.0119;
+double sys_JERsmear_pp_mu12_J4 = 0.0161;
 
 // PYTHIAHYDJET 30-90%
 // -- mu5
-double sys_JERsmear_PbPb_mu5_C2_J2 = 0.0284;
-double sys_JERsmear_PbPb_mu5_C2_J3 = 0.0584;
-double sys_JERsmear_PbPb_mu5_C2_J4 = 0.0687;
+double sys_JERsmear_PbPb_mu5_C2_J2 = 0.0247;
+double sys_JERsmear_PbPb_mu5_C2_J3 = 0.0061;
+double sys_JERsmear_PbPb_mu5_C2_J4 = 0.0505;
 // -- mu7
-double sys_JERsmear_PbPb_mu7_C2_J2 = 0.0210;
-double sys_JERsmear_PbPb_mu7_C2_J3 = 0.0312;
-double sys_JERsmear_PbPb_mu7_C2_J4 = 0.0127;
+double sys_JERsmear_PbPb_mu7_C2_J2 = 0.1234;
+double sys_JERsmear_PbPb_mu7_C2_J3 = 0.0105;
+double sys_JERsmear_PbPb_mu7_C2_J4 = 0.1097;
 // -- mu12
-double sys_JERsmear_PbPb_mu12_C2_J2 = 0.0296;
-double sys_JERsmear_PbPb_mu12_C2_J3 = 0.0041;
-double sys_JERsmear_PbPb_mu12_C2_J4 = 0.0064;
+double sys_JERsmear_PbPb_mu12_C2_J2 = 0.0346;
+double sys_JERsmear_PbPb_mu12_C2_J3 = 0.0204;
+double sys_JERsmear_PbPb_mu12_C2_J4 = 0.0339;
 
 // PbPb 0-30%
 // -- mu5
-double sys_JERsmear_PbPb_mu5_C1_J2 = 0.0531;
-double sys_JERsmear_PbPb_mu5_C1_J3 = 0.0164;
-double sys_JERsmear_PbPb_mu5_C1_J4 = 0.0188;
+double sys_JERsmear_PbPb_mu5_C1_J2 = 0.0855;
+double sys_JERsmear_PbPb_mu5_C1_J3 = 0.1390;
+double sys_JERsmear_PbPb_mu5_C1_J4 = 0.0132;
 // -- mu7
-double sys_JERsmear_PbPb_mu7_C1_J2 = 0.0755;
-double sys_JERsmear_PbPb_mu7_C1_J3 = 0.0050;
-double sys_JERsmear_PbPb_mu7_C1_J4 = 0.0248;
+double sys_JERsmear_PbPb_mu7_C1_J2 = 0.1699;
+double sys_JERsmear_PbPb_mu7_C1_J3 = 0.0663;
+double sys_JERsmear_PbPb_mu7_C1_J4 = 0.0518;
 // -- mu12
-double sys_JERsmear_PbPb_mu12_C1_J2 = 0.0353;
-double sys_JERsmear_PbPb_mu12_C1_J3 = 0.0447;
-double sys_JERsmear_PbPb_mu12_C1_J4 = 0.0172;
+double sys_JERsmear_PbPb_mu12_C1_J2 = 0.0756;
+double sys_JERsmear_PbPb_mu12_C1_J3 = 0.0018;
+double sys_JERsmear_PbPb_mu12_C1_J4 = 0.0060;
 
 
 // ------------- centrality-shift systematics ------------------
@@ -275,7 +321,93 @@ double sys_centShift_PbPb_mu12_C1_J3 = 0.0;
 double sys_centShift_PbPb_mu12_C1_J4 = 0.0;
 
 
+// ------------- JEU-shift-up systematics ------------------
 
+// PYTHIA
+// -- mu5
+double sys_JEUshiftUp_pp_mu5_J2 = 0.0;
+double sys_JEUshiftUp_pp_mu5_J3 = 0.0;
+double sys_JEUshiftUp_pp_mu5_J4 = 0.0;
+// -- mu7
+double sys_JEUshiftUp_pp_mu7_J2 = 0.0218;
+double sys_JEUshiftUp_pp_mu7_J3 = 0.0168;
+double sys_JEUshiftUp_pp_mu7_J4 = 0.0205;
+// -- mu12
+double sys_JEUshiftUp_pp_mu12_J2 = 0.0080;
+double sys_JEUshiftUp_pp_mu12_J3 = 0.0075;
+double sys_JEUshiftUp_pp_mu12_J4 = 0.0026;
+
+// PYTHIAHYDJET 30-90%
+// -- mu5
+double sys_JEUshiftUp_PbPb_mu5_C2_J2 = 0.0;
+double sys_JEUshiftUp_PbPb_mu5_C2_J3 = 0.0;
+double sys_JEUshiftUp_PbPb_mu5_C2_J4 = 0.0;
+// -- mu7
+double sys_JEUshiftUp_PbPb_mu7_C2_J2 = 0.0268;
+double sys_JEUshiftUp_PbPb_mu7_C2_J3 = 0.0090;
+double sys_JEUshiftUp_PbPb_mu7_C2_J4 = 0.0333;
+// -- mu12
+double sys_JEUshiftUp_PbPb_mu12_C2_J2 = 0.0058;
+double sys_JEUshiftUp_PbPb_mu12_C2_J3 = 0.0091;
+double sys_JEUshiftUp_PbPb_mu12_C2_J4 = 0.0236;
+
+// PbPb 0-30%
+// -- mu5
+double sys_JEUshiftUp_PbPb_mu5_C1_J2 = 0.0;
+double sys_JEUshiftUp_PbPb_mu5_C1_J3 = 0.0;
+double sys_JEUshiftUp_PbPb_mu5_C1_J4 = 0.0;
+// -- mu7
+double sys_JEUshiftUp_PbPb_mu7_C1_J2 = 0.0652;
+double sys_JEUshiftUp_PbPb_mu7_C1_J3 = 0.0142;
+double sys_JEUshiftUp_PbPb_mu7_C1_J4 = 0.0231;
+// -- mu12
+double sys_JEUshiftUp_PbPb_mu12_C1_J2 = 0.0381;
+double sys_JEUshiftUp_PbPb_mu12_C1_J3 = 0.0056;
+double sys_JEUshiftUp_PbPb_mu12_C1_J4 = 0.0083;
+
+// ------------- JEU-shift-down systematics ------------------
+
+// PYTHIA
+// -- mu5
+double sys_JEUshiftDown_pp_mu5_J2 = 0.0;
+double sys_JEUshiftDown_pp_mu5_J3 = 0.0;
+double sys_JEUshiftDown_pp_mu5_J4 = 0.0;
+// -- mu7
+double sys_JEUshiftDown_pp_mu7_J2 = 0.0300;
+double sys_JEUshiftDown_pp_mu7_J3 = 0.0116;
+double sys_JEUshiftDown_pp_mu7_J4 = 0.0226;
+// -- mu12
+double sys_JEUshiftDown_pp_mu12_J2 = 0.0104;
+double sys_JEUshiftDown_pp_mu12_J3 = 0.0206;
+double sys_JEUshiftDown_pp_mu12_J4 = 0.0018;
+
+// PYTHIAHYDJET 30-90%
+// -- mu5
+double sys_JEUshiftDown_PbPb_mu5_C2_J2 = 0.0;
+double sys_JEUshiftDown_PbPb_mu5_C2_J3 = 0.0;
+double sys_JEUshiftDown_PbPb_mu5_C2_J4 = 0.0;
+// -- mu7
+double sys_JEUshiftDown_PbPb_mu7_C2_J2 = 0.0280;
+double sys_JEUshiftDown_PbPb_mu7_C2_J3 = 0.0134;
+double sys_JEUshiftDown_PbPb_mu7_C2_J4 = 0.0265;
+// -- mu12
+double sys_JEUshiftDown_PbPb_mu12_C2_J2 = 0.0064;
+double sys_JEUshiftDown_PbPb_mu12_C2_J3 = 0.0147;
+double sys_JEUshiftDown_PbPb_mu12_C2_J4 = 0.0008;
+
+// PbPb 0-30%
+// -- mu5
+double sys_JEUshiftDown_PbPb_mu5_C1_J2 = 0.0;
+double sys_JEUshiftDown_PbPb_mu5_C1_J3 = 0.0;
+double sys_JEUshiftDown_PbPb_mu5_C1_J4 = 0.0;
+// -- mu7
+double sys_JEUshiftDown_PbPb_mu7_C1_J2 = 0.0267;
+double sys_JEUshiftDown_PbPb_mu7_C1_J3 = 0.0793;
+double sys_JEUshiftDown_PbPb_mu7_C1_J4 = 0.0065;
+// -- mu12
+double sys_JEUshiftDown_PbPb_mu12_C1_J2 = 0.0182;
+double sys_JEUshiftDown_PbPb_mu12_C1_J3 = 0.0034;
+double sys_JEUshiftDown_PbPb_mu12_C1_J4 = 0.0172;
 
 
 
