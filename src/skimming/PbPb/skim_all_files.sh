186aa14f71b9f1f3467d20285099391534774bd9
claytoniousfunk <cbennett0715@gmail.com>
2024-05-03 09:29:29 -0500

    edit script

--- src/skimming/PbPb/skim_all_files.sh
+++ src/skimming/PbPb/skim_all_files.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-STARTGROUP=0
-ENDGROUP=0
-j=0
 
-for i in {1..359}
+for i in {1..7203}
 
 do
     
-    let j=$i-1
-
-    if [ $i -eq 1 ] ; then
-	let STARTGROUP=0
-	let ENDGROUP=20
-
-    elif [ $i -ne 359 ] ; then
-	let STARTGROUP=20*$j+1
-	let ENDGROUP=20*i
-    
-    elif [ $i -eq 359 ] ; then
-	let STARTGROUP=20*$j+1
-	let ENDGROUP=7176
-    fi
-
-    root -l -q "PbPb_skim.C($i,$STARTGROUP,$ENDGROUP)"
+    root -l -q "PbPb_skim_simple.C($i)"
 
 done
