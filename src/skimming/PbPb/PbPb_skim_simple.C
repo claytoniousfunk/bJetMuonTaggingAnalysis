37631c0cd84bb522bb56b3933a818471bb88b700
claytoniousfunk <cbennett0715@gmail.com>
2024-05-06 11:36:11 -0500

    fix endfile variable

--- src/skimming/PbPb/PbPb_skim_simple.C
+++ src/skimming/PbPb/PbPb_skim_simple.C
@@ -12,7 +12,7 @@ void PbPb_skim_simple(int group = 1){
   std::string filename;
   Int_t ifile = 0;
 
-  int endfile = 6033;
+  int endfile = 7176;
 
   TFile *old_file;
   TString filename_string = "";
