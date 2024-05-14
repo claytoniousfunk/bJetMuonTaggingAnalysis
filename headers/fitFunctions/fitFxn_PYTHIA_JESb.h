284602998fbee3c08fde672644a39019adf3f030
claytoniousfunk <cbennett0715@gmail.com>
2024-05-14 12:07:16 -0500

    updated JESb fit function & parameters

--- headers/fitFunctions/fitFxn_PYTHIA_JESb.h
+++ headers/fitFunctions/fitFxn_PYTHIA_JESb.h
@@ -1,5 +1,5 @@
 void loadFitFxn_PYTHIA_JESb(){
-  fitFxn_PYTHIA_JESb = new TF1("fitFxn_PYTHIA_JESb","[0]+[1]*x+[2]*x*x+[3]*x*x*x",0,500);
+  fitFxn_PYTHIA_JESb = new TF1("fitFxn_PYTHIA_JESb","([0]+[1]*x)/([2]+[3]*x)",0,500);
   fitFxn_PYTHIA_JESb->SetParameter(0,JESbFitParam_0);
   fitFxn_PYTHIA_JESb->SetParameter(1,JESbFitParam_1);
   fitFxn_PYTHIA_JESb->SetParameter(2,JESbFitParam_2);
