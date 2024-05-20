4fcb06a257b0edd029fea80d8e90b791e919f9bc
claytoniousfunk <cbennett0715@gmail.com>
2024-05-20 16:53:35 -0500

    configure for BJet skim

--- src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
+++ src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
@@ -8,17 +8,17 @@ void PYTHIAHYDJET_skim_simple(int group = 1){
   // output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_DiJet_withGS_withWTA_2/PYTHIAHYDJET_DiJet_skim_output_%i",group);
 
   // MuJet, N = 2923
-  in_file_name = "../../../fileNames/fileNames_PYTHIAHYDJET_MuJet_withGS_withWTA.txt";
-  output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_MuJet_withGS_withWTA_2/PYTHIAHYDJET_MuJet_skim_output_%i",group);
+  // in_file_name = "../../../fileNames/fileNames_PYTHIAHYDJET_MuJet_withGS_withWTA.txt";
+  // output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_MuJet_withGS_withWTA_2/PYTHIAHYDJET_MuJet_skim_output_%i",group);
 
   // MuJet, N = 4257
-  // in_file_name = "../../../fileNames/fileNames_PYTHIAHYDJET_BJet_withGS_withWTA.txt";
-  // output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_BJet_withGS_withWTA_2/PYTHIAHYDJET_BJet_skim_output_%i",group);
+  in_file_name = "../../../fileNames/fileNames_PYTHIAHYDJET_BJet_withGS_withWTA.txt";
+  output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIAHYDJET_BJet_withGS_withWTA_2/PYTHIAHYDJET_BJet_skim_output_%i",group);
 
 
 
 
-  int endfile = 6884;
+  int endfile = 4257;
 
 
 
