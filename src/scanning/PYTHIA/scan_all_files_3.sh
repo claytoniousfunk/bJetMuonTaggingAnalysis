06262d65194e6a1bc92ef1d64d40fa1addb8d877
claytoniousfunk <cbennett0715@gmail.com>
2024-05-20 23:18:53 -0500

    back to normal scan

--- src/scanning/PYTHIA/scan_all_files_3.sh
+++ src/scanning/PYTHIA/scan_all_files_3.sh
@@ -3,12 +3,12 @@
 # N = 1193 for PYTHIA DiJet
 # N = 1262 for PYTHIA DiJet response (noRecoJetPtCut)
 
-for i in {1001..1262}
+for i in {1001..1193}
 
 do
 
-    #root -l -q "PYTHIA_scan.C($i)"
-    root -l -q "PYTHIA_scan_response.C($i)"
+    root -l -q "PYTHIA_scan.C($i)"
+    #root -l -q "PYTHIA_scan_response.C($i)"
 
 done
 
