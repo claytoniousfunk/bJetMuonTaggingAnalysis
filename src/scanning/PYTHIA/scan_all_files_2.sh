477cd88240399118a0fb409060b0630f8ea37023
claytoniousfunk <cbennett0715@gmail.com>
2024-06-04 10:54:23 -0500

    run response code

--- src/scanning/PYTHIA/scan_all_files_2.sh
+++ src/scanning/PYTHIA/scan_all_files_2.sh
@@ -6,8 +6,8 @@ for i in {501..1000}
 
 do
 
-    root -l -q "PYTHIA_scan.C($i)"
-    #root -l -q "PYTHIA_scan_response.C($i)"
+    #root -l -q "PYTHIA_scan.C($i)"
+    root -l -q "PYTHIA_scan_response.C($i)"
 
 done
 
