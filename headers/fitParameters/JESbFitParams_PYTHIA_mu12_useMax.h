884a2cc32c772f09b5e0bcfbadf29cf00ebd515a
claytoniousfunk <cbennett0715@gmail.com>
2024-06-11 14:35:26 -0500

    new params using max instead of mu from gauss fit

---
+++ headers/fitParameters/JESbFitParams_PYTHIA_mu12_useMax.h
@@ -0,0 +1,6 @@
+double JESbFitParam_0 = 0.966856;
+double JESbFitParam_1 = 0.000101645;
+double JESbFitParam_2 = -2.76202e-07;
+double JESbFitParam_3 = 1.0255;
+double JESbFitParam_4 = 4.44779e-07;
+double JESbFitParam_5 = -2.18396e-07;
