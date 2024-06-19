08d22778d04b9a8b7d8246539a0da1394183635e
claytoniousfunk <cbennett0715@gmail.com>
2024-06-19 10:53:53 -0500

    rearrange

--- src/scanning/PYTHIA/PYTHIA_scan.C
+++ src/scanning/PYTHIA/PYTHIA_scan.C
@@ -177,7 +177,17 @@ TH2D *h_genJetPt_pthat;
 TH2D *h_recoGenDr_flavor[NJetPtIndices];
 TH2D *h_recoGenDpt_flavor[NJetPtIndices];
 
+TFile *f_neutrino_energy_fraction_map;
+TH2D *neutrino_energy_fraction_map;
+TH1D *neutrino_energy_fraction_map_proj;
+
 void PYTHIA_scan(int group = 1){
+
+  f_neutrino_energy_fraction_map = TFile::Open("/eos/cms/store/group/phys_heavyions/cbennett/maps/neutrino_energy_fraction_map.root");
+  neutrino_energy_fraction_map;
+  neutrino_energy_fraction_map_proj;
+  f_neutrino_energy_fraction_map->GetObject("neutrino_energy_fraction_map",neutrino_energy_fraction_map);
+  f_neutrino_energy_fraction_map->Close();
 
   TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIA_DiJet_withGS/PYTHIA_DiJet_skim_output_%i.root",group);
   TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_mu12_tight_pTmu-14_pThat-30_removeHYDJETjets_leadingXjetDump_jetPtReweight_vzReweight_bJetNeutrinoEnergyShift/PYTHIA_DiJet_scan_output_%i.root",group);
@@ -371,11 +381,7 @@ void PYTHIA_scan(int group = 1){
   }
 
 
-  TFile *f_neutrino_energy_fraction_map = TFile::Open("/eos/cms/store/group/phys_heavyions/cbennett/maps/neutrino_energy_fraction_map.root");
-  TH2D *neutrino_energy_fraction_map;
-  TH1D *neutrino_energy_fraction_map_proj;
-  f_neutrino_energy_fraction_map->GetObject("neutrino_energy_fraction_map",neutrino_energy_fraction_map);
-  f_neutrino_energy_fraction_map->Close();
+  
   
 
   TFile *f = TFile::Open(input);
