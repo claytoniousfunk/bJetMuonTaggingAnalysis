eefe1d44b412a65999c482c2cd7247a2d9f31be5
claytoniousfunk <cbennett0715@gmail.com>
2024-06-25 20:06:28 -0500

    bJetEnergyShift newFitMethod

--- src/scanning/PYTHIA/PYTHIA_scan.C
+++ src/scanning/PYTHIA/PYTHIA_scan.C
@@ -51,7 +51,8 @@
 // JESb fit params
 //#include "../../../headers/fitParameters/JESbFitParams_PYTHIA_mu7.h"
 //#include "../../../headers/fitParameters/JESbFitParams_PYTHIA_mu12.h"
-#include "../../../headers/fitParameters/JESbFitParams_PYTHIA_mu12_useMax.h"
+//#include "../../../headers/fitParameters/JESbFitParams_PYTHIA_mu12_useMax.h"
+#include "../../../headers/fitParameters/JESbFitParams_PYTHIA_mu12_newFitMethod.h"
 // bJetNeutrinoEnergy fit params
 #include "../../../headers/fitParameters/bJetNeutrinoEnergyFitParams_PYTHIA_mu12.h"
 
@@ -183,8 +184,8 @@ void PYTHIA_scan(int group = 1){
 
 
   //TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIA_DiJet_withGS/PYTHIA_DiJet_skim_output_%i.root",group);
-  TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_skim_PYTHIA_DiJet_withGS_withNeutrinos/PYTHIA_DiJet_skim_output_%i.root",group);
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_withNeutrinos_mu12_tight_pTmu-14_pThat-30_removeHYDJETjets_leadingXjetDump_jetPtReweight_vzReweight/PYTHIA_DiJet_scan_output_%i.root",group);
+  TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_skim_PYTHIA_DiJet_withGS/PYTHIA_DiJet_skim_output_%i.root",group);
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_mu12_tight_pTmu-14_pThat-30_removeHYDJETjets_leadingXjetDump_jetPtReweight_vzReweight_bJetEnergyShift_newFitMethod/PYTHIA_DiJet_scan_output_%i.root",group);
 
 
   printIntroduction_PYTHIA_scan_V3p7();
