c839e68f55cb80ad4af8c25e7c2edfb996375a69
claytoniousfunk <cbennett0715@gmail.com>
2024-06-25 20:08:37 -0500

    new fit params

---
+++ headers/fitParameters/JESbFitParams_PYTHIA_mu12_newFitMethod.h
@@ -0,0 +1,8 @@
+// mu-tagged bJet fit params
+double JESbFitParam_0 = 0.938888;
+double JESbFitParam_1 = 6.71347e-05;
+double JESbFitParam_2 = 0.0;
+// inclusive allJet fit params
+double JESbFitParam_3 = 1.10034;
+double JESbFitParam_4 = -0.000416378;
+double JESbFitParam_5 = 6.55225e-07;
