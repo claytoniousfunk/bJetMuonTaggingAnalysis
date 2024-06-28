c8887873b8d0ea9ff261025c54b8b6f4900106bb
claytoniousfunk <cbennett0715@gmail.com>
2024-06-28 09:23:43 -0500

    implement genJetPt smearing

--- src/scanning/PYTHIA/PYTHIA_scan.C
+++ src/scanning/PYTHIA/PYTHIA_scan.C
@@ -185,7 +185,7 @@ void PYTHIA_scan(int group = 1){
 
   //TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIA_DiJet_withGS/PYTHIA_DiJet_skim_output_%i.root",group);
   TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/output_PYTHIA_DiJet_withGS/PYTHIA_DiJet_skim_output_%i.root",group);
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_mu12_tight_pTmu-14_pThat-30_removeHYDJETjets_leadingXjetDump_jetPtReweight_vzReweight_bJetEnergyShift_newFitMethod/PYTHIA_DiJet_scan_output_%i.root",group);
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_withGS_mu12_tight_pTmu-14_pThat-30_removeHYDJETjets_leadingXjetDump_jetPtReweight_vzReweight_genJetPtSmear/PYTHIA_DiJet_scan_output_%i.root",group);
 
 
   printIntroduction_PYTHIA_scan_V3p7();
@@ -1092,7 +1092,20 @@ void PYTHIA_scan(int group = 1){
       double genJetPhi_i = em->genjetphi[i]; // recoJetPhi
 
       double muPtRel_i = -1.0;
-		
+
+
+      // apply gen-pT smear
+      double mu = 1.0;
+      double sigma = 0.2;
+      double smear = 0.0;
+
+      if(apply_genJetPt_smear){
+	sigma = 0.663*JER_fxn->Eval(); // apply a 20% smear
+	smear = randomGenerator->Gaus(mu,sigma);
+	genJetPt_i = genJetPt_i * smear;
+      }
+
+      
       if(TMath::Abs(genJetEta_i) > 1.6 || genJetPt_i < jetPtCut) continue;
 
       if(genJetPt_i > leadingGenJetPt) leadingGenJetPt = genJetPt_i;
