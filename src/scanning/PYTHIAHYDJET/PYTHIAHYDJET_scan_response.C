e38a5f334767a2481aac2668444bbe3dbb9b1a79
claytoniousfunk <cbennett0715@gmail.com>
2024-07-08 12:00:57 -0500

    get rid of extraneous headers

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan_response.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan_response.C
@@ -96,52 +96,6 @@ TF1 *fitFxn_hiBin, *fitFxn_vz, *fitFxn_jetPt;
 #include "../../../headers/functions/jet_filter/remove_HYDJET_jet.h"
 
 
-// event map
-#include "../../../../eventMap/eventMap.h"
-// jet corrector
-#include "../../../../JetEnergyCorrections/JetCorrector.h"
-// jet uncertainty
-#include "../../../../JetEnergyCorrections/JetUncertainty.h"
-// general analysis variables
-#include "../../../../headers/AnalysisSetupV2p1.h"
-// vz-fit parameters
-#include "../../../../headers/fitParameters/vzFitParams_PH.h"
-// hiBin-fit parameters
-#include "../../../../headers/fitParameters/hiBinFitParams.h"
-
-TF1 *fitFxn_hiBin, *fitFxn_vz;
-// vz-fit function
-#include "../../../../headers/fitFunctions/fitFxn_vz_PH.h"
-// hiBin-fit function
-#include "../../../../headers/fitFunctions/fitFxn_hiBin.h"
-
-// eta-phi mask function
-#include "../../../../headers/functions/etaPhiMask.h"
-// getDr function
-#include "../../../../headers/functions/getDr.h"
-// getJetPtBin function
-#include "../../../../headers/functions/getJetPtBin.h"
-// getCentBin function
-#include "../../../../headers/functions/getCentBin_v2.h"
-// getPtRel function
-#include "../../../../headers/functions/getPtRel.h"
-// isQualityMuon_hybridSoft function
-#include "../../../../headers/functions/isQualityMuon_hybridSoft.h"
-// isQualityMuon_tight function
-#include "../../../../headers/functions/isQualityMuon_tight.h"
-// isWDecayMuon function
-#include "../../../../headers/functions/isWDecayMuon.h"
-// triggerIsOn function
-#include "../../../../headers/functions/triggerIsOn.h"
-// pthat filter function
-#include "../../../../headers/functions/passesLeadingGenJetPthatFilter.h"
-// print introduction
-#include "../../../../headers/introductions/printIntroduction_PYTHIAHYDJET_response_v2.h"
-// analysis config
-#include "../../../../headers/config/config_PYTHIAHYDJET.h"
-// read config
-#include "../../../../headers/config/readConfig.h"
-
 
 void PYTHIAHYDJET_scan_response(int group = 1){
 
