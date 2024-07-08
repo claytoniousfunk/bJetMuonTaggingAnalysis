d37b2afc65c8ade20e746fc89ad63b986979b05c
claytoniousfunk <cbennett0715@gmail.com>
2024-07-07 21:49:25 -0500

    new scan scripts for P+H

---
+++ src/scanning/PYTHIAHYDJET/scan_nominal_scripts/scan_script_13.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# N = 7202 for PH DiJet
+# N = 3472 for PH DiJet w/ WTA
+# N = 4257 for PH BJet
+# N = 2923 for PH MuJet
+# N = 6190 for PH DiJet additionalMC
+
+for i in {6001..6500}
+
+do
+
+    root -l  -b -q "PYTHIAHYDJET_scan.C($i)"
+
+done
+
+      
+
+
