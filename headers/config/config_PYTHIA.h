53c8223d27c676f7e0d8e5bea14c64eb1374a041
claytoniousfunk <cbennett0715@gmail.com>
2024-07-11 17:11:35 -0500

    incl jet scan

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -7,7 +7,7 @@ TString hltString = "hltTree";
 const int NeventFilters = 3;
 std::string eventFilters[NeventFilters] = {"pPAprimaryVertexFilter","HBHENoiseFilterResultRun2Loose","pBeamScrapingFilter"};
 // options for systematic studies
-bool apply_JER_smear = true;
+bool apply_JER_smear = false;
 bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
