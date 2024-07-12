8472b4f67332ce656674867a526ab135735e562c
claytoniousfunk <cbennett0715@gmail.com>
2024-07-12 16:09:16 -0500

    back to pThat-15

--- headers/AnalysisSetupV2p1.h
+++ headers/AnalysisSetupV2p1.h
@@ -63,8 +63,8 @@ double nuPtCut = 0.0;
 double nuPtMaxCut = 1000.0;
 
 //const double pthatcut = 15.0;
-//const double pthatcut = 25.0;
-const double pthatcut = 30.0;
+const double pthatcut = 25.0;
+//const double pthatcut = 30.0;
 //const double pthatcut = 40.0;
 //const double pthatcut = 50.0;
 
