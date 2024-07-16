d9ee03ad2021fb87fc531c5dbd5d76308ccb9035
claytoniousfunk <cbennett0715@gmail.com>
2024-07-16 08:42:21 -0500

    added doJERCorrection option

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -7,7 +7,7 @@ TString hltString = "hltTree";
 const int NeventFilters = 3;
 std::string eventFilters[NeventFilters] = {"pPAprimaryVertexFilter","HBHENoiseFilterResultRun2Loose","pBeamScrapingFilter"};
 // options for systematic studies
-bool apply_JER_smear = true;
+bool apply_JER_smear = false;
 bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
@@ -28,6 +28,7 @@ bool doBJetNeutrinoEnergyShift = false;
 bool doNeutrinoEnergyAddition = false;
 bool doInclNeutrinoEnergySubtraction = false;
 bool apply_genJetPt_smear = false;
+bool doJERCorrection = true;
 
 
 
