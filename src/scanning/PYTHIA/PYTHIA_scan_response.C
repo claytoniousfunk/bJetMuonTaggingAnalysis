55d4ecf7b1bb9f63d88d1fc60df682bb14eed301
claytoniousfunk <cbennett0715@gmail.com>
2024-07-16 08:56:38 -0500

    call for the fit fxn

--- src/scanning/PYTHIA/PYTHIA_scan_response.C
+++ src/scanning/PYTHIA/PYTHIA_scan_response.C
@@ -58,6 +58,8 @@ TF1 *fitFxn_hiBin, *fitFxn_vz, *fitFxn_jetPt, *fitFxn_PYTHIA_JESb, *fitFxn_PYTHI
 #include "../../../headers/fitFunctions/fitFxn_jetPt.h"
 // JESb-fit function
 #include "../../../headers/fitFunctions/fitFxn_PYTHIA_JESb.h"
+// JER-correction function
+#include "../../../headers/fitFunctions/fitFxn_PYTHIA_JESCorrection.h"
 
 // eta-phi mask function
 #include "../../../headers/functions/etaPhiMask.h"
