a1f48cc1c73c00b5d8544bb27372f98eb4216722
claytoniousfunk <cbennett0715@gmail.com>
2024-10-16 00:56:32 -0500

    add back the muon tree

--- src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
+++ src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
@@ -75,14 +75,14 @@ void PYTHIAHYDJET_skim_simple(int group = 1){
     old_file->GetObject("hiEvtAnalyzer/HiTree",old_evt_tree);
     old_file->GetObject("hltanalysis/HltTree",old_hlt_tree);
     old_file->GetObject("akCs4PFJetAnalyzer/t",old_jet_tree);
-    //old_file->GetObject("ggHiNtuplizerGED/EventTree",old_muon_tree);
+    old_file->GetObject("ggHiNtuplizerGED/EventTree",old_muon_tree);
     old_file->GetObject("HiGenParticleAna/hi",old_gen_tree);
     // deactivate all branches                                                                                   
     old_filter_tree->SetBranchStatus("*",0);
     old_evt_tree->SetBranchStatus("*",0);
     old_hlt_tree->SetBranchStatus("*",0);
     old_jet_tree->SetBranchStatus("*",0);
-    //old_muon_tree->SetBranchStatus("*",0);
+    old_muon_tree->SetBranchStatus("*",0);
     old_gen_tree->SetBranchStatus("*",0);
     // activate only our variables of interest
     // filters
@@ -128,21 +128,21 @@ void PYTHIAHYDJET_skim_simple(int group = 1){
     old_jet_tree->SetBranchStatus("genphi",1);
     old_jet_tree->SetBranchStatus("ngen",1);
     // muons
-    // old_muon_tree->SetBranchStatus("muPt",1);
-    // old_muon_tree->SetBranchStatus("muEta",1);
-    // old_muon_tree->SetBranchStatus("muPhi",1);
-    // old_muon_tree->SetBranchStatus("muChi2NDF",1);
-    // old_muon_tree->SetBranchStatus("muInnerD0",1);
-    // old_muon_tree->SetBranchStatus("muInnerDz",1);
-    // old_muon_tree->SetBranchStatus("muIsPF",1);
-    // old_muon_tree->SetBranchStatus("muIsGlobal",1);
-    // old_muon_tree->SetBranchStatus("muIsTracker",1);
-    // old_muon_tree->SetBranchStatus("muMuonHits",1);
-    // old_muon_tree->SetBranchStatus("muStations",1);
-    // old_muon_tree->SetBranchStatus("muTrkLayers",1);
-    // old_muon_tree->SetBranchStatus("muPixelHits",1);
-    // old_muon_tree->SetBranchStatus("muCharge",1);
-    // old_muon_tree->SetBranchStatus("nMu",1);
+    old_muon_tree->SetBranchStatus("muPt",1);
+    old_muon_tree->SetBranchStatus("muEta",1);
+    old_muon_tree->SetBranchStatus("muPhi",1);
+    old_muon_tree->SetBranchStatus("muChi2NDF",1);
+    old_muon_tree->SetBranchStatus("muInnerD0",1);
+    old_muon_tree->SetBranchStatus("muInnerDz",1);
+    old_muon_tree->SetBranchStatus("muIsPF",1);
+    old_muon_tree->SetBranchStatus("muIsGlobal",1);
+    old_muon_tree->SetBranchStatus("muIsTracker",1);
+    old_muon_tree->SetBranchStatus("muMuonHits",1);
+    old_muon_tree->SetBranchStatus("muStations",1);
+    old_muon_tree->SetBranchStatus("muTrkLayers",1);
+    old_muon_tree->SetBranchStatus("muPixelHits",1);
+    old_muon_tree->SetBranchStatus("muCharge",1);
+    old_muon_tree->SetBranchStatus("nMu",1);
     // gen
     old_gen_tree->SetBranchStatus("pt",1);
     old_gen_tree->SetBranchStatus("eta",1);
@@ -158,21 +158,21 @@ void PYTHIAHYDJET_skim_simple(int group = 1){
     new_evt_tree = (TTree*) old_evt_tree->CloneTree(0);
     new_hlt_tree = (TTree*) old_hlt_tree->CloneTree(0);
     new_jet_tree = (TTree*) old_jet_tree->CloneTree(0);
-    // new_muon_tree = (TTree*) old_muon_tree->CloneTree(0);
+    new_muon_tree = (TTree*) old_muon_tree->CloneTree(0);
     new_gen_tree = (TTree*) old_gen_tree->CloneTree(0);
 
     new_filter_tree->CopyEntries(old_filter_tree);
     new_evt_tree->CopyEntries(old_evt_tree);
     new_hlt_tree->CopyEntries(old_hlt_tree);
     new_jet_tree->CopyEntries(old_jet_tree);
-    // new_muon_tree->CopyEntries(old_muon_tree);
+    new_muon_tree->CopyEntries(old_muon_tree);
     new_gen_tree->CopyEntries(old_gen_tree);
 
     new_filter_tree->SetName("filterTree");
     new_evt_tree->SetName("evtTree");
     new_hlt_tree->SetName("hltTree");
     new_jet_tree->SetName("jetTree");
-    // new_muon_tree->SetName("muonTree");
+    new_muon_tree->SetName("muonTree");
     new_gen_tree->SetName("genParticleTree");
 
     new_file->Write();
