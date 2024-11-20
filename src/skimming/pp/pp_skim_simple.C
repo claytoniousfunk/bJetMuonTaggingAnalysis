d809656203210533e237ee2343bee75807bcd212
claytoniousfunk <cbennett0715@gmail.com>
2024-11-20 09:42:15 -0600

    fix filename txt name

--- src/skimming/pp/pp_skim_simple.C
+++ src/skimming/pp/pp_skim_simple.C
@@ -2,7 +2,7 @@ void pp_skim_simple(int group = 1){
 
   string in_file_name;
   string output_file_base = "";
-  in_file_name = "../../../fileNames/fileNames_forest_pp_MinBias.txt";
+  in_file_name = "../../../fileNames/fileNames_pp_MinBias.txt";
   output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_skims_pp_HIZeroBias1_alt/pp_MinBias_skim_output_%i",group);
   string output_file_extension = "";
   output_file_extension += ".root";
