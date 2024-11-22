468a103491f0bd05e03d073945d84cd36a50bc11
claytoniousfunk <cbennett0715@gmail.com>
2024-11-22 16:13:54 -0600

    raise pThat from 30 to 45

--- headers/AnalysisSetupV2p2.h
+++ headers/AnalysisSetupV2p2.h
@@ -71,7 +71,8 @@ double nuPtMaxCut = 1000.0;
 
 //const double pthatcut = 15.0;
 //const double pthatcut = 25.0;
-const double pthatcut = 30.0;
+//const double pthatcut = 30.0;
+const double pthatcut = 45.0;
 //const double pthatcut = 40.0;
 //const double pthatcut = 50.0;
 
