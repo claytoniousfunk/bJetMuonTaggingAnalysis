dae18ab34e5355a19e932b3ffe8c74a5fadae5e6
claytoniousfunk <cbennett0715@gmail.com>
2024-11-22 23:20:15 -0600

    updated vz fit params

--- headers/fitParameters/vzFitParams_PH_mu12.h
+++ headers/fitParameters/vzFitParams_PH_mu12.h
@@ -4,12 +4,12 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 0.995199;
-double vzFitParam_1 = -0.0133438;
-double vzFitParam_2 = 0.000964176;
-double vzFitParam_3 = -7.01703e-05;
+double vzFitParam_0 = 1.00981;
+double vzFitParam_1 = -0.0122981;
+double vzFitParam_2 = 0.0;
+double vzFitParam_3 = 0.0;
 
-double e_vzFitParam_0 = 0.0130413;
-double e_vzFitParam_1 = 0.00349894;
-double e_vzFitParam_2 = 0.000324597;
-double e_vzFitParam_3 = 4.0079e-05;
+double e_vzFitParam_0 = 0.00812228;
+double e_vzFitParam_1 = 0.00160118;
+double e_vzFitParam_2 = 0.0;
+double e_vzFitParam_3 = 0.0;
