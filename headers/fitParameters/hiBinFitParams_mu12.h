62953433eff7318ef7e2cf34f98036ce03186775
claytoniousfunk <cbennett0715@gmail.com>
2024-11-24 21:21:00 -0600

    updated hiBin reweight params for mu12 PbPb MC

--- headers/fitParameters/hiBinFitParams_mu12.h
+++ headers/fitParameters/hiBinFitParams_mu12.h
@@ -1,8 +1,8 @@
 
-double hiBinFitParam_0 = 3.41572;
-double hiBinFitParam_1 = -0.0545867;
-double hiBinFitParam_2 = 0.000286138;
-double hiBinFitParam_3 = -4.84883e-07;
+double hiBinFitParam_0 = 3.82589;
+double hiBinFitParam_1 = -0.0643431;
+double hiBinFitParam_2 = 0.000358641;
+double hiBinFitParam_3 = -6.58354e-07;
 double hiBinFitParam_4 = 0.0;
 double hiBinFitParam_5 = 0.0;
 double hiBinFitParam_6 = 0.0;
@@ -10,10 +10,10 @@ double hiBinFitParam_7 = 0.0;
 double hiBinFitParam_8 = 0.0;
 
 
-double e_hiBinFitParam_0 = 0.0974763;
-double e_hiBinFitParam_1 = 0.00257909;
-double e_hiBinFitParam_2 = 2.2063e-05;
-double e_hiBinFitParam_3 = 6.05039e-08;
+double e_hiBinFitParam_0 = 0.0644626;
+double e_hiBinFitParam_1 = 0.00155959;
+double e_hiBinFitParam_2 = 1.24649e-05;
+double e_hiBinFitParam_3 = 3.25745e-08;
 double e_hiBinFitParam_4 = 0.0;
 double e_hiBinFitParam_5 = 0.0;
 double e_hiBinFitParam_6 = 0.0;
