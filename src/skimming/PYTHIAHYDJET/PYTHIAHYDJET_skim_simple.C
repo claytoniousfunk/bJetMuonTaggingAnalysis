008335910a44a8c6c9119a14ce64fe67b2ec79c0
claytoniousfunk <cbennett0715@gmail.com>
2024-12-02 13:32:41 -0600

    new batch 1 skim

--- src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
+++ src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
@@ -36,14 +36,18 @@ void PYTHIAHYDJET_skim_simple(int group = 1){
   // output_file_base += Form("/eos/user/c/cbennett/skims/dripping-tap/output_skim_PH_DiJet_batch14/PYTHIAHYDJET_DiJet_skim_output_%i",group);
 
   // DiJet, additional MC, incl, pTjet-15, N = 1325
-  in_file_name = "../../../fileNames/fileNames_PH_DiJet_batch15.txt";
-  output_file_base += Form("/eos/user/c/cbennett/skims/dripping-tap/output_skim_PH_DiJet_batch15/PYTHIAHYDJET_DiJet_skim_output_%i",group);
+  //in_file_name = "../../../fileNames/fileNames_PH_DiJet_batch15.txt";
+  //output_file_base += Form("/eos/user/c/cbennett/skims/dripping-tap/output_skim_PH_DiJet_batch15/PYTHIAHYDJET_DiJet_skim_output_%i",group);
+
+  // BATCH 1: DiJet, additional MC, incl, pTjet-15, N = 1653
+  in_file_name = "../../../fileNames/dripping-tap/fileNames_PH_DiJet_batch1.txt";
+  output_file_base += Form("/eos/user/c/cbennett/skims/dripping-tap/output_skim_PH_DiJet_batch1/PYTHIAHYDJET_DiJet_skim_output_%i",group);
   
 
 
 
 
-  int endfile = 1188;
+  int endfile = 1653;
 
 
 
