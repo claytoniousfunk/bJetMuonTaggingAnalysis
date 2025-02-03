96d42e82b03334a0f3b61c6568db5c01f01e89b7
claytoniousfunk <cbennett0715@gmail.com>
2025-02-03 09:53:38 -0600

    run over new skims

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
@@ -114,7 +114,7 @@ TH1D *h_jetTrkMaxPtRel[NCentralityIndices][NJetPtIndices];
 void PYTHIAHYDJET_jetTrkMax_scan(int group = 1){
 
   
-  TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_PH_DiJet_onlyJets_withTrackMaxInfo_100Files_partial/PYTHIAHYDJET_DiJet_skim_output_%i.root",group);
+  TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_PH_DiJet_onlyJets_withTrackMaxInfo_allFiles_partial/PYTHIAHYDJET_DiJet_skim_output_%i.root",group);
   TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_DiJet_jetTrkMax/PYTHIAHYDJET_scan_output_%i.root",group);
 
   // TString input = Form("/eos/user/c/cbennett/skims/output_PYTHIAHYDJET_MuJet_withGS_withWTA_2/PYTHIAHYDJET_MuJet_skim_output_%i.root",group);
