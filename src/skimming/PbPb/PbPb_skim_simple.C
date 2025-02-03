71bcc7528d8d9f27dba3658113ee9a45acbef26e
claytoniousfunk <cbennett0715@gmail.com>
2025-02-03 09:47:36 -0600

    run over new fileNames list

--- src/skimming/PbPb/PbPb_skim_simple.C
+++ src/skimming/PbPb/PbPb_skim_simple.C
@@ -2,7 +2,7 @@ void PbPb_skim_simple(int group = 1){
 
   string in_file_name;
   string output_file_base = "";
-  in_file_name = "../../../fileNames/fileNames_PbPb_DiJet_onlyJets_withTrackMaxInfo_100Files_partial.txt";
+  in_file_name = "../../../fileNames/fileNames_PbPb_DiJet_onlyJets_withTrackMaxInfo_allFiles_partial.txt";
   output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_DiJet_onlyJets_withTrackMaxInfo_partial/PbPb_DiJet_skim_output_%i",group);
   string output_file_extension = "";
   output_file_extension += ".root";
