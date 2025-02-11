874782c230559c1e77335770c0ff92a442e74145
claytoniousfunk <cbennett0715@gmail.com>
2025-02-10 22:42:31 -0600

    turn on dR reweight

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -31,8 +31,8 @@ bool apply_genJetPt_smear = false;
 bool doJERCorrection = false;
 // muptrel reweight
 bool doMuPtRelReweight = false;
-bool doHadronPtRelReweight = true;
+bool doHadronPtRelReweight = false;
 // dR reweight
-bool doDRReweight = false;
+bool doDRReweight = true;
 
 
