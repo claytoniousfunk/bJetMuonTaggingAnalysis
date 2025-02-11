3c84e77b6c1f26b405543d738ff43e67a5f54cbd
claytoniousfunk <cbennett0715@gmail.com>
2025-02-10 23:22:35 -0600

    dR fit for 0-90% PbPb (derived from J1 bin)

---
+++ headers/fitParameters/dRFitParams/dRFitParams_PbPb.h
@@ -0,0 +1,11 @@
+double dRFitParam_0 = 1.21823;
+double dRFitParam_1 = -10.7375;
+double dRFitParam_2 = 126.258;
+double dRFitParam_3 = -586.853;
+double dRFitParam_4 = 1056.73;
+
+double e_dRFitParam_0 = 0.00983994;
+double e_dRFitParam_1 = 0.557573;
+double e_dRFitParam_2 = 9.50505;
+double e_dRFitParam_3 = 59.0831;
+double e_dRFitParam_4 = 117.598;
