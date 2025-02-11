323796d9ef45919b010232b97ae14e85fd7468ed
claytoniousfunk <cbennett0715@gmail.com>
2025-02-10 23:07:17 -0600

    make hadronPtRelReweight a 4th order polynomial, new parameters

--- headers/fitParameters/hadronPtRelFitParams.h
+++ headers/fitParameters/hadronPtRelFitParams.h
@@ -1,5 +1,11 @@
-double hadronPtRelFitParam_0 = 0.859821;
-double hadronPtRelFitParam_1 = 0.134136;
+double hadronPtRelFitParam_0 = 0.974068;
+double hadronPtRelFitParam_1 = -0.266868;
+double hadronPtRelFitParam_2 = 0.370324;
+double hadronPtRelFitParam_3 = -0.0875637;
+double hadronPtRelFitParam_4 = 0.00586286;
 
-double e_hadronPtRelFitParam_0 = 0.013579;
-double e_hadronPtRelFitParam_1 = 0.011803;
+double e_hadronPtRelFitParam_0 = 0.0256353;
+double e_hadronPtRelFitParam_1 = 0.0839997;
+double e_hadronPtRelFitParam_2 = 0.0775224;
+double e_hadronPtRelFitParam_3 = 0.0232925;
+double e_hadronPtRelFitParam_4 = 0.00195031;
