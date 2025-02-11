df961b55b2f8553803271fa1830f362a54ec8b12
claytoniousfunk <cbennett0715@gmail.com>
2025-02-11 00:03:27 -0600

    new fit params for PbPb hadron pTrel

---
+++ headers/fitParameters/hadronPtRelFitParams_PbPb.h
@@ -0,0 +1,11 @@
+double hadronPtRelFitParam_0 = 1.07353;
+double hadronPtRelFitParam_1 = -0.273479;
+double hadronPtRelFitParam_2 = 0.154237;
+double hadronPtRelFitParam_3 = -0.0120447;
+double hadronPtRelFitParam_4 = 0.;
+
+double e_hadronPtRelFitParam_0 = 0.00637801;
+double e_hadronPtRelFitParam_1 = 0.0128996;
+double e_hadronPtRelFitParam_2 = 0.00637508;
+double e_hadronPtRelFitParam_3 = 0.000713137;
+double e_hadronPtRelFitParam_4 = 0.;
