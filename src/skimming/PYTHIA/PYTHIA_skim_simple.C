bb8c624f7533414842c2072c90adc27c8953bf17
claytoniousfunk <cbennett0715@gmail.com>
2025-02-14 00:48:17 -0600

    save wta info

--- src/skimming/PYTHIA/PYTHIA_skim_simple.C
+++ src/skimming/PYTHIA/PYTHIA_skim_simple.C
@@ -84,6 +84,8 @@ void PYTHIA_skim_simple(int group = 1){
     old_jet_tree->SetBranchStatus("rawpt",1);
     old_jet_tree->SetBranchStatus("jteta",1);
     old_jet_tree->SetBranchStatus("jtphi",1);
+    old_jet_tree->SetBranchStatus("WTAeta",1);
+    old_jet_tree->SetBranchStatus("WTAphi",1);
     old_jet_tree->SetBranchStatus("trackMax",1);
     old_jet_tree->SetBranchStatus("nref",1);
     old_jet_tree->SetBranchStatus("jtPartonFlavor",1);
