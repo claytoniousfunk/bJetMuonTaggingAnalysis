a7bb865be557f3f98ba1b0a18538c963a2466b2a
Clayton Bennett <cbennett0715@gmail.com>
2025-02-19 01:22:13 +0100

    double dR bins

--- src/scanning/PYTHIA/PYTHIA_jetTrkMax_scan.C
+++ src/scanning/PYTHIA/PYTHIA_jetTrkMax_scan.C
@@ -154,7 +154,7 @@ void PYTHIA_jetTrkMax_scan(int group = 1){
 
   TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_PYTHIA_DiJet_withJetTrackMaxInfo/PYTHIA_DiJet_skim_output_%i.root",group);
 
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_jetTrkMax_jet80_pThat-15/PYTHIA_DiJet_scan_output_%i.root",group);
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIA_DiJet_jetTrkMax_jet80_pThat-15_doubleDRBins/PYTHIA_DiJet_scan_output_%i.root",group);
 
   printIntroduction_PYTHIA_scan_V3p7();
   readConfig();
