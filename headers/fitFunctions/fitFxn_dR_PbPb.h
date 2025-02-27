66afd523bb8735dd3d4a2d5018a399b47bc30e50
claytoniousfunk <cbennett0715@gmail.com>
2025-02-27 17:04:32 -0600

    correct for low-dR narrowing

--- headers/fitFunctions/fitFxn_dR_PbPb.h
+++ headers/fitFunctions/fitFxn_dR_PbPb.h
@@ -2,19 +2,15 @@ void loadFitFxn_dR(){
 
   cout << "~~ loading fit function ..." << endl;
   
-  fitFxn_dR = new TF1("fitFxn_dR","[0] + [1]*x + [2]*x*x + [3]*x*x*x + [4]*x*x*x*x",0,0.4);
+  fitFxn_dR = new TF1("fitFxn_dR","1 + [0]*exp(-[1]*x)",0,0.4);
 
   fitFxn_dR->SetParameter(0,dRFitParam_0);
   fitFxn_dR->SetParameter(1,dRFitParam_1);
-  fitFxn_dR->SetParameter(2,dRFitParam_2);
-  fitFxn_dR->SetParameter(3,dRFitParam_3);
-  fitFxn_dR->SetParameter(4,dRFitParam_4);
+  
 
   fitFxn_dR->SetParError(0,e_dRFitParam_0);
   fitFxn_dR->SetParError(1,e_dRFitParam_1);
-  fitFxn_dR->SetParError(2,e_dRFitParam_2);
-  fitFxn_dR->SetParError(3,e_dRFitParam_3);
-  fitFxn_dR->SetParError(4,e_dRFitParam_4);
+  
 
   cout << "~~ ... finished!" << endl;
  
