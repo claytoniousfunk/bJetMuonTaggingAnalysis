3b7dc15a9976d98e1c63cc6bbbe93922f19780e2
claytoniousfunk <cbennett0715@gmail.com>
2025-02-28 12:27:25 -0600

    new hadronPtRel fix fxn

--- headers/fitFunctions/fitFxn_hadronPtRel.h
+++ headers/fitFunctions/fitFxn_hadronPtRel.h
@@ -2,20 +2,14 @@ void loadFitFxn_hadronPtRel(){
 
   cout << "~~ loading fit function ..." << endl;
   
-  fitFxn_hadronPtRel = new TF1("fitFxn_hadronPtRel","[0] + [1]*x + [2]*x*x + [3]*x*x*x + [4]*x*x*x*x",0,10);
+  fitFxn_hadronPtRel = new TF1("fitFxn_hadronPtRel","[0] + [1]*x",0,10);
 
   fitFxn_hadronPtRel->SetParameter(0,hadronPtRelFitParam_0);
   fitFxn_hadronPtRel->SetParameter(1,hadronPtRelFitParam_1);
-  fitFxn_hadronPtRel->SetParameter(2,hadronPtRelFitParam_2);
-  fitFxn_hadronPtRel->SetParameter(3,hadronPtRelFitParam_3);
-  fitFxn_hadronPtRel->SetParameter(4,hadronPtRelFitParam_4);
-
+ 
   fitFxn_hadronPtRel->SetParError(0,e_hadronPtRelFitParam_0);
   fitFxn_hadronPtRel->SetParError(1,e_hadronPtRelFitParam_1);
-  fitFxn_hadronPtRel->SetParError(2,e_hadronPtRelFitParam_2);
-  fitFxn_hadronPtRel->SetParError(3,e_hadronPtRelFitParam_3);
-  fitFxn_hadronPtRel->SetParError(4,e_hadronPtRelFitParam_4);
-
+ 
   cout << "~~ ... finished!" << endl;
  
 }
--- headers/fitParameters/hadronPtRelFitParams.h
+++ headers/fitParameters/hadronPtRelFitParams.h
@@ -1,11 +1,6 @@
-double hadronPtRelFitParam_0 = 0.974068;
-double hadronPtRelFitParam_1 = -0.266868;
-double hadronPtRelFitParam_2 = 0.370324;
-double hadronPtRelFitParam_3 = -0.0875637;
-double hadronPtRelFitParam_4 = 0.00586286;
+double hadronPtRelFitParam_0 = 0.659622;
+double hadronPtRelFitParam_1 = 0.349395;
 
-double e_hadronPtRelFitParam_0 = 0.0256353;
-double e_hadronPtRelFitParam_1 = 0.0839997;
-double e_hadronPtRelFitParam_2 = 0.0775224;
-double e_hadronPtRelFitParam_3 = 0.0232925;
-double e_hadronPtRelFitParam_4 = 0.00195031;
+
+double e_hadronPtRelFitParam_0 = 0.0207023;
+double e_hadronPtRelFitParam_1 = 0.0241225;
