147a1fe82f1c092beedfd3ce447640b822b6fc6d
claytoniousfunk <cbennett0715@gmail.com>
2025-03-13 17:15:10 -0500

    fix fitFxn form

--- headers/fitFunctions/fitFxn_jetPt_C4.h
+++ headers/fitFunctions/fitFxn_jetPt_C4.h
@@ -1,5 +1,5 @@
 void loadFitFxn_jetPt_C4(){
-  fitFxn_jetPt_C4 = new TF1("fitFxn_jetPt_C4","[0]+[1]*x",0,500);
+  fitFxn_jetPt_C4 = new TF1("fitFxn_jetPt_C4","1 + [0]*exp(-[1]*x)",0,500);
   fitFxn_jetPt_C4->SetParameter(0,jetPtFitParam_C4_0);
   fitFxn_jetPt_C4->SetParameter(1,jetPtFitParam_C4_1);
 }
