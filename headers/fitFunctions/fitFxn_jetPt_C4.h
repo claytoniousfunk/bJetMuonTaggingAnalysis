742e6f19032542030c4f7e6f6fc3a351af0ad485
claytoniousfunk <cbennett0715@gmail.com>
2025-03-13 15:33:38 -0500

    fix function names

--- headers/fitFunctions/fitFxn_jetPt_C4.h
+++ headers/fitFunctions/fitFxn_jetPt_C4.h
@@ -1,5 +1,5 @@
 void loadFitFxn_jetPt_C4(){
-  fitFxn_jetPt = new TF1("fitFxn_jetPt","[0]+[1]*x",0,500);
-  fitFxn_jetPt->SetParameter(0,jetPtFitParam_C4_0);
-  fitFxn_jetPt->SetParameter(1,jetPtFitParam_C4_1);
+  fitFxn_jetPt_C4 = new TF1("fitFxn_jetPt_C4","[0]+[1]*x",0,500);
+  fitFxn_jetPt_C4->SetParameter(0,jetPtFitParam_C4_0);
+  fitFxn_jetPt_C4->SetParameter(1,jetPtFitParam_C4_1);
 }
