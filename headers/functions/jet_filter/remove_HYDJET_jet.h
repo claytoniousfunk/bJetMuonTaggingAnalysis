573c18446d52fcd9917283c3bc9f2a89d110ac39
claytoniousfunk <cbennett0715@gmail.com>
2025-04-01 19:22:57 -0500

    up the parameter to 0.45

--- headers/functions/jet_filter/remove_HYDJET_jet.h
+++ headers/functions/jet_filter/remove_HYDJET_jet.h
@@ -8,7 +8,8 @@ bool remove_HYDJET_jet(double pThat, double pTjet){
   bool result = false;
 
   //if(pThat < 0.35 * pTjet) result = true;
-  if(pThat < 0.4 * pTjet) result = true;
+  //if(pThat < 0.4 * pTjet) result = true;
+  if(pThat < 0.45 * pTjet) result = true;
 
   return result;
 
