09ca13406c42ad77fc27a0d2b502b065e0c97f2d
claytoniousfunk <cbennett0715@gmail.com>
2025-04-15 20:15:33 -0500

    apply correctHiBinShift

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan_response.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan_response.C
@@ -104,7 +104,7 @@ TF1 *fitFxn_hiBin, *fitFxn_vz, *fitFxn_jetPt, *fitFxn_PYTHIA_JESb, *fitFxn_PYTHI
 void PYTHIAHYDJET_scan_response(int group = 1){
 
   TString input = Form("/eos/user/c/cbennett/skims/output_skim_PH_DiJet_pTjet-5_reskim/PYTHIAHYDJET_DiJet_skim_output_%i.root",group);
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PH_DiJet_pTjet-5_pThat-15_removeHYDJETjet0p45_response_fineCentBins/PYTHIAHYDJET_scan_output_%i.root",group);
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PH_DiJet_pTjet-5_pThat-15_removeHYDJETjet0p45_response_fineCentBins_correctHiBinShift/PYTHIAHYDJET_scan_output_%i.root",group);
   
 
   readConfig();
@@ -306,13 +306,13 @@ void PYTHIAHYDJET_scan_response(int group = 1){
 	
     int matchFlag[10] = {0,0,0,0,0,0,0,0,0,0};
 
-    int CentralityIndex = getCentBin(em->hiBin-10);
+    int CentralityIndex = getCentBin(em->hiBin+10);
 
     if(CentralityIndex < 0) continue;
 
 
     
-    double w_reweight_hiBin = fitFxn_hiBin->Eval(em->hiBin-10);
+    double w_reweight_hiBin = fitFxn_hiBin->Eval(em->hiBin+10);
 
 	
     //double w_reweight_vz = fitFxn_vz->Eval(em->vz);
