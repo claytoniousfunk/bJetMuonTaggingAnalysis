8a31caa14a101167d221c77a7e9b7941916dc843
clayton <clayton@debian>
2025-05-20 10:15:59 -0500

    run over batch 1

--- headers/config/config_PYTHIAHYDJET.h
+++ headers/config/config_PYTHIAHYDJET.h
@@ -35,10 +35,10 @@ bool onlyOneMuonTaggedJetPerEvent = false;
 TString generator = "PYTHIAHYDJET";
 
 // data set
-bool doDiJetSample = true;
+bool doDiJetSample = false;
 bool doMuJetSample = false;
 bool doBJetSample = false;
-bool doDiJetSample_batch1 = false;
+bool doDiJetSample_batch1 = true;
 bool doDiJetSample_batch2 = false;
 bool doDiJetSample_batch3 = false;
 bool doDiJetSample_batch4 = false;
