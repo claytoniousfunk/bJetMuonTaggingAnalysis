b243852f88d1939d11bdf1331f450e2594eedd2b
clayton <clayton@debian>
2025-05-21 11:23:15 -0500

    updated vz fit params

--- headers/fitFunctions/fitFxn_vz_PH.h
+++ headers/fitFunctions/fitFxn_vz_PH.h
@@ -2,7 +2,7 @@ void loadFitFxn_vz(){
 
   cout << "~~ loading fit function ..." << endl;
   
-  fitFxn_vz = new TF1("fitFxn_vz","[0] + [1]*x+ [2]*x*x + [3]*x*x*x",-15,15);
+  fitFxn_vz = new TF1("fitFxn_vz","[0] + [1]*x",-15,15);
 
   fitFxn_vz->SetParameter(0,vzFitParam_0);
   fitFxn_vz->SetParameter(1,vzFitParam_1);
--- headers/fitParameters/vzFitParams_PH_mu12.h
+++ headers/fitParameters/vzFitParams_PH_mu12.h
@@ -2,14 +2,14 @@
 // ------- vz fit parameters
 // ---------- 3rd order polynomial
 
-int N_vzFitParam = 4;
+int N_vzFitParam = 2;
 
-double vzFitParam_0 = 0.975875;
-double vzFitParam_1 = 0.00243224;
-double vzFitParam_2 = 0.000715301;
+double vzFitParam_0 = 1.01391e+00;
+double vzFitParam_1 = -1.43684e-02;
+double vzFitParam_2 = 0.0;
 double vzFitParam_3 = 0.0;
 
-double e_vzFitParam_0 = 0.00727791;
-double e_vzFitParam_1 = 0.00107092;
-double e_vzFitParam_2 = 0.000149009;
+double e_vzFitParam_0 = 7.03984e-03;
+double e_vzFitParam_1 = 1.37839e-03;
+double e_vzFitParam_2 = 0.0;
 double e_vzFitParam_3 = 0.0;
