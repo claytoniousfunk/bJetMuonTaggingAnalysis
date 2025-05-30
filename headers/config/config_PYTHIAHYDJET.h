778960c2bc3f627da2412fc4aa868d03aa6b1134
clayton <clayton@debian>
2025-05-30 10:57:41 -0500

    turn on hiBin reweight

--- headers/config/config_PYTHIAHYDJET.h
+++ headers/config/config_PYTHIAHYDJET.h
@@ -13,7 +13,7 @@ bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
 bool skipGenParticles = false; // set TRUE for MuJet and BJet datasets
 // reweighting functions
-bool doHiBinReweight = false;
+bool doHiBinReweight = true;
 bool doVzReweight = false;
 bool doJetPtReweight = false;
 // jet-based filters
