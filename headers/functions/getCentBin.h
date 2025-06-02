1f4406da0e8a0350cb22b5af59f8da2d0b282e5d
clayton <clayton@debian>
2025-06-02 09:56:29 -0500

    fix getCentBin logic

--- headers/functions/getCentBin.h
+++ headers/functions/getCentBin.h
@@ -1,13 +1,13 @@
 const int getCentBin(int hiBin)
 {
 	int result = -1;
-	if (hiBin >= hiBin_C0_lo && hiBin < hiBin_C0_hi)
+	if (hiBin >= hiBin_C0_lo && hiBin <= hiBin_C0_hi)
 		result = 1;
-	else if (hiBin >= hiBin_C0_hi && hiBin < hiBin_C1_hi)
+	else if (hiBin > hiBin_C0_hi && hiBin <= hiBin_C1_hi)
 		result = 2;
-	else if (hiBin >= hiBin_C1_hi && hiBin < hiBin_C2_hi)
+	else if (hiBin > hiBin_C1_hi && hiBin <= hiBin_C2_hi)
 		result = 3;
-	else if (hiBin >= hiBin_C2_hi && hiBin < hiBin_C3_hi)
+	else if (hiBin > hiBin_C2_hi && hiBin <= hiBin_C3_hi)
 		result = 4;
 	else
 		;
