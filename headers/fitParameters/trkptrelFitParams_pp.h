f3840cfb10f926973be5b6cd2ca6f33ee0698ecf
clayton <clayton@debian>
2025-06-26 11:58:29 -0500

    new explicit trkptrel reweight fxns and params

---
+++ headers/fitParameters/trkptrelFitParams_pp.h
@@ -0,0 +1,9 @@
+double trkptrelFitParam_0 = 1.50576;
+double trkptrelFitParam_1 = -0.999548;
+double trkptrelFitParam_2 = 0.399199;
+double trkptrelFitParam_3 = -0.045131;
+
+double e_trkptrelFitParam_0 = 0.0647629;
+double e_trkptrelFitParam_1 = 0.152119;
+double e_trkptrelFitParam_2 = 0.0958898;
+double e_trkptrelFitParam_3 = 0.0157988;
