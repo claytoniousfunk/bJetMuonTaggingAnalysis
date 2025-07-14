39f0213513d20ed4d753aab6ce4992de4c843be9
clayton <clayton@debian>
2025-07-14 12:29:51 -0500

    fix centEdges array

--- headers/AnalysisSetupV2p3.h
+++ headers/AnalysisSetupV2p3.h
@@ -20,7 +20,7 @@ const int hiBin_C0_hi = 20;
 const int hiBin_C1_hi = 60;
 const int hiBin_C2_hi = 100;
 const int hiBin_C3_hi = 160;
-int centEdges[NCentralityIndices] = {hiBin_C0_lo, hiBin_C0_hi, hiBin_C1_hi, hiBin_C2_hi};
+int centEdges[NCentralityIndices] = {hiBin_C0_lo, hiBin_C0_hi, hiBin_C1_hi, hiBin_C2_hi, hiBin_C3_hi};
 
 /* const int hiBin_C0_lo = 0; */
 /* const int hiBin_C0_hi = 60; */
