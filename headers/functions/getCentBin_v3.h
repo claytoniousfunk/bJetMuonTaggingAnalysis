9482711d6695ff569595eee08599137fd0a4796a
clayton <clayton@debian>
2025-07-15 08:47:34 -0500

    5 cent bins

--- headers/functions/getCentBin_v3.h
+++ headers/functions/getCentBin_v3.h
@@ -1,17 +1,17 @@
 const int getCentBin(int hiBin)
 {
-	int result = -1;
-	if (hiBin >= hiBin_C0_lo && hiBin < hiBin_C0_hi)
-		result = 1;
-	else if (hiBin >= hiBin_C0_hi && hiBin < hiBin_C1_hi)
-		result = 2;
-	else if (hiBin >= hiBin_C1_hi && hiBin < hiBin_C2_hi)
-		result = 3;
-	else if (hiBin >= hiBin_C2_hi && hiBin < hiBin_C3_hi)
-		result = 4;
-	else if (hiBin >= hiBin_C3_hi && hiBin < hiBin_C4_hi)
-		result = 5;
-	else
-		;
-	return result;
+  int result = -1;
+  if (hiBin >= hiBin_C0_lo && hiBin < hiBin_C0_hi)
+    result = 1;
+  else if (hiBin >= hiBin_C0_hi && hiBin < hiBin_C1_hi)
+    result = 2;
+  else if (hiBin >= hiBin_C1_hi && hiBin < hiBin_C2_hi)
+    result = 3;
+  else if (hiBin >= hiBin_C2_hi && hiBin < hiBin_C3_hi)
+    result = 4;
+  else if (hiBin >= hiBin_C3_hi && hiBin < hiBin_C4_hi)
+    result = 5;
+  else
+    ;
+  return result;
 }	
