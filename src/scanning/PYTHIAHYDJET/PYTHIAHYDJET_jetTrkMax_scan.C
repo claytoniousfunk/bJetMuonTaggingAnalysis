599727595b1a191dee0303995186f9fa17edd51a
clayton <clayton@debian>
2025-07-21 12:00:10 -0500

    turn off dR reweight

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
@@ -134,7 +134,7 @@ void PYTHIAHYDJET_jetTrkMax_scan(int group = 1){
 
   
   TString input = Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_PH_DiJet_onlyJets_withTrackMaxInfo_withHLT/PYTHIAHYDJET_DiJet_skim_output_%i.root",group);
-  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_DiJet_jetTrkMax_pThat-20_trkpT-14_hiBinShift-10_dRReweight_ultraFineCentBins_2025-07-21/PYTHIAHYDJET_scan_output_%i.root",group);   
+  TString output = Form("/eos/cms/store/group/phys_heavyions/cbennett/scanningOutput/output_PYTHIAHYDJET_DiJet_jetTrkMax_pThat-20_trkpT-14_hiBinShift-10_ultraFineCentBins_2025-07-21/PYTHIAHYDJET_scan_output_%i.root",group);   
   
   
   // JET ENERGY CORRECTIONS
@@ -422,7 +422,7 @@ void PYTHIAHYDJET_jetTrkMax_scan(int group = 1){
       // double w_jet = w * fitFxn_trkptrel->Eval(jetTrkMaxPtRel_i);
       
       // apply dR reweight
-      double w_jet = w * fitFxn_dR->Eval(jetTrkMaxDR_i);
+      // double w_jet = w * fitFxn_dR->Eval(jetTrkMaxDR_i);
 
       // apply hadronPtRel reweight
       // double w_jet = w * fitFxn_hadronPtRel->Eval(jetTrkMaxPtRel_i);
