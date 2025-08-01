261140a3c017f24e565ca48dec4d2470d65d6dc8
clayton <clayton@debian>
2025-07-31 22:36:08 -0500

    revert to nominal binning

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
@@ -38,8 +38,8 @@
 #include "../../../JetEnergyCorrections/JetUncertainty.h"
 // general analysis variables
 //#include "../../../headers/AnalysisSetupV2p2.h"
-//#include "../../../headers/AnalysisSetupV2p3.h"
-#include "../../../headers/AnalysisSetupV2p4.h"
+#include "../../../headers/AnalysisSetupV2p3.h"
+//#include "../../../headers/AnalysisSetupV2p4.h"
 // vz-fit parameters
 //#include "../../../headers/fitParameters/vzFitParams_PH_mu5.h"
 //#include "../../../headers/fitParameters/vzFitParams_PH_mu7.h"
@@ -84,8 +84,8 @@ TF1 *fitFxn_hiBin, *fitFxn_vz, *fitFxn_jetPt, *fitFxn_hadronPtRel, *fitFxn_dR, *
 #include "../../../headers/functions/getJetPtBin.h"
 // getCentBin function
 //#include "../../../headers/functions/getCentBin_v2.h" // course centrality binning
-//#include "../../../headers/functions/getCentBin.h" // fine centrality binning
-#include "../../../headers/functions/getCentBin_v3.h" // ultra-fine centrality binning 
+#include "../../../headers/functions/getCentBin.h" // fine centrality binning
+//#include "../../../headers/functions/getCentBin_v3.h" // ultra-fine centrality binning 
 // getPtRel function
 #include "../../../headers/functions/getPtRel.h"
 // isQualityMuon_hybridSoft function
