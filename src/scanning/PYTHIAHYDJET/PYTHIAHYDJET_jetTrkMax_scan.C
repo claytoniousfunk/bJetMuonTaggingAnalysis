77e104640e4d0ec9717987b6982a596de593afe9
clayton <clayton@debian>
2025-08-01 11:00:46 -0500

    turn off trkptrelReweight

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_jetTrkMax_scan.C
@@ -195,7 +195,7 @@ void PYTHIAHYDJET_jetTrkMax_scan(int group = 1){
 						 hiBinShift);
 
 
-  outputDatasetName.Append("_trkptrelReweight");
+  //outputDatasetName.Append("_trkptrelReweight");
   
   TString output = Form("%s%s/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
 
@@ -494,13 +494,13 @@ void PYTHIAHYDJET_jetTrkMax_scan(int group = 1){
       }
 
       // apply no reweight
-      // double w_jet = w;
+      double w_jet = w;
 
       // apply mupt reweight
       // double w_jet = w * fitFxn_mupt->Eval(jetTrkMax_i);
 
       // apply trkptrel reweight
-      double w_jet = w * fitFxn_trkptrel->Eval(jetTrkMaxPtRel_i);
+      // double w_jet = w * fitFxn_trkptrel->Eval(jetTrkMaxPtRel_i);
       
       // apply dR reweight
       // double w_jet = w * fitFxn_dR->Eval(jetTrkMaxDR_i);
