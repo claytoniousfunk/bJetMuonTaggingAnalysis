b787305c9722445845b89ee8a2169aca2907a9e8
clayton <clayton@debian>
2025-09-30 14:09:45 -0500

    turn on apply_JEU_shift_up

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -8,7 +8,7 @@ const int NeventFilters = 3;
 std::string eventFilters[NeventFilters] = {"pPAprimaryVertexFilter","HBHENoiseFilterResultRun2Loose","pBeamScrapingFilter"};
 // options for systematic studies
 bool apply_JER_smear = false;
-bool apply_JEU_shift_up = false;
+bool apply_JEU_shift_up = true;
 bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
 bool skipGenParticles = false;
