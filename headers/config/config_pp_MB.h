80709f214df4528030ae496994c9d8a20c1ebda0
clayton <clayton@debian>
2025-10-13 09:04:52 -0500

    configure for applyJERsmear scan

--- headers/config/config_pp_MB.h
+++ headers/config/config_pp_MB.h
@@ -14,6 +14,6 @@ bool doJESCorrection = false;
 bool doBJetNeutrinoEnergyShift = false;
 bool doJERCorrection = false;
 // options for systematic studies
-bool apply_JER_smear = false;
+bool apply_JER_smear = true;
 bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
