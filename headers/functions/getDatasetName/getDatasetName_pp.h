bb2de874199601955cb0b6110103a71894d8afac
clayton <clayton@debian>
2025-10-15 16:11:39 -0500

    get alt dataset

--- headers/functions/getDatasetName/getDatasetName_pp.h
+++ headers/functions/getDatasetName/getDatasetName_pp.h
@@ -7,7 +7,7 @@ TString getDatasetName(bool doSingleMuonSample = true,
   TString result = "";
   
   if(doSingleMuonSample) result="/eos/user/c/cbennett/skims/output_pp_SingleMuon/";
-  else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_pp_HIZeroBias1/";
+  else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_pp_HIZeroBias1_alt/";
   else if(doHighEGJetSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_pp_HighEGJet/";
   else{};
 
