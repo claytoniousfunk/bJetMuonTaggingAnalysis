1a1c7547b52ca1bcd07803d4de4bff8ac5ace19a
clayton <clayton@debian>
2025-10-15 16:59:39 -0500

    jet60 HighEGJet scan

--- headers/config/config_pp.h
+++ headers/config/config_pp.h
@@ -11,7 +11,7 @@ std::string eventFilters_MinBias[NeventFilters_MinBias] = {"pprimaryVertexFilter
 // jet options
 bool doJetTrkMaxFilter = true;
 bool doEtaPhiMask = false;
-bool applyJet60Trigger = false;
+bool applyJet60Trigger = true;
 bool applyJet80Trigger = false;
 bool doJESCorrection = false;
 // spectra alterations
@@ -23,6 +23,6 @@ bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // select dataset
 bool doSingleMuonSample = false;
-bool doMinBiasSample = true;
-bool doHighEGJetSample = false;
+bool doMinBiasSample = false;
+bool doHighEGJetSample = true;
 
