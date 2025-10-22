5bf27a4f5b2a794f7a3547e5ccb936c5b6ffa888
clayton <clayton@debian>
2025-10-22 16:37:00 -0500

    new skim with jet triggers

--- src/skimming/PYTHIA/PYTHIA_skim_simple.C
+++ src/skimming/PYTHIA/PYTHIA_skim_simple.C
@@ -2,8 +2,8 @@ void PYTHIA_skim_simple(int group = 1){
 
   string in_file_name;
   string output_file_base = "";
-  in_file_name = "../../../fileNames/fileNames_testForest_PYTHIA_DiJet.txt";
-  output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_testForest_PYTHIA_DiJet/PYTHIA_DiJet_skim_output_%i",group);
+  in_file_name = "../../../fileNames/fileNames_PYTHIA_DiJet_withGS.txt";
+  output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_PYTHIA_DiJet_withGS_withJetTriggers/PYTHIA_DiJet_skim_output_%i",group);
   string output_file_extension = "";
   output_file_extension += ".root";
 
@@ -12,9 +12,9 @@ void PYTHIA_skim_simple(int group = 1){
   std::string filename;
   Int_t ifile = 0;
 
-  // int endfile = 384; // PYTHIA_withGS forest
+  int endfile = 1443; // PYTHIA_withGS forest
   // int endfile = 987; // PYTHIA_withGS_withNeutrinos forest
-  int endfile = 20; // PYTHIA_DiJet_withJetTrackMaxInfo forest
+  // int endfile = 20; // PYTHIA_DiJet_withJetTrackMaxInfo forest
 
   TFile *old_file;
   TString filename_string = "";
@@ -105,28 +105,28 @@ void PYTHIA_skim_simple(int group = 1){
     old_jet_tree->SetBranchStatus("trackMaxDR",1);
 
     // muons
-    // old_muon_tree->SetBranchStatus("muPt",1);
-    // old_muon_tree->SetBranchStatus("muEta",1);
-    // old_muon_tree->SetBranchStatus("muPhi",1);
-    // old_muon_tree->SetBranchStatus("muChi2NDF",1);
-    // old_muon_tree->SetBranchStatus("muInnerD0",1);
-    // old_muon_tree->SetBranchStatus("muInnerDz",1);
-    // old_muon_tree->SetBranchStatus("muIsPF",1);
-    // old_muon_tree->SetBranchStatus("muIsGlobal",1);
-    // old_muon_tree->SetBranchStatus("muIsTracker",1);
-    // old_muon_tree->SetBranchStatus("muMuonHits",1);
-    // old_muon_tree->SetBranchStatus("muStations",1);
-    // old_muon_tree->SetBranchStatus("muTrkLayers",1);
-    // old_muon_tree->SetBranchStatus("muPixelHits",1);
-    // old_muon_tree->SetBranchStatus("muCharge",1);
-    // old_muon_tree->SetBranchStatus("nMu",1);
+    old_muon_tree->SetBranchStatus("muPt",1);
+    old_muon_tree->SetBranchStatus("muEta",1);
+    old_muon_tree->SetBranchStatus("muPhi",1);
+    old_muon_tree->SetBranchStatus("muChi2NDF",1);
+    old_muon_tree->SetBranchStatus("muInnerD0",1);
+    old_muon_tree->SetBranchStatus("muInnerDz",1);
+    old_muon_tree->SetBranchStatus("muIsPF",1);
+    old_muon_tree->SetBranchStatus("muIsGlobal",1);
+    old_muon_tree->SetBranchStatus("muIsTracker",1);
+    old_muon_tree->SetBranchStatus("muMuonHits",1);
+    old_muon_tree->SetBranchStatus("muStations",1);
+    old_muon_tree->SetBranchStatus("muTrkLayers",1);
+    old_muon_tree->SetBranchStatus("muPixelHits",1);
+    old_muon_tree->SetBranchStatus("muCharge",1);
+    old_muon_tree->SetBranchStatus("nMu",1);
     // gen
-    // old_gen_tree->SetBranchStatus("pt",1);
-    // old_gen_tree->SetBranchStatus("eta",1);
-    // old_gen_tree->SetBranchStatus("phi",1);
-    // old_gen_tree->SetBranchStatus("chg",1);
-    // old_gen_tree->SetBranchStatus("pdg",1);
-    // old_gen_tree->SetBranchStatus("n",1);
+    old_gen_tree->SetBranchStatus("pt",1);
+    old_gen_tree->SetBranchStatus("eta",1);
+    old_gen_tree->SetBranchStatus("phi",1);
+    old_gen_tree->SetBranchStatus("chg",1);
+    old_gen_tree->SetBranchStatus("pdg",1);
+    old_gen_tree->SetBranchStatus("n",1);
     
 
     new_file = (TFile*) TFile::Open((TString) (output_file_base+output_file_extension),"recreate");
@@ -135,22 +135,22 @@ void PYTHIA_skim_simple(int group = 1){
     new_evt_tree = (TTree*) old_evt_tree->CloneTree(0);
     new_hlt_tree = (TTree*) old_hlt_tree->CloneTree(0);
     new_jet_tree = (TTree*) old_jet_tree->CloneTree(0);
-    // new_muon_tree = (TTree*) old_muon_tree->CloneTree(0);
-    // new_gen_tree = (TTree*) old_gen_tree->CloneTree(0);
+    new_muon_tree = (TTree*) old_muon_tree->CloneTree(0);
+    new_gen_tree = (TTree*) old_gen_tree->CloneTree(0);
 
     new_filter_tree->CopyEntries(old_filter_tree);
     new_evt_tree->CopyEntries(old_evt_tree);
     new_hlt_tree->CopyEntries(old_hlt_tree);
     new_jet_tree->CopyEntries(old_jet_tree);
-    // new_muon_tree->CopyEntries(old_muon_tree);
-    // new_gen_tree->CopyEntries(old_gen_tree);
+    new_muon_tree->CopyEntries(old_muon_tree);
+    new_gen_tree->CopyEntries(old_gen_tree);
 
     new_filter_tree->SetName("filterTree");
     new_evt_tree->SetName("evtTree");
     new_hlt_tree->SetName("hltTree");
     new_jet_tree->SetName("jetTree");
-    // new_muon_tree->SetName("muonTree");
-    // new_gen_tree->SetName("genParticleTree");
+    new_muon_tree->SetName("muonTree");
+    new_gen_tree->SetName("genParticleTree");
 
     new_file->Write();
 
