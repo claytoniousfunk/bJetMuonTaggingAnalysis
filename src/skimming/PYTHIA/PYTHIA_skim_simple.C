a874ae80fb1b4c6253822bdafffbaa4af00ec2ff
clayton <clayton@debian>
2025-10-22 16:43:50 -0500

    change write out location

--- src/skimming/PYTHIA/PYTHIA_skim_simple.C
+++ src/skimming/PYTHIA/PYTHIA_skim_simple.C
@@ -3,7 +3,7 @@ void PYTHIA_skim_simple(int group = 1){
   string in_file_name;
   string output_file_base = "";
   in_file_name = "../../../fileNames/fileNames_PYTHIA_DiJet_withGS.txt";
-  output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skim_PYTHIA_DiJet_withGS_withJetTriggers/PYTHIA_DiJet_skim_output_%i",group);
+  output_file_base += Form("/eos/user/c/cbennett/skims/output_skim_PYTHIA_DiJet_withGS_withJetTriggers/PYTHIA_DiJet_skim_output_%i",group);
   string output_file_extension = "";
   output_file_extension += ".root";
 
