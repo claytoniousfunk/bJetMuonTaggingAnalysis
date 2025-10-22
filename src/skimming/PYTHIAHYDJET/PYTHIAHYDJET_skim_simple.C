fe0728f88ba0db776d1c133f5689d7d456a5c950
clayton <clayton@debian>
2025-10-22 11:41:11 -0500

    write to user space

--- src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
+++ src/skimming/PYTHIAHYDJET/PYTHIAHYDJET_skim_simple.C
@@ -25,7 +25,7 @@ void PYTHIAHYDJET_skim_simple(int group = 1){
 
   // DiJet, incl, pTjet-5, N = 7117
   in_file_name = "../../../fileNames/fileNames_PH_DiJet_pTjet-5.txt";
-  output_file_base += Form("/eos/cms/store/group/phys_heavyions/cbennett/output_skim_PH_DiJet_pTjet-5_withJetTriggers/PYTHIAHYDJET_DiJet_skim_output_%i",group);
+  output_file_base += Form("/eos/user/c/cbennett/skims/output_skim_PH_DiJet_pTjet-5_withJetTriggers/PYTHIAHYDJET_DiJet_skim_output_%i",group);
 
   // DiJet, additional MC, incl, pTjet-15, N = 1411
   // in_file_name = "../../../fileNames/fileNames_PH_DiJet_batch13.txt";
