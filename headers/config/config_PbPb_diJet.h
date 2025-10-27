6d213f06dcc9f963c3377d850f6889c18fb8f0d6
clayton <clayton@debian>
2025-10-27 10:43:36 -0500

    apply jet60 trigger

--- headers/config/config_PbPb_diJet.h
+++ headers/config/config_PbPb_diJet.h
@@ -7,8 +7,8 @@ const int NeventFilters = 2;
 std::string eventFilters[NeventFilters] = {"pprimaryVertexFilter","pclusterCompatibilityFilter"};
 bool doJetTrkMaxFilter = false;
 bool doEtaPhiMask = false;
-bool applyJet60Trigger = false;
-bool applyJet80Trigger = true;
+bool applyJet60Trigger = true;
+bool applyJet80Trigger = false;
 bool applyMinBiasTrigger = false;
 // experimental filters
 bool onlyOneMuonTaggedJetPerEvent = false;
