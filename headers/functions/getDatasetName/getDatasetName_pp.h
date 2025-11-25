f3bc4d88e65cbfe7f13a23ecd15bc632b3087285
clayton <clayton@debian>
2025-11-25 10:24:24 -0600

    new minbias dataset (X3 more stats), back to original JECs

--- headers/functions/getDatasetName/getDatasetName_pp.h
+++ headers/functions/getDatasetName/getDatasetName_pp.h
@@ -7,7 +7,8 @@ TString getDatasetName(bool doSingleMuonSample = true,
   TString result = "";
   
   if(doSingleMuonSample) result="/eos/user/c/cbennett/skims/output_pp_SingleMuon/";
-  else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_pp_HIZeroBias1_alt/";
+  //else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_pp_HIZeroBias1_alt/";
+  else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_pp_HIZeroBias1/";
   else if(doHighEGJetSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_pp_HighEGJet/";
   else{};
 
