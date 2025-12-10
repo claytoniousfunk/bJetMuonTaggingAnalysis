28d1fc34b3bc30470dc0f9f3e7dc260b854a52f8
clayton <clayton@debian>
2025-12-10 11:22:56 -0600

    change minBias dataset to one with relevant triggers

--- headers/functions/getDatasetName/getDatasetName_PbPb.h
+++ headers/functions/getDatasetName/getDatasetName_PbPb.h
@@ -7,7 +7,8 @@ TString getDatasetName(bool doSingleMuonSample = true,
   TString result = "";
   
   if(doSingleMuonSample) result="/eos/user/c/cbennett/skims/output_PbPb_SingleMuon_withWTA/";
-  else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_PbPb_HIMinimumBias0/";
+  //else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_PbPb_HIMinimumBias0/";
+  else if(doMinBiasSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_MinBias_withMinBiasTrigger/";
   else if(doHardProbesSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_HardProbes_2/";
   else{};
 
