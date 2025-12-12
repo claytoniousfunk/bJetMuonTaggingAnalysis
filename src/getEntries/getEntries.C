024a89e725740cb622dc714c93d3583acdb6ec3a
clayton <clayton@debian>
2025-12-12 11:06:30 -0600

    new getEntries macro

---
+++ src/getEntries/getEntries.C
@@ -0,0 +1,10 @@
+
+
+
+void getEntries(int i = 0){
+
+  TFile *f = TFile::Open(Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_pp_HighEGJet/pp_skim_output_%i.root",i));
+  TTree *t = (TTree*)f->Get("evtTree");
+  cout << (t ? t->GetEntries() : 0) << endl;
+
+}
