6d428815e60f601561b0a2c53d8c26af0e44a195
clayton <clayton@debian>
2025-12-15 12:23:54 -0600

    run over cleanup set

--- src/getEntries/getEntries.C
+++ src/getEntries/getEntries.C
@@ -3,7 +3,7 @@
 
 void getEntries(int i = 0){
 
-  TFile *f = TFile::Open(Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_pp_HighEGJet/pp_skim_output_%i.root",i));
+  TFile *f = TFile::Open(Form("/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_pp_HighEGJet_cleanup/pp_skim_output_%i.root",i));
   TTree *t = (TTree*)f->Get("evtTree");
   cout << (t ? t->GetEntries() : 0) << endl;
 
