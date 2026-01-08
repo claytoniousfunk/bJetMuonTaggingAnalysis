5cba37d7ccf0296195bfa6dfaf7dad1dbc733553
clayton <clayton@debian>
2026-01-08 14:55:00 -0600

    new parameters and fit function (pol8)

--- headers/fitParameters/hiBinFitParams_mu12.h
+++ headers/fitParameters/hiBinFitParams_mu12.h
@@ -1,22 +1,22 @@
 
-double hiBinFitParam_0 = 2.55108e+00;
-double hiBinFitParam_1 = -8.45487e-01;
-double hiBinFitParam_2 = 3.46340e+00;
-double hiBinFitParam_3 = -5.51622e-02;
-double hiBinFitParam_4 = 2.84669e-04;
-double hiBinFitParam_5 = -4.63494e-07;
-double hiBinFitParam_6 = 0.0;
-double hiBinFitParam_7 = 0.0;
-double hiBinFitParam_8 = 0.0;
+double hiBinFitParam_0 = 2.23319;
+double hiBinFitParam_1 = 0.129792;
+double hiBinFitParam_2 = -0.0117398;
+double hiBinFitParam_3 = 0.000397573;
+double hiBinFitParam_4 = -7.34681e-06;
+double hiBinFitParam_5 = 7.90566e-08;
+double hiBinFitParam_6 = -4.93426e-10;
+double hiBinFitParam_7 = 1.65641e-12;
+double hiBinFitParam_8 = -2.31409e-15;
 
 
-double e_hiBinFitParam_0 = 1.56803e+00;
-double e_hiBinFitParam_1 = 4.64730e-01;
-double e_hiBinFitParam_2 = 6.97830e-02;
-double e_hiBinFitParam_3 = 1.99507e-03;
-double e_hiBinFitParam_4 = 1.83196e-05;
-double e_hiBinFitParam_5 = 5.37099e-08;
-double e_hiBinFitParam_6 = 0.0;
-double e_hiBinFitParam_7 = 0.0;
-double e_hiBinFitParam_8 = 0.0;
+double e_hiBinFitParam_0 = 0.446242;
+double e_hiBinFitParam_1 = 0.0884157;
+double e_hiBinFitParam_2 = 0.00609432;
+double e_hiBinFitParam_3 = 0.000202276;
+double e_hiBinFitParam_4 = 3.71082e-06;
+double e_hiBinFitParam_5 = 3.97474e-08;
+double e_hiBinFitParam_6 = 2.47765e-10;
+double e_hiBinFitParam_7 = 8.3337e-13;
+double e_hiBinFitParam_8 = 1.1697e-15;
 
