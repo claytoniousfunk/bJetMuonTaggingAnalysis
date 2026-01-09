7983e7ceda059139367dc1cb5b90516e929f20a0
clayton <clayton@debian>
2026-01-09 10:11:21 -0600

    pthat to 30, edit removeHYDJETjet fxn

--- headers/config/config_PYTHIA.h
+++ headers/config/config_PYTHIA.h
@@ -13,8 +13,8 @@ bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
 bool skipGenParticles = true;
 // reweighting functions
-bool doVzReweight = true;
-bool doJetPtReweight = true;
+bool doVzReweight = false;
+bool doJetPtReweight = false;
 // jet-based filers
 bool doGenJetPthatFilter = false;
 bool doLeadingXjetDumpFilter = false;
