2e5a4533140c235a871509212060c692a066640e
clayton <clayton@debian>
2026-01-09 12:20:49 -0600

    new fit fxns and parameters for vz reweight

--- headers/fitParameters/vzFitParams_PH_mu12.h
+++ headers/fitParameters/vzFitParams_PH_mu12.h
@@ -2,14 +2,14 @@
 // ------- vz fit parameters
 // ---------- 3rd order polynomial
 
-int N_vzFitParam = 2;
+int N_vzFitParam = 4;
 
-double vzFitParam_0 = 1.01391e+00;
-double vzFitParam_1 = -1.43684e-02;
-double vzFitParam_2 = 0.0;
-double vzFitParam_3 = 0.0;
+double vzFitParam_0 = 1.00298e+00;
+double vzFitParam_1 = -2.57205e-02;
+double vzFitParam_2 = 8.99841e-04;
+double vzFitParam_3 = 5.73702e-05;
 
-double e_vzFitParam_0 = 7.03984e-03;
-double e_vzFitParam_1 = 1.37839e-03;
-double e_vzFitParam_2 = 0.0;
-double e_vzFitParam_3 = 0.0;
+double e_vzFitParam_0 = 1.38486e-02;
+double e_vzFitParam_1 = 3.72158e-03;
+double e_vzFitParam_2 = 3.38136e-04;
+double e_vzFitParam_3 = 4.29366e-05;
--- headers/fitParameters/vzFitParams_PYTHIA_mu12.h
+++ headers/fitParameters/vzFitParams_PYTHIA_mu12.h
@@ -4,13 +4,13 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 0.975875;
-double vzFitParam_1 = 0.00243224;
-double vzFitParam_2 = 0.000715301;
-double vzFitParam_3 = 0.0;
+double vzFitParam_0 = 9.74504e-01;
+double vzFitParam_1 = 4.97307e-03;
+double vzFitParam_2 = 7.05924e-04;
+double vzFitParam_3 = -1.14953e-05;
 
-double e_vzFitParam_0 = 0.00727791;
-double e_vzFitParam_1 = 0.00107092;
-double e_vzFitParam_2 = 0.000149009;
-double e_vzFitParam_3 = 0.0;
+double e_vzFitParam_0 = 7.65411e-03;
+double e_vzFitParam_1 = 2.02665e-03;
+double e_vzFitParam_2 = 1.50048e-04;
+double e_vzFitParam_3 = 2.01103e-05;
 
