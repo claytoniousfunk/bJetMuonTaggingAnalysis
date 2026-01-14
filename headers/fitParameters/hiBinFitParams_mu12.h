42ed164f43ea10d43de1e085494eefae62261628
clayton <clayton@debian>
2026-01-14 08:49:42 -0600

    apply vz and hiBin reweight

--- headers/fitParameters/hiBinFitParams_mu12.h
+++ headers/fitParameters/hiBinFitParams_mu12.h
@@ -1,22 +1,22 @@
 
-double hiBinFitParam_0 = 2.23319;
-double hiBinFitParam_1 = 0.129792;
-double hiBinFitParam_2 = -0.0117398;
-double hiBinFitParam_3 = 0.000397573;
-double hiBinFitParam_4 = -7.34681e-06;
-double hiBinFitParam_5 = 7.90566e-08;
-double hiBinFitParam_6 = -4.93426e-10;
-double hiBinFitParam_7 = 1.65641e-12;
-double hiBinFitParam_8 = -2.31409e-15;
+double hiBinFitParam_0 = 3.70117;
+double hiBinFitParam_1 = -0.0620687;
+double hiBinFitParam_2 = 0.000348846;
+double hiBinFitParam_3 = -6.55547e-07;
+double hiBinFitParam_4 = 0.;
+double hiBinFitParam_5 = 0.;
+double hiBinFitParam_6 = 0.;
+double hiBinFitParam_7 = 0.;
+double hiBinFitParam_8 = 0.;
 
 
-double e_hiBinFitParam_0 = 0.446242;
-double e_hiBinFitParam_1 = 0.0884157;
-double e_hiBinFitParam_2 = 0.00609432;
-double e_hiBinFitParam_3 = 0.000202276;
-double e_hiBinFitParam_4 = 3.71082e-06;
-double e_hiBinFitParam_5 = 3.97474e-08;
-double e_hiBinFitParam_6 = 2.47765e-10;
-double e_hiBinFitParam_7 = 8.3337e-13;
-double e_hiBinFitParam_8 = 1.1697e-15;
+double e_hiBinFitParam_0 = 0.0937337;
+double e_hiBinFitParam_1 = 0.00262006;
+double e_hiBinFitParam_2 = 2.37674e-05;
+double e_hiBinFitParam_3 = 6.93284e-08;
+double e_hiBinFitParam_4 = 0.;
+double e_hiBinFitParam_5 = 0.;
+double e_hiBinFitParam_6 = 0.;
+double e_hiBinFitParam_7 = 0.;
+double e_hiBinFitParam_8 = 0.;
 
--- headers/fitParameters/vzFitParams_PH_mu12.h
+++ headers/fitParameters/vzFitParams_PH_mu12.h
@@ -4,12 +4,12 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 1.00298e+00;
-double vzFitParam_1 = -2.57205e-02;
-double vzFitParam_2 = 8.99841e-04;
-double vzFitParam_3 = 5.73702e-05;
+double vzFitParam_0 = 9.98744e-01;
+double vzFitParam_1 = -1.32932e-02;
+double vzFitParam_2 = 1.01163e-03;
+double vzFitParam_3 = -1.74418e-05;
 
-double e_vzFitParam_0 = 1.38486e-02;
-double e_vzFitParam_1 = 3.72158e-03;
-double e_vzFitParam_2 = 3.38136e-04;
-double e_vzFitParam_3 = 4.29366e-05;
+double e_vzFitParam_0 = 1.24962e-02;
+double e_vzFitParam_1 = 3.11688e-03;
+double e_vzFitParam_2 = 3.31402e-04;
+double e_vzFitParam_3 = 3.87838e-05;
