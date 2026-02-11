52e203f9dc2848148263c25453df06dbccdd17bc
clayton <clayton@debian>
2026-02-11 14:46:01 -0600

    new fit fxn / parameters

--- headers/fitFunctions/fitFxn_pp_HLT.h
+++ headers/fitFunctions/fitFxn_pp_HLT.h
@@ -3,9 +3,15 @@ void loadFitFxn_pp_HLT(){
 
   cout << "~~ loading fit function ..." << endl;
 
-  fitFxn_pp_HLT = new TF1("fitFxn_pp_HLT","[0]",15,100);
+  fitFxn_pp_HLT = new TF1("fitFxn_pp_HLT","[0] - ([0] - [1])*exp(-[2]*x)",15,100);
+
   fitFxn_pp_HLT->SetParameter(0,HLTFitParam_0);
+  fitFxn_pp_HLT->SetParameter(1,HLTFitParam_1);
+  fitFxn_pp_HLT->SetParameter(2,HLTFitParam_2);
+  
   fitFxn_pp_HLT->SetParError(0,e_HLTFitParam_0);
+  fitFxn_pp_HLT->SetParError(1,e_HLTFitParam_1);
+  fitFxn_pp_HLT->SetParError(2,e_HLTFitParam_2);
 
 }
 
--- headers/fitParameters/HLTFitParams_pp.h
+++ headers/fitParameters/HLTFitParams_pp.h
@@ -1,2 +1,7 @@
-double HLTFitParam_0 = 0.98246;
-double e_HLTFitParam_0 = 5.51916e-05;
+double HLTFitParam_0 = 0.956957;
+double HLTFitParam_1 = 1.16729e-08;
+double HLTFitParam_2 = 0.306607;
+
+double e_HLTFitParam_0 = 0.000633395;
+double e_HLTFitParam_1 = 0.763116;
+double e_HLTFitParam_2 = 0.0114768;
