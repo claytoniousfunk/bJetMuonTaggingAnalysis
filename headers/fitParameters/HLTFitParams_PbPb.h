13e69401895ebdacb3d9f17aa07bde825cfa8ec6
clayton <clayton@debian>
2026-02-11 14:54:03 -0600

    updated parameters

--- headers/fitParameters/HLTFitParams_PbPb.h
+++ headers/fitParameters/HLTFitParams_PbPb.h
@@ -1,43 +1,43 @@
 
 ////////////////////////////////////////
 
-double HLTFitParam_C4_0 = 0.971696;
-double HLTFitParam_C4_1 = 0.886305;
-double HLTFitParam_C4_2 = 0.0473851;
+double HLTFitParam_C4_0 = 0.971698;
+double HLTFitParam_C4_1 = 0.886301;
+double HLTFitParam_C4_2 = 0.0473848;
 
-double e_HLTFitParam_C4_0 = 0.0188258;
-double e_HLTFitParam_C4_1 = 0.06047;
-double e_HLTFitParam_C4_2 = 0.0486248;
+double e_HLTFitParam_C4_0 = 0.0188033;
+double e_HLTFitParam_C4_1 = 0.0601624;
+double e_HLTFitParam_C4_2 = 0.0477871;
 
 ////////////////////////////////////////
 
-double HLTFitParam_C3_0 = 0.954869;
+double HLTFitParam_C3_0 = 0.954866;
 double HLTFitParam_C3_1 = 0.888563;
-double HLTFitParam_C3_2 = 0.0660236;
+double HLTFitParam_C3_2 = 0.0660258;
 
-double e_HLTFitParam_C3_0 = 0.00838191;
-double e_HLTFitParam_C3_1 = 0.0432969;
-double e_HLTFitParam_C3_2 = 0.0443602;
-
-////////////////////////////////////////
-
-double HLTFitParam_C2_0 = 0.948642;
-double HLTFitParam_C2_1 = 0.70917;
-double HLTFitParam_C2_2 = 0.0832607;
-
-double e_HLTFitParam_C2_0 = 0.00615371;
-double e_HLTFitParam_C2_1 = 0.0789005;
-double e_HLTFitParam_C2_2 = 0.0213808;
+double e_HLTFitParam_C3_0 = 0.00837971;
+double e_HLTFitParam_C3_1 = 0.0432095;
+double e_HLTFitParam_C3_2 = 0.0438969;
 
 ////////////////////////////////////////
 
-double HLTFitParam_C1_0 = 0.907914;
-double HLTFitParam_C1_1 = 0.567075;
-double HLTFitParam_C1_2 = 0.0787966;
+double HLTFitParam_C2_0 = 0.94863;
+double HLTFitParam_C2_1 = 0.708984;
+double HLTFitParam_C2_2 = 0.0833156;
 
-double e_HLTFitParam_C1_0 = 0.0112361;
-double e_HLTFitParam_C1_1 = 0.122321;
-double e_HLTFitParam_C1_2 = 0.0240992;
+double e_HLTFitParam_C2_0 = 0.00615118;
+double e_HLTFitParam_C2_1 = 0.0785794;
+double e_HLTFitParam_C2_2 = 0.0213415;
+
+////////////////////////////////////////
+
+double HLTFitParam_C1_0 = 0.907918;
+double HLTFitParam_C1_1 = 0.567051;
+double HLTFitParam_C1_2 = 0.0787974;
+
+double e_HLTFitParam_C1_0 = 0.0112423;
+double e_HLTFitParam_C1_1 = 0.121233;
+double e_HLTFitParam_C1_2 = 0.0240583;
 
 
 
