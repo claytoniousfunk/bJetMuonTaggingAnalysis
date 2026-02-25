2b3ac9bfb88b6debbe9b1186211af8777235c3d4
clayton <clayton@debian>
2026-02-25 10:22:53 -0600

    get rid of extraneous fit parameters

--- headers/fitParameters/hiBinFitParams_PYTHIAHYDJET.h
+++ headers/fitParameters/hiBinFitParams_PYTHIAHYDJET.h
@@ -6,21 +6,11 @@ double hiBinFitParam_mu5_0 = 3.35491;
 double hiBinFitParam_mu5_1 = -0.0589663;
 double hiBinFitParam_mu5_2 = 0.000350464;
 double hiBinFitParam_mu5_3 = -7.05299e-07;
-double hiBinFitParam_mu5_4 = 0.;
-double hiBinFitParam_mu5_5 = 0.;
-double hiBinFitParam_mu5_6 = 0.;
-double hiBinFitParam_mu5_7 = 0.;
-double hiBinFitParam_mu5_8 = 0.;
 
 double e_hiBinFitParam_mu5_0 = 0.154961;
 double e_hiBinFitParam_mu5_1 = 0.00445506;
 double e_hiBinFitParam_mu5_2 = 4.12655e-05;
 double e_hiBinFitParam_mu5_3 = 1.22559e-07;
-double e_hiBinFitParam_mu5_4 = 0.;
-double e_hiBinFitParam_mu5_5 = 0.;
-double e_hiBinFitParam_mu5_6 = 0.;
-double e_hiBinFitParam_mu5_7 = 0.;
-double e_hiBinFitParam_mu5_8 = 0.;
 
 ////////////////////////////////////////////
 
@@ -28,21 +18,11 @@ double hiBinFitParam_mu7_0 = 3.36709;
 double hiBinFitParam_mu7_1 = -0.0554937;
 double hiBinFitParam_mu7_2 = 0.00030277;
 double hiBinFitParam_mu7_3 = -5.42342e-07;
-double hiBinFitParam_mu7_4 = 0.;
-double hiBinFitParam_mu7_5 = 0.;
-double hiBinFitParam_mu7_6 = 0.;
-double hiBinFitParam_mu7_7 = 0.;
-double hiBinFitParam_mu7_8 = 0.;
 
 double e_hiBinFitParam_mu7_0 = 0.10813;
 double e_hiBinFitParam_mu7_1 = 0.00311537;
 double e_hiBinFitParam_mu7_2 = 2.88812e-05;
 double e_hiBinFitParam_mu7_3 = 8.57709e-08;
-double e_hiBinFitParam_mu7_4 = 0.;
-double e_hiBinFitParam_mu7_5 = 0.;
-double e_hiBinFitParam_mu7_6 = 0.;
-double e_hiBinFitParam_mu7_7 = 0.;
-double e_hiBinFitParam_mu7_8 = 0.;
 
 ////////////////////////////////////////////
 
@@ -50,19 +30,9 @@ double hiBinFitParam_mu12_0 = 3.63402;
 double hiBinFitParam_mu12_1 = -0.0602146;
 double hiBinFitParam_mu12_2 = 0.000332076;
 double hiBinFitParam_mu12_3 = -6.06758e-07;
-double hiBinFitParam_mu12_4 = 0.;
-double hiBinFitParam_mu12_5 = 0.;
-double hiBinFitParam_mu12_6 = 0.;
-double hiBinFitParam_mu12_7 = 0.;
-double hiBinFitParam_mu12_8 = 0.;
 
 double e_hiBinFitParam_mu12_0 = 0.0987279;
 double e_hiBinFitParam_mu12_1 = 0.00273216;
 double e_hiBinFitParam_mu12_2 = 2.47368e-05;
 double e_hiBinFitParam_mu12_3 = 7.23575e-08;
-double e_hiBinFitParam_mu12_4 = 0.;
-double e_hiBinFitParam_mu12_5 = 0.;
-double e_hiBinFitParam_mu12_6 = 0.;
-double e_hiBinFitParam_mu12_7 = 0.;
-double e_hiBinFitParam_mu12_8 = 0.;
 
