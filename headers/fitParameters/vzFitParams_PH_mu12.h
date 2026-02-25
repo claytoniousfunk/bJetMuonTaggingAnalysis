c591880d8c9d3ae8ba93fcb7980d025b1699ab8f
clayton <clayton@debian>
2026-02-24 22:55:05 -0600

    update

--- headers/fitParameters/vzFitParams_PH_mu12.h
+++ headers/fitParameters/vzFitParams_PH_mu12.h
@@ -4,12 +4,12 @@
 
 int N_vzFitParam = 4;
 
-double vzFitParam_0 = 9.98744e-01;
-double vzFitParam_1 = -1.32932e-02;
-double vzFitParam_2 = 1.01163e-03;
-double vzFitParam_3 = -1.74418e-05;
+double vzFitParam_m12_0 = 9.98744e-01;
+double vzFitParam_m12_1 = -1.32932e-02;
+double vzFitParam_m12_2 = 1.01163e-03;
+double vzFitParam_m12_3 = -1.74418e-05;
 
-double e_vzFitParam_0 = 1.24962e-02;
-double e_vzFitParam_1 = 3.11688e-03;
-double e_vzFitParam_2 = 3.31402e-04;
-double e_vzFitParam_3 = 3.87838e-05;
+double e_vzFitParam_m12_0 = 1.24962e-02;
+double e_vzFitParam_m12_1 = 3.11688e-03;
+double e_vzFitParam_m12_2 = 3.31402e-04;
+double e_vzFitParam_m12_3 = 3.87838e-05;
