ab7c40a9bd842fb80c6a989afbfbc22ca98b4dfd
clayton <clayton@debian>
2026-02-25 10:16:31 -0600

    hiBin fit parameters and functions for mu5, mu7, + mu12

--- headers/fitFunctions/fitFxn_hiBin.h
+++ headers/fitFunctions/fitFxn_hiBin.h
@@ -1,18 +1,59 @@
-void loadFitFxn_hiBin(){
 
-  cout << "~~~~ loading hiBin fit function..." << endl;
+void loadFitFxn_hiBin_mu5(){
+
+  cout << "~~~~ loading hiBin fit function (mu5)..." << endl;
   
-  //fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]*TMath::Exp([1]*x) + [2] + [3]*x + [4]*x*x + [5]*x*x*x",0,190);
-  fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]+ [1]*x + [2]*x*x + [3]*x*x*x + [4]*x*x*x*x + [5]*x*x*x*x*x + [6]*x*x*x*x*x*x + [7]*x*x*x*x*x*x*x + [8]*x*x*x*x*x*x*x*x",0,160);
-  fitFxn_hiBin->SetParameter(0,hiBinFitParam_0);
-  fitFxn_hiBin->SetParameter(1,hiBinFitParam_1);
-  fitFxn_hiBin->SetParameter(2,hiBinFitParam_2);
-  fitFxn_hiBin->SetParameter(3,hiBinFitParam_3);
-  fitFxn_hiBin->SetParameter(4,hiBinFitParam_4);
-  fitFxn_hiBin->SetParameter(5,hiBinFitParam_5);
-  fitFxn_hiBin->SetParameter(6,hiBinFitParam_6);
-  fitFxn_hiBin->SetParameter(7,hiBinFitParam_7);
-  fitFxn_hiBin->SetParameter(8,hiBinFitParam_8);
+  fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]+ [1]*x + [2]*x*x + [3]*x*x*x",0,160);
+
+  fitFxn_hiBin->SetParameter(0,hiBinFitParam_mu5_0);
+  fitFxn_hiBin->SetParameter(1,hiBinFitParam_mu5_1);
+  fitFxn_hiBin->SetParameter(2,hiBinFitParam_mu5_2);
+  fitFxn_hiBin->SetParameter(3,hiBinFitParam_mu5_3);
+
+  fitFxn_hiBin->SetParError(0,e_hiBinFitParam_mu5_0);
+  fitFxn_hiBin->SetParError(1,e_hiBinFitParam_mu5_1);
+  fitFxn_hiBin->SetParError(2,e_hiBinFitParam_mu5_2);
+  fitFxn_hiBin->SetParError(3,e_hiBinFitParam_mu5_3);
+
+  cout << "~~~~ ... done!" << endl;
+  
+}
+
+void loadFitFxn_hiBin_mu7(){
+
+  cout << "~~~~ loading hiBin fit function (mu7)..." << endl;
+  
+  fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]+ [1]*x + [2]*x*x + [3]*x*x*x",0,160);
+
+  fitFxn_hiBin->SetParameter(0,hiBinFitParam_mu7_0);
+  fitFxn_hiBin->SetParameter(1,hiBinFitParam_mu7_1);
+  fitFxn_hiBin->SetParameter(2,hiBinFitParam_mu7_2);
+  fitFxn_hiBin->SetParameter(3,hiBinFitParam_mu7_3);
+
+  fitFxn_hiBin->SetParError(0,e_hiBinFitParam_mu7_0);
+  fitFxn_hiBin->SetParError(1,e_hiBinFitParam_mu7_1);
+  fitFxn_hiBin->SetParError(2,e_hiBinFitParam_mu7_2);
+  fitFxn_hiBin->SetParError(3,e_hiBinFitParam_mu7_3);
+
+  cout << "~~~~ ... done!" << endl;
+  
+}
+
+void loadFitFxn_hiBin_mu12(){
+
+  cout << "~~~~ loading hiBin fit function (mu12)..." << endl;
+  
+  fitFxn_hiBin = new TF1("fitFxn_hiBin","[0]+ [1]*x + [2]*x*x + [3]*x*x*x",0,160);
+
+  fitFxn_hiBin->SetParameter(0,hiBinFitParam_mu12_0);
+  fitFxn_hiBin->SetParameter(1,hiBinFitParam_mu12_1);
+  fitFxn_hiBin->SetParameter(2,hiBinFitParam_mu12_2);
+  fitFxn_hiBin->SetParameter(3,hiBinFitParam_mu12_3);
+
+  fitFxn_hiBin->SetParError(0,e_hiBinFitParam_mu12_0);
+  fitFxn_hiBin->SetParError(1,e_hiBinFitParam_mu12_1);
+  fitFxn_hiBin->SetParError(2,e_hiBinFitParam_mu12_2);
+  fitFxn_hiBin->SetParError(3,e_hiBinFitParam_mu12_3);
 
   cout << "~~~~ ... done!" << endl;
   
