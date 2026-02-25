04f7a8e6a5c6a6562f584fb3d609048e459ae604
clayton <clayton@debian>
2026-02-25 09:56:42 -0600

    mu5, mu7, and mu12 fit parameters in a single header

---
+++ headers/fitParameters/vzFitParams_PYTHIAHYDJET.h
@@ -0,0 +1,40 @@
+// PYTHIA
+// ---------- vz fit parameters
+// ---------- 3rd order polynomial
+
+////////////////////////////////////////////////
+
+double vzFitParam_mu5_0 = 9.36300e-01;
+double vzFitParam_mu5_1 = -1.46719e-02;
+double vzFitParam_mu5_2 = 3.02369e-03;
+double vzFitParam_mu5_3 = 2.04703e-05;
+
+double e_vzFitParam_mu5_0 = 2.20511e-02;
+double e_vzFitParam_mu5_1 = 6.25420e-03;
+double e_vzFitParam_mu5_2 = 5.58661e-04;
+double e_vzFitParam_mu5_3 = 7.35975e-05;
+
+////////////////////////////////////////////////
+
+double vzFitParam_mu7_0 = 9.81506e-01;
+double vzFitParam_mu7_1 = -1.21066e-02;
+double vzFitParam_mu7_2 = 1.29050e-03;
+double vzFitParam_mu7_3 = -8.35634e-06;
+
+double e_vzFitParam_mu7_0 = 1.40943e-02;
+double e_vzFitParam_mu7_1 = 3.96917e-03;
+double e_vzFitParam_mu7_2 = 3.78670e-04;
+double e_vzFitParam_mu7_3 = 4.79740e-05;
+
+////////////////////////////////////////////////
+
+double vzFitParam_mu12_0 = 9.96560e-01;
+double vzFitParam_mu12_1 = -1.46536e-02;
+double vzFitParam_mu12_2 = 7.34228e-04;
+double vzFitParam_mu12_3 = -1.57123e-05;
+
+double e_vzFitParam_mu12_0 = 1.27347e-02;
+double e_vzFitParam_mu12_1 = 3.41298e-03;
+double e_vzFitParam_mu12_2 = 3.34077e-04;
+double e_vzFitParam_mu12_3 = 4.15961e-05;
+
