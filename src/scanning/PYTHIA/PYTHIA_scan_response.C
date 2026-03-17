19ead63d2ea422dde25db47f351a14e79aed3b94
clayton <clayton@debian>
2026-03-17 12:36:10 -0500

    scan odd events

--- src/scanning/PYTHIA/PYTHIA_scan_response.C
+++ src/scanning/PYTHIA/PYTHIA_scan_response.C
@@ -157,7 +157,8 @@ void PYTHIA_scan_response(int group = 1){
   //TString output = Form("%s%s_muTaggedJets/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
   //TString output = Form("%s%s_muTaggedJetsNoTrigger/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
   //TString output = Form("%s%s_genMuTaggedGenJets/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
-  TString output = Form("%s%s_evenEvents/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
+  //TString output = Form("%s%s_evenEvents/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
+  TString output = Form("%s%s_oddEvents/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
   
 
   std::cout << "output dataset = " << output << std::endl;
@@ -406,11 +407,11 @@ void PYTHIA_scan_response(int group = 1){
 
     if(evi==0) cout << "Processing events..." << endl;
 
-    // // only take odd events
-    // if(evi % 2 == 0) continue;
+    // only take odd events
+    if(evi % 2 == 0) continue;
 
-    // only take even events
-    if(evi % 2 == 1) continue;
+    // // only take even events
+    // if(evi % 2 == 1) continue;
 
     em->getEvent(evi);
 
