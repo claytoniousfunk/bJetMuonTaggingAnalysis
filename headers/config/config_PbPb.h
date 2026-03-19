d0d78811fcd4f82810654f5a39399c547494259b
clayton <clayton@debian>
2026-03-19 02:17:19 -0500

    scan minBias

--- headers/config/config_PbPb.h
+++ headers/config/config_PbPb.h
@@ -17,7 +17,7 @@ bool doWDecayFilter = true;
 // High Level Triggers (HLTs)
 bool applyJet60Trigger = false;
 bool applyJet80Trigger = false;
-bool applyJet100Trigger = true;
+bool applyJet100Trigger = false;
 bool applyMinBiasTrigger = false;
 bool applyMu12TriggerEfficiencyCorrection = false;
 bool fillMu5 = false;
@@ -34,6 +34,6 @@ bool apply_JEU_shift_up = false;
 bool apply_JEU_shift_down = false;
 // select dataset
 bool doSingleMuonSample = false;
-bool doMinBiasSample = false;
-bool doHardProbesSample = true;
+bool doMinBiasSample = true;
+bool doHardProbesSample = false;
 
