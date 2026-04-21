f837a47be5221ca57e5df9c7923d3a4bc3ab1aa7
clayton <clayton@debian>
2026-04-20 23:08:16 -0500

    fix syntax

--- headers/fitParameters/hiBinFitParams_HYDJET.h
+++ headers/fitParameters/hiBinFitParams_HYDJET.h
@@ -6,7 +6,7 @@ double hiBinFitParam_1 = 0.0387776;
 double hiBinFitParam_2 = -0.000432978;
 double hiBinFitParam_3 = 1.53708e-06;
 
-double e_hiBinFitParam_0 = 0.0470433
+double e_hiBinFitParam_0 = 0.0470433;
 double e_hiBinFitParam_1 = 0.00644498;
 double e_hiBinFitParam_2 = 0.000158804;
 double e_hiBinFitParam_3 = 9.82955e-07;
