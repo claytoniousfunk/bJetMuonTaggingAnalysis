7fd20332c0d37711ce06c72fa081eb9c627d5aed
clayton <clayton@debian>
2026-04-22 11:00:02 -0500

    update input skim for SingleMuon

--- headers/functions/getDatasetName/getDatasetName_PbPb.h
+++ headers/functions/getDatasetName/getDatasetName_PbPb.h
@@ -7,10 +7,10 @@ TString getDatasetName(bool doSingleMuonSample = true,
   TString result = "";
   
   //if(doSingleMuonSample) result="/eos/user/c/cbennett/skims/output_PbPb_SingleMuon_withWTA/";
-  if(doSingleMuonSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_SingleMuon_2026-04-20/";
+  if(doSingleMuonSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_SingleMuon_2026-04-22/";
   else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_PbPb_HIMinimumBias0/";
   //else if(doMinBiasSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_MinBias_withMinBiasTrigger/";
-  else if(doHardProbesSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_HardProbes_2026-04-22/";
+  else if(doHardProbesSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_HardProbes_2026-04-20/";
   else{};
 
   return result;
