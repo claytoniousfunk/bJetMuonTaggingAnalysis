3d6ee44c2eadd6714ce89ac874e0c85a455a8871
clayton <clayton@debian>
2026-04-22 10:39:19 -0500

    updated skim

--- headers/functions/getDatasetName/getDatasetName_PbPb.h
+++ headers/functions/getDatasetName/getDatasetName_PbPb.h
@@ -10,7 +10,7 @@ TString getDatasetName(bool doSingleMuonSample = true,
   if(doSingleMuonSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_SingleMuon_2026-04-20/";
   else if(doMinBiasSample) result="/eos/user/c/cbennett/skims/output_skims_PbPb_HIMinimumBias0/";
   //else if(doMinBiasSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_MinBias_withMinBiasTrigger/";
-  else if(doHardProbesSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_HardProbes_2026-04-20/";
+  else if(doHardProbesSample) result="/eos/cms/store/group/phys_heavyions/cbennett/skims/output_skims_PbPb_HardProbes_2026-04-22/";
   else{};
 
   return result;
