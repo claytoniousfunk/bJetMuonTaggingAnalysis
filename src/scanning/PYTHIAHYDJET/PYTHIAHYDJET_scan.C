1a1b73e45a55c80884085ac992886e140d441a2f
clayton <clayton@debian>
2026-05-07 21:02:21 -0500

    scan of P+H ultra-fine cent bins

--- src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan.C
+++ src/scanning/PYTHIAHYDJET/PYTHIAHYDJET_scan.C
@@ -38,7 +38,8 @@
 #include "../../../JetEnergyCorrections/JetUncertainty.h"
 // general analysis variables
 //#include "../../../headers/AnalysisSetupV2p2.h" // coarse centrality bins
-#include "../../../headers/AnalysisSetupV2p3.h" // fine centrality bins
+//#include "../../../headers/AnalysisSetupV2p3.h" // nominal centrality bins
+#include "../../../headers/AnalysisSetupV2p4.h" // ultra-fine centrality bins
 // vz-fit parameters
 //#include "../../../headers/fitParameters/vzFitParams_PH_mu5.h"
 //#include "../../../headers/fitParameters/vzFitParams_PH_mu7.h"
@@ -100,7 +101,8 @@ TF1 *fitFxn_PYTHIAHYDJET_HLT_C4, *fitFxn_PYTHIAHYDJET_HLT_C3, *fitFxn_PYTHIAHYDJ
 #include "../../../headers/functions/getJetPtBin.h"
 // getCentBin function
 //#include "../../../headers/functions/getCentBin_v2.h" // course centrality binning
-#include "../../../headers/functions/getCentBin.h" // fine centrality binning 
+//#include "../../../headers/functions/getCentBin.h" // nominal centrality binning
+#include "../../../headers/functions/getCentBin_V2p4.h" // ultra-fine centrality binning 
 // getPtRel function
 #include "../../../headers/functions/getPtRel.h"
 // isQualityMuon_hybridSoft function
@@ -434,7 +436,8 @@ void PYTHIAHYDJET_scan(int group = 1){
 						 fillMu7,
 						 fillMu12);
 
-  TString output = Form("%s%s/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
+  //TString output = Form("%s%s/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
+  TString output = Form("%s%s_ultraFineCentBins/PYTHIAHYDJET_scan_output_%i.root",outputBaseDir.Data(),outputDatasetName.Data(),group);
 
   std::cout << "output dataset = " << output << std::endl;
   
