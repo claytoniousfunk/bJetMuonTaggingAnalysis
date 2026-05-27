7e2446c395183299d06737d29ec22a86211fe0c4
clayton <clayton@debian>
2026-05-27 00:49:20 -0500

    accidentally used P+H fit, this one is correct

--- headers/fitParameters/hiBinFitParams_HYDJET.h
+++ headers/fitParameters/hiBinFitParams_HYDJET.h
@@ -1,12 +1,12 @@
 
 ////////////////////////////////////////////
 
-double hiBinFitParam_0 = 3.82921;
-double hiBinFitParam_1 = -0.0619206;
-double hiBinFitParam_2 = 0.000325971;
-double hiBinFitParam_3 = -5.48669e-07;
+double hiBinFitParam_0 = 3.62748;
+double hiBinFitParam_1 = -0.0617797;
+double hiBinFitParam_2 = 0.000356537;
+double hiBinFitParam_3 = -6.95915e-07;
 
-double e_hiBinFitParam_0 = 0.00361472;
-double e_hiBinFitParam_1 = 0.000126704;
-double e_hiBinFitParam_2 = 1.36041e-06;
-double e_hiBinFitParam_3 = 4.47245e-09;
+double e_hiBinFitParam_0 = 0.0109484;
+double e_hiBinFitParam_1 = 0.00032837;
+double e_hiBinFitParam_2 = 3.17465e-06;
+double e_hiBinFitParam_3 = 9.74277e-09;
