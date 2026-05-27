40acbca24453c8a8d0143ca3698970afc563bc75
clayton <clayton@debian>
2026-05-27 00:38:13 -0500

    fix parameters

--- headers/fitParameters/hiBinFitParams_HYDJET.h
+++ headers/fitParameters/hiBinFitParams_HYDJET.h
@@ -1,12 +1,12 @@
 
 ////////////////////////////////////////////
 
-double hiBinFitParam_0 = 0.382476;
-double hiBinFitParam_1 = 0.0387776;
-double hiBinFitParam_2 = -0.000432978;
-double hiBinFitParam_3 = 1.53708e-06;
+double hiBinFitParam_0 = 3.82921;
+double hiBinFitParam_1 = -0.0619206;
+double hiBinFitParam_2 = 0.000325971;
+double hiBinFitParam_3 = -5.48669e-07;
 
-double e_hiBinFitParam_0 = 0.0470433;
-double e_hiBinFitParam_1 = 0.00644498;
-double e_hiBinFitParam_2 = 0.000158804;
-double e_hiBinFitParam_3 = 9.82955e-07;
+double e_hiBinFitParam_0 = 0.00361472;
+double e_hiBinFitParam_1 = 0.000126704;
+double e_hiBinFitParam_2 = 1.36041e-06;
+double e_hiBinFitParam_3 = 4.47245e-09;
