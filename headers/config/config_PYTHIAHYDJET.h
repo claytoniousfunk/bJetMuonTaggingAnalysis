b227c2f6bf754d97c60d116a67fc9fc1e9cb0b62
clayton <clayton@debian>
2026-05-27 09:06:53 -0500

    turn off pThat weight

--- headers/config/config_PYTHIAHYDJET.h
+++ headers/config/config_PYTHIAHYDJET.h
@@ -13,7 +13,7 @@ bool apply_JEU_shift_down = false;
 // options to skip genParticle loops (for forests with no genParticle info)
 bool skipGenParticles = false; // set TRUE for MuJet and BJet datasets
 // reweighting functions
-bool doPThatWeight = true;
+bool doPThatWeight = false;
 bool doHiBinReweight = false;
 bool doHiBinReweightToHardProbesJet80 = true;
 bool doVzReweight = true;
