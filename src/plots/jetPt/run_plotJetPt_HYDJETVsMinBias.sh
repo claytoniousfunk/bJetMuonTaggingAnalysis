5231a8babfa903948d9d270753576cbbabc5ecce
clayton <clayton@debian>
2026-05-29 10:03:47 -0500

    bash to run a bunch of plotJetPt scripts (over all cent ranges)

---
+++ src/plots/jetPt/run_plotJetPt_HYDJETVsMinBias.sh
@@ -0,0 +1,32 @@
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0)"
+
+root -l -q "plotJetPt_HYDJETVsMinBias.C(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)"
