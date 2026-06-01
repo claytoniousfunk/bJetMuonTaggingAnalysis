180dfa8a3366ff15fe659109b908cd639b5808b0
clayton <clayton@debian>
2026-06-01 14:00:57 -0500

    scan jet40

--- headers/config/config_pp.h
+++ headers/config/config_pp.h
@@ -14,8 +14,8 @@ bool doEtaPhiMask = false;
 bool doWDecayFilter = true;
 // triggers
 bool applyJet15Trigger = false;
-bool applyJet30Trigger = true;
-bool applyJet40Trigger = false;
+bool applyJet30Trigger = false;
+bool applyJet40Trigger = true;
 bool applyJet60Trigger = false;
 bool applyJet80Trigger = false;
 bool applyJet100Trigger = false;
