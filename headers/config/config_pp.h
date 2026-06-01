24946f440d1b35a565c585552f6b262ef7582349
clayton <clayton@debian>
2026-06-01 12:30:02 -0500

    turn off applyMu12TriggerEfficiencyCorrection

--- headers/config/config_pp.h
+++ headers/config/config_pp.h
@@ -26,7 +26,7 @@ bool fillMu5 = false;
 bool fillMu7 = false;
 bool fillMu12 = true;
 // spectra alterations
-bool applyMu12TriggerEfficiencyCorrection = true;
+bool applyMu12TriggerEfficiencyCorrection = false;
 bool doJESCorrection = false;
 bool doBJetNeutrinoEnergyShift = false;
 bool doJERCorrection = false;
