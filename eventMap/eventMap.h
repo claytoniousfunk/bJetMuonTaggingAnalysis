4abdeaddc7b7b79debd3a27adb45ba5bbf3c3411
clayton <clayton@debian>
2026-06-01 12:24:07 -0500

    include lower threshold jet triggers for pp scan

--- eventMap/eventMap.h
+++ eventMap/eventMap.h
@@ -134,7 +134,7 @@ public :
   int HLT_HIAK4CaloJet80_v1;
   int HLT_HIPuAK4CaloJet40Eta5p1_v1, HLT_HIPuAK4CaloJet60Eta5p1_v1, HLT_HIPuAK4CaloJet80Eta5p1_v1, HLT_HIPuAK4CaloJet100Eta5p1_v1;
   int HLT_HIPuAK4CaloJet40Eta5p1_v1_Prescl, HLT_HIPuAK4CaloJet60Eta5p1_v1_Prescl, HLT_HIPuAK4CaloJet80Eta5p1_v1_Prescl, HLT_HIPuAK4CaloJet100Eta5p1_v1_Prescl;
-  int HLT_HIAK4PFJet40_v1_Prescl, HLT_HIAK4PFJet60_v1_Prescl, HLT_HIAK4PFJet80_v1_Prescl, HLT_HIAK4PFJet100_v1_Prescl;
+  int HLT_HIAK4PFJet15_v1_Prescl, HLT_HIAK4PFJet30_v1_Prescl, HLT_HIAK4PFJet40_v1_Prescl, HLT_HIAK4PFJet60_v1_Prescl, HLT_HIAK4PFJet80_v1_Prescl, HLT_HIAK4PFJet100_v1_Prescl, HLT_HIAK4PFJet100_v1_Prescl;
   int HLT_HICsAK4PFJet60Eta1p5_v1, HLT_HICsAK4PFJet80Eta1p5_v1, HLT_HICsAK4PFJet100Eta1p5_v1;
   int HLT_HICsAK4PFJet60Eta1p5_v1_Prescl, HLT_HICsAK4PFJet80Eta1p5_v1_Prescl, HLT_HICsAK4PFJet100Eta1p5_v1_Prescl;
 
@@ -432,6 +432,12 @@ void eventMap::loadHLT(const char* name){
   //evtTree->SetBranchAddress("HLT_HIAK4PFJet30_v1",&HLT_HIAK4PFJet30_v1);
   //evtTree->SetBranchAddress("HLT_HIAK4PFJet40_v1",&HLT_HIAK4PFJet40_v1);
   //evtTree->SetBranchAddress("HLT_HIAK4PFJet40_v1_Prescl",&HLT_HIAK4PFJet40_v1_Prescl);
+  evtTree->SetBranchAddress("HLT_HIAK4PFJet15_v1",&HLT_HIAK4PFJet15_v1);
+  evtTree->SetBranchAddress("HLT_HIAK4PFJet15_v1_Prescl",&HLT_HIAK4PFJet15_v1_Prescl);
+  evtTree->SetBranchAddress("HLT_HIAK4PFJet30_v1",&HLT_HIAK4PFJet30_v1);
+  evtTree->SetBranchAddress("HLT_HIAK4PFJet30_v1_Prescl",&HLT_HIAK4PFJet30_v1_Prescl);
+  evtTree->SetBranchAddress("HLT_HIAK4PFJet40_v1",&HLT_HIAK4PFJet40_v1);
+  evtTree->SetBranchAddress("HLT_HIAK4PFJet40_v1_Prescl",&HLT_HIAK4PFJet40_v1_Prescl);
   evtTree->SetBranchAddress("HLT_HIAK4PFJet60_v1",&HLT_HIAK4PFJet60_v1);
   evtTree->SetBranchAddress("HLT_HIAK4PFJet60_v1_Prescl",&HLT_HIAK4PFJet60_v1_Prescl);
   evtTree->SetBranchAddress("HLT_HIAK4PFJet80_v1",&HLT_HIAK4PFJet80_v1);
